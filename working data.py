689a6f6078f6f70585bc0a85f4621c0963db0700
tessterr <168154763+tessterr@users.noreply.github.com>
2024-06-12 18:22:25 +0530

    Update working data.py

--- working data.py
+++ working data.py
@@ -1,1 +1,9 @@
 'https://www.facebook.com/login/'
+
+{
+    'jazoest': re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+    'lsd': re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+    'email': idf,
+    'login_source': 'comet_headerless_login',
+    'next': '',
+    'encpass': '#PWD_BROWSER:0:{}:{}'.format(re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1), ps) }
