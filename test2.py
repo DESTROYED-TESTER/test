32d471c32a6683bee67ab44328d25366d679c2d5
tessterr <168154763+tessterr@users.noreply.github.com>
2024-06-23 15:12:00 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -2834,7 +2834,7 @@ def Subscraption():
 	id = "".join(uuid+sk+lk)
 	key1 = UMO+id+ML1
 	Key2 = ATOM+key1
-	r1=requests.get("https://github.com/ATONxSMILE404/Approve.txt/blob/main/Aporove.txt").text
+	r1=requests.get("https://github.com/ATONxSMILE404/APPRO/blob/main/Approve.txt").text
 	if Key2 in r1:
 		os.system('clear')
 		mainx() 
