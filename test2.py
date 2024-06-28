0aeddc56b4e950e42f360c8f97a581fa98d3e75a
tessterr <168154763+tessterr@users.noreply.github.com>
2024-06-28 16:12:27 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1137,7 +1137,7 @@ def m6(idf,pwv):
     'DNT': '1',
     'Pragma': 'no-cache',
     'TE': 'Trailers', }
-   lo = session.post('https://m.facebook.com/login/device-based/login/async/',data=log_data,headers=header_freefb).text
+   lo = session.post('https://x.facebook.com/login/device-based/login/async/',data=log_data,headers=header_freefb).text
    log_cookies=session.cookies.get_dict().keys()
    if 'c_user' in log_cookies:
     coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
@@ -1205,7 +1205,7 @@ def m7(idf,pwv):
     'DNT': '1',
     'Pragma': 'no-cache',
     'TE': 'Trailers', }#'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36',}
-   lo = session.post('https://business.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=120&lwc=1348028&',data=log_data,headers=header_freefb).text
+   lo = session.post('https://x.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=120&lwc=1348028&',data=log_data,headers=header_freefb).text
    log_cookies=session.cookies.get_dict().keys()
    if 'c_user' in log_cookies:
     coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
