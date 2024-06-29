38d140e186c18914c8b499ddd07927499a2d3700
tessterr <168154763+tessterr@users.noreply.github.com>
2024-06-29 17:40:36 +0530

    Update atnw.py

--- atnw.py
+++ atnw.py
@@ -142,6 +142,9 @@ c8=f'{dot}[{H}017{M}-{H}019{M}-{H}016{M}-{H}013{M}-{H}018{M}-{H}014{M}-{H}015{P}
 mtd,cp_xdx,cokix=[],[],[]
 token = ('6628496363:AAFRd1HpukVfL1uuaXfUPABhyaAfLYkzRTU')
 ID = ('1778046662')
+def key():
+    uID = "ATOM-"+base64.b16encode(str(os.getuid()).encode()).decode()+hashlib.md5((platform.version() + str(os.getuid()) + platform.platform() + os.getlogin() + platform.release()).replace(' ', '').encode()).hexdigest()
+    return uID.upper()
 def clear():
   os.system('clear')
 os.system("pkg install espeak")
