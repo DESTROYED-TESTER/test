14be5655afd7a10af3356af03826d9a00002dba7
tessterr <168154763+tessterr@users.noreply.github.com>
2024-06-30 21:15:39 +0530

    Update gtp.py

--- gtp.py
+++ gtp.py
@@ -266,7 +266,7 @@ def ____banner____():
 {faltu} {black}Your self-worth is determined by you... {pvt}
 {rad}[{white}🔖{rad}] {yelloww}ONWER AND CEO {white}▶︎ }{BLUE}𝐜𝐡𝐨𝐲𝐨𝐧 {rad}𝐗 {BLUE}𝐬𝐮𝐦𝐨𝐧
 {rad}[{white}🔖{rad}] {green}only premium accept
-{rad}[{white}🔖{rad}] {green}TOOL FILE & RANDOM TYPES{rad})
+{rad}[{white}🔖{rad}] {green}TOOL FILE & RANDOM TYPES{rad}
 {puti}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━""")
 
 def fuckxd():
