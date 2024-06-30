c701f36d660dad2367af4614dfc2289f911d00c3
tessterr <168154763+tessterr@users.noreply.github.com>
2024-06-30 21:21:40 +0530

    Update gtp.py

--- gtp.py
+++ gtp.py
@@ -263,7 +263,7 @@ def ____banner____():
  /_____\___|_|   \___/  /_/\_\_____/ 
                                         
                                      {rad}♻️ 
-{faltu} {black}Your self-worth is determined by you... 
+{faltu} {black}Your self-worth is determined by you... {pvt}
 {rad}[{white}🔖{rad}] {yelloww}ONWER AND CEO {white}▶︎ {BLUE}𝐜𝐡𝐨𝐲𝐨𝐧 {rad}𝐗 {BLUE}𝐬𝐮𝐦𝐨𝐧
 {rad}[{white}🔖{rad}] {green}only premium accept
 {rad}[{white}🔖{rad}] {green}TOOL FILE & RANDOM TYPES
