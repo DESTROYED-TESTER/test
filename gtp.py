31fef5505a9bab74c11af588bd790bfdb29b6073
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-07-05 10:33:02 +0530

    Update gtp.py

--- gtp.py
+++ gtp.py
@@ -1008,7 +1008,6 @@ def m8(ids,pwv):
             "method": 'GET', 
             "path": '/',
             "scheme": 'https', 
-            "authority": 'p.facebook.com',
             "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             "accept-language": 'en-US,en;q=0.9',
             "cache-control": 'max-age=0',
