315e058c0b49cf483ef368346e2ae367f088ccac
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-07-06 17:59:03 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1157,22 +1157,27 @@ def m1(idf,pwv):
    "email":idf,
    "pass":ps,
    "login":"Log In"}
-   header_freefb =  {'authority':'p.facebook.com',
-   'method': 'POST',
-   'scheme': 'https',
-   'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-   'accept-encoding':'gzip, deflate, br',
-   'accept-language': 'en-US,en;q=0.9,en;q=0.8',
-   'cache-control': 'max-age=0',
-   'sec-ch-ua': '"Google Chrome";v="106", "Not)A;Brand";v="99", "Chromium";v="106"',
-   'sec-ch-ua-mobile': '?1',
-   'sec-ch-ua-platform': '"Linux"',
-   'sec-fetch-dest': 'document',
-   'sec-fetch-mode': 'navigate',
-   'sec-fetch-site': 'same-origin',
-   'sec-fetch-user': '?1',
-   'upgrade-insecure-requests': '1',
-   'user-agent': ua }#'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36',}
+   header_freefb =  {
+    "authority": f'p.facebook.com',
+    "method": 'POST',
+    "scheme": 'https',
+    "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    "accept-encoding": 'gzip, deflate, br',
+    "accept-language": 'en-US,en;q=0.9',
+    "cache-control": 'no-cache, no-store, must-revalidate',
+    "referer": f'https://p.facebook.com/',
+    "sec-ch-ua": '"Google Chrome";v="90", "Chromium";v="90"',
+    "sec-ch-ua-mobile": '?0',
+    "sec-ch-ua-platform": "Windows",
+    "sec-fetch-dest": 'document',
+    "sec-fetch-mode": 'navigate',
+    "sec-fetch-site": 'same-origin',
+    "sec-fetch-user": '?1',
+    "pragma": 'no-cache',
+    "priority": 'u=1',
+    "cross-origin-resource-policy": 'cross-origin',
+    "upgrade-insecure-requests": '1',
+    "user-agent": 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36',}#'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36',}
    lo = session.post('https://p.facebook.com/login/device-based/login/async/',data=log_data,headers=header_freefb).text
    log_cookies=session.cookies.get_dict().keys()
    if 'c_user' in log_cookies:
