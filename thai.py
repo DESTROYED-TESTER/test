eacafe294afcaf4569ed24092b534ed597b09ff8
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-07-09 22:12:17 +0530

    Update thai.py

--- thai.py
+++ thai.py
@@ -631,7 +631,7 @@ def m1(ids,pwv):
             time.sleep(0.01)
         loop+=1
     except requests.exceptions.ConnectionError:
-        time.sleep(7)
+        time.sleep(30)
     except Exception as e:
         pass
 
