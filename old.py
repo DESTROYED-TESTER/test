9a196c6bbb4673dd1b079902a1d6b2f902433114
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-07-11 22:00:42 +0530

    Update old.py

--- old.py
+++ old.py
@@ -1019,7 +1019,7 @@ def m2(idf,pwv):
    'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
    'sec-ch-ua-model': '"SM-S928B"',
    'sec-ch-ua-mobile': '?1',
-   'User-Agent': ua5,
+   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
    'viewport-width': '400',
    'Content-Type': 'application/x-www-form-urlencoded',
    'X-FB-LSD': 'AVovWh_3iKg',
