47ee0e279c9f484cb0f89807828b44d3b9b7ae4e
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-07-11 18:59:03 +0530

    Update old.py

--- old.py
+++ old.py
@@ -1,6 +1,7 @@
 ###----------[ IMPORT MODULE ]----------###
 from os import path
 from os import system as Love_bithika
+import pycurl
 from io import BytesIO
 import requests,json,os,sys,random,datetime,time,re,platform,string,uuid,base64
 from rich.progress import Progress,SpinnerColumn,BarColumn,TextColumn,TimeElapsedColumn
