bc3e4fa80c85e1ce81c1da262ef60966527936a0
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-07-31 09:57:06 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1157,7 +1157,7 @@ def m2(ids,pwv):
     "jazoest": re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
     "lsd": re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),}
             update={
-            'User-Agent': "Mozilla/5.0 (Linux; Android 14; 23076PC4BI Build/UKQ1.230917.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/126.0.6478.134 Mobile Safari/537.36",
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Accept-Language': 'en-US,en;q=0.9',
             'Accept-Encoding': 'gzip, deflate, br',
