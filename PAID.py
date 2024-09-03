9cd7005021d4ae41b7090bf31048c0713530213e
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-09-03 12:44:17 +0530

    Update PAID.py

--- PAID.py
+++ PAID.py
@@ -375,7 +375,7 @@ def india():
 		clear()
 		print(f'\033[1;32m[\033[1;31m✓\033[1;32m] EXAMPLE : 9815 | 9814 | 9861 | 9840  ');linex()
 		cod = input(f'{G}[{A}?{G}]{G} CHOICE  : ')
-		code= random.choice([6,7,8,9])
+		code= random.choice(['6','7','8','9'])
 		clear();print(f'\033[1;32m[\033[1;31m✓\033[1;32m] EXAMPLE : 3000 | 5000 | 10000 | 99999 ');linex()
 		limit = int(input(f'{G}[{A}?{G}]{G} CHOICE  : '))
 		clear()
