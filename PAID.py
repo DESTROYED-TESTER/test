dca1d8a4e585ed2d869d38cf4cd7693c67eb50c2
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-09-28 03:01:27 +0530

    Update PAID.py

--- PAID.py
+++ PAID.py
@@ -1763,7 +1763,7 @@ def cracker(uid, pwx, tl):
             'sec-ch-ua-full-version-list': '',
             'sec-ch-prefers-color-scheme': 'light',
             'upgrade-insecure-requests': '1',
-            'user-agent': ua,
+            'user-agent': rua,
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'dnt': '1',
             'origin': 'https://m.prod.facebook.com',
