542877b33c7406bfde9118ae9c20290e9cf97499
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-09-27 15:54:19 +0530

    Update ins.py

--- ins.py
+++ ins.py
@@ -394,13 +394,6 @@ def crack(uid, pww, total_idz):
     except:
         pass
 menu()
- 
-        print(f"\r\033[1;91m [ERROR] - Status code {response.status_code}")
-                continue
-        loop+=1
-    except ConnectionError:
-        time.sleep(10)
-    except:
         pass
 menu()
  
