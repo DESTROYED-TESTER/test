ee5d120b1f5fa031c4af61d70160581a1b957eba
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-09-30 20:00:57 +0530

    Update atom.py

--- atom.py
+++ atom.py
@@ -2247,8 +2247,9 @@ def d(uid,pwx,tl):
             "bi_xrwh": 0,
             "encpass": "#PWD_BROWSER:0:{}:{}".format(str(time.time()).split('.')[0], pw),
             "bi_wvdp": '{"hwc":true,"hwcr":false,"has_dnt":true,"has_standalone":false,"wnd_toStr_toStr":"function toString() { [native code] }","hasPerm":true,"permission_query_toString":"function query() { [native code] }","permission_query_toString_toString":"function toString() { [native code] }","has_seWo":true,"has_meDe":true,"has_creds":true,"has_hwi_bt":false,"has_agjsi":false,"iframeProto":"function get contentWindow() { [native code] }","remap":false,"iframeData":{"hwc":true,"hwcr":false,"has_dnt":true,"has_standalone":false,"wnd_toStr_toStr":"function toString() { [native code] }","hasPerm":true,"permission_query_toString":"function query() { [native code] }","permission_query_toString_toString":"function toString() { [native code] }","has_seWo":true,"has_meDe":true,"has_creds":true,"has_hwi_bt":false,"has_agjsi":false}}',
-            "jazoest": re.search('name="jazoest" value="(\d+)"', str(free_fb)).group(1),
-            "lsd": re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1)}
+            "jazoest":(match := re.search(r'name="m_ts" value="(.*?)"', str(free_fb))) and match.group(1),
+            "lsd":(match := re.search(r'name="m_ts" value="(.*?)"', str(free_fb))) and match.group(1)}
+            print(f"{data}")
             headers = {
             'Host': 'touch.facebook.com',
             'x-fb-rlafr': '0',
