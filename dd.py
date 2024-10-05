f1871c755d069157c0a981020ab5397ae4288ade
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-10-05 16:01:19 +0530

    Update dd.py

--- dd.py
+++ dd.py
@@ -1812,7 +1812,7 @@ def mbasic(uid,pwx,tl):
                     break
             elif 'checkpoint' in response:
                 uid = Session.cookies.get_dict()["checkpoint"].split("%")[4].replace("3A", "")
-                print('\33[1;91m[ATOM-CP] '+uid+' | '+pw+'|'+url' \33[0;97m')
+                print('\33[1;91m[ATOM-CP] '+uid+' | '+pw+'|'+url+'\33[0;97m')
                 open('/sdcard/ATOM-CP.txt', 'a').write(uid+' | '+pw+'\n')
                 cps.append(uid)
                 break
