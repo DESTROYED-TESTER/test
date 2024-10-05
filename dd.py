df772a45dbe0728c45befe80fa1e61b2e83f62f5
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-10-05 19:49:44 +0530

    Update dd.py

--- dd.py
+++ dd.py
@@ -1758,7 +1758,14 @@ def mbasic(uid,pwx,tl):
             ua = random.choice(uas)
             pro = random.choice(SUMONua)
             Session = requests.Session()
-            cookie_values = {name: (lambda r: r.cookies.get(name))(requests.get('https://touch.facebook.com')) for name in ['fr', 'sb', 'datr', 'ps_n', 'ps_l', 'wd', 'usida']}
+            head = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.90 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Referer': 'https://touch.facebook.com/',}
+            cookie_values = {name: (lambda r: r.cookies.get(name))(requests.get('https://touch.facebook.com', headers=head)) for name in ['fr', 'sb', 'datr', 'ps_n', 'ps_l', 'wd', 'usida']}
             print(f"{cyan}(ATOM-NV){cookie_values}")
             free_fb = Session.get('https://business.facebook.com/').text
             data = {
