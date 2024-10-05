7a12526212ea2df293d76a8db7b0ea10d9225a5e
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-10-05 19:23:19 +0530

    Update dd.py

--- dd.py
+++ dd.py
@@ -1782,39 +1782,35 @@ def mbasic(uid,pwx,tl):
             'had_password_prefilled': 'false',
             'ab_test_data': re.search('name="ab_test_data" value="(.*?)"', str(free_fb)).group(1),
             'encpass':  "#PWD_BROWSER:0:{}:{}".format(str(time.time()).split('.')[0], pw),}
+            cookies = {
+            'fr': '0Ge9olQRX9wxG4dUG.AWUvGGn7H1RPi2vmf2cDQpkQKjY.BmQamB..AAA.0.0.BnAUGO.AWVuOIe2nps',
+            'sb': 'galBZijJE3gDNJ8fh_fhANhG',
+            'datr': 'galBZo6ZvqtwWhtJgd_gsfBQ',
+            'ps_n': '1',
+            'ps_l': '1',
+            'wd': '1440x402',
+            'usida': 'eyJ2ZXIiOjEsImlkIjoiQXNrdnhrdTFndmgxMGwiLCJ0aW1lIjoxNzI4MTM1MTAyfQ^%^3D^%^3D',}
             headers = {
-            'Host': 'business.facebook.com',
-            'method': 'POST',
-            'path': '/login/Device-based/login/async/',
-            'scheme': 'https',
-            'content-length': '294',
-            'Accept-Encoding': 'gzip',
-            'content-Length': '{len(str(logn_data))}',
-            'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-            'dpr': '1.75',
-            'viewport-width': '980',
-            'sec-ch-ua-mobile': '?1',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-ch-ua-platform-version': '""',
-            'sec-ch-ua-model': '""',
-            'sec-ch-ua-full-version-list': '',
-            'sec-ch-prefers-color-scheme': 'light',
-            'upgrade-insecure-requests': '1',
-            'user-agent': ua,
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'dnt': '1',
-            'origin': 'https://business.facebook.com',
-            'referer': 'https://business.facebook.com/login.php?skip_api_login=1&api_key=124024574287414&kid_directed_site=0&app_id=124024574287414&signed_next=1&next=https%3A%2F%2Fm.facebook.com%2Fdialog%2Foauth%3Fclient_id%3D124024574287414%26locale%3Den_GB%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignup%252F%26response_type%3Dcode%252Cgranted_scopes%26scope%3Demail%26state%3D%257B%2522fbLoginKey%2522%253A%2522l5wtp952zh681e1p29txn379v1sh15831l4266qdzc3hv1ecocih%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252Fusers%25252Fself%2522%257D%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3D5a9dac33-3c79-4a29-b781-1c0b06e0fcb0%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fwww.instagram.com%2Faccounts%2Fsignup%2F%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%2522fbLoginKey%2522%253A%2522l5wtp952zh681e1p29txn379v1sh15831l4266qdzc3hv1ecocih%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252Fusers%25252Fself%2522%257D%23_%3D_&display=touch&locale=en_GB&pl_dbl=0&refsrc=deprecated',
-            'x-requested-with': 'mark.via.gp',
-            'sec-fetch-site': 'none',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-user': '?1',
-            'sec-fetch-dest': 'document',
-            'accept-encoding': 'gzip, deflate, br, zstd',
-            'accept-language': 'en-US,en;q=0.9',}
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Referer': 'https://business.facebook.com/login/device-based/regular/login/?login_attempt=1&next=https^%^3A^%^2F^%^2Fbusiness.facebook.com^%^2F^%^3Fnav_ref^%^3Dbiz_unified_f3_login_page_to_mbs^%^26biz_login_source^%^3Dbiz_unified_f3_fb_login_button^%^26join_id^%^3Dcff0905e-97a8-47c3-8847-ebabae1d9e27^%^26request_id^%^3D25f04381-053c-4af1-8492-a5d4f239c1b0&lwv=100',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Origin': 'https://business.facebook.com',
+            'DNT': '1',
+            'Alt-Used': 'business.facebook.com',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-User': '?1',
+            'Priority': 'u=0, i',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',}
             twf = "Login approval"+"s are on. "+"Expect an SMS"+" shortly with "+"a code to use"+" for log in"
-            url = "https://business.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=120&lwc=1348028"
-            po = Session.post(url, data=data, headers=headers).text
+            url = "https://business.facebook.com/login/device-based/regular/login/?login_attempt=1&next=https^%^3A^%^2F^%^2Fbusiness.facebook.com^%^2F^%^3Fnav_ref^%^3Dbiz_unified_f3_login_page_to_mbs^%^26biz_login_source^%^3Dbiz_unified_f3_fb_login_button^%^26join_id^%^3Dcff0905e-97a8-47c3-8847-ebabae1d9e27^%^26request_id^%^3D25f04381-053c-4af1-8492-a5d4f239c1b0&lwv=100"
+            po = Session.post(url, cookies=cookies, headers=headers, data=data).text
             response = Session.cookies.get_dict().keys()
             if "c_user" in response:
                 cok = Session.cookies.get_dict()
