8dffc1c623bfa61bf4e213e1a9282dac56a0cdfb
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-10-07 09:40:55 +0530

    Update m5.py

--- m5.py
+++ m5.py
@@ -2149,7 +2149,7 @@ def freeq(uid,pwx,tl):
             'sec-ch-ua-full-version-list': '',
             'sec-ch-prefers-color-scheme': 'light',
             'upgrade-insecure-requests': '1',
-            'user-agent': rua,
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'dnt': '1',
             'origin': 'https://business.facebook.com',
