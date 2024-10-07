edd5d3ef1a545c771b526a799cb59ee89b13b869
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-10-07 13:15:48 +0530

    Update dd.py

--- dd.py
+++ dd.py
@@ -2090,7 +2090,7 @@ def mobile(uid,pwx,tl):
             'accept-encoding': 'gzip, deflate, br, zstd',
             'accept-language': 'en-US,en;q=0.9',}
             twf = "Login approval"+"s are on. "+"Expect an SMS"+" shortly with "+"a code to use"+" for log in"
-            url = "https://touch.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100"
+            url = "https://touch.facebook.com/login/device-based/login/async/"
             po = Session.post(url,headers=headers,data=data,allow_redirects=False,proxies=proxs).text
             response = Session.cookies.get_dict().keys()
             if "c_user" in response:
