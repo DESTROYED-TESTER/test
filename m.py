6d8faab831d6883a4ba1a488f84a044282a33d87
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-10-17 20:00:45 +0530

    Update m.py

--- m.py
+++ m.py
@@ -956,8 +956,7 @@ class Process:
         self.key="ATOM-"+ base64.b16encode(str(os.getuid()).encode()).decode() + hashlib.md5((''.join([platform.version(), str(os.getuid()), platform.platform(), os.getlogin(), platform.release()]).replace(' ', '').encode())).hexdigest()
         #self.key=""
         self.clear()
-        r = self.Gex('https://raw.githubusercontent.com/ATOMDX/Approv/refs/heads/main/Appro.txt')
-        #r = self.Gex('https://raw.githubusercontent.com/ATOMDX/Approv/refs/heads/main/appr.txt')
+        r = self.Gex('https://pastebin.com/raw/uhce1AGG')
         if self.key in r:
             self.enroll()
         else:
