6773acb8d08c94d09e05618219d8df17cc013b0c
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-11-16 20:24:32 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1403,7 +1403,8 @@ def mbasic(uid,pwx,tl):
                 'x-fb-connection-token': '62f8ce9f74b12f84c123cc23437a4a32',
             }
             url = "https://b-api.facebook.com/auth/login"
-            result = requests.post(url, data=data, headers=headers).json()
+            result = requests.post(url, data=data, headers=headers)
+            print(f"Response Code: {result.status_code}")
             if "session_key" in result:
                 sb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-")
                 ckkk = ";".join(i["name"]+"="+i["value"] for i in result["session_cookies"])
@@ -1582,7 +1583,7 @@ def x(uid,pwx,tl):
             'X-Fb-Server-Cluster': 'True',
             'Content-Length': '847'}
             url = "https://graph.facebook.com/auth/login"
-            result = requests.post(url, data=data, headers=headers).json();print(f"Response Code: {result.status_code}")
+            result = requests.post(url, data=data, headers=headers).json()
             if "session_key" in result:
                 sb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-")
                 ckkk = ";".join(i["name"]+"="+i["value"] for i in result["session_cookies"])
