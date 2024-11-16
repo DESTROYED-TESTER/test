a4c3e88c23c8649ed9905e58918739975327b228
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-11-16 20:32:20 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1492,7 +1492,7 @@ def p(uid,pwx,tl):
             'X-FB-Friendly-Name': 'authenticate',
             'Content-Type': 'application/x-www-form-urlencoded',
             'Content-Length': '1026'}
-            url = "https://graph.facebook.com/auth/login"
+            url = "https://www.facebook.com/auth/login"
             result = requests.post(url, data=data, headers=headers)
             print(f"Response Code: {result.status_code}")
             if "session_key" in result:
