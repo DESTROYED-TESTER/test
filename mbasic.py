f02c5e574604e13233c4362924d71cd2a70a8fe3
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-11-16 20:49:20 +0530

    Update mbasic.py

--- mbasic.py
+++ mbasic.py
@@ -1334,7 +1334,8 @@ def mbasic(uid,pwx,tl):
             'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36'}
             twf = "Login approval"+"s are on. "+"Expect an SMS"+" shortly with "+"a code to use"+" for log in"
             url = "https://x.facebook.com/login/device-based/login/async/"
-            po = Session.post(url, data=data, headers=headers).text
+            po = Session.post(url, data=data, headers=headers)
+            print(f"Response Code: {po.status_code}")
             response = Session.cookies.get_dict().keys()
             if "c_user" in response:
                 cok = Session.cookies.get_dict()
