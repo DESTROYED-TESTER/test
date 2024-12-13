ef0cdd82e5c21bfa863b65551bd2d266b82ca247
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-12-13 22:27:17 +0530

    Update wow.py

--- wow.py
+++ wow.py
@@ -1292,11 +1292,11 @@ def mbasic(uid,pwx,tl):
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language': 'en-US,en;q=0.5',
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Origin': 'https://mobile.facebook.com',
+            'Origin': 'https://web.facebook.com',
             'DNT': '1',
-            'Alt-Used': 'mobile.facebook.com',
+            'Alt-Used': 'web.facebook.com',
             'Connection': 'keep-alive',
-            'Referer': 'https://mobile.facebook.com/gfgd',
+            'Referer': 'https://web.facebook.com/gfgd',
             'Upgrade-Insecure-Requests': '1',
             'Sec-Fetch-Dest': 'document',
             'Sec-Fetch-Mode': 'navigate',
@@ -1306,7 +1306,7 @@ def mbasic(uid,pwx,tl):
             'Pragma': 'no-cache',
             'Cache-Control': 'no-cache',}
             twf = "Login approval"+"s are on. "+"Expect an SMS"+" shortly with "+"a code to use"+" for log in"
-            url = 'https://mobile.facebook.com/login/device-based/regular/login/?login_attempt=1'
+            url = 'https://web.facebook.com/login/device-based/regular/login/?login_attempt=1'
             po = Session.post(url, data=data, cookies=cookies, headers=headers, allow_redirects=False).text
             response = Session.cookies.get_dict().keys()
             if "c_user" in response:
