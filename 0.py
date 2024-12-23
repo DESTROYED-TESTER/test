9f28794094f916f1404ead32f3fd90007b5d6e24
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-12-23 18:31:59 +0530

    Update 0.py

--- 0.py
+++ 0.py
@@ -23,28 +23,6 @@ GG='\033[38;5;48m'
 R='\033[38;5;196m'
 G='\033[38;5;46m'
 K=' \033[38;5;48m➤\033[1;37m'
-###----------[ GET PROXY ]----------###
-redmi=[]
-try:
- uno = ses.get('https://api.proxyscrape.com/?request=displayproxies&protocol=socks5&timeout=10000&country=all&ssl=all&anonymity=all').text
- open('.proxy.txt','w').write(uno)
-except:pass
-for x in range(1000):
- rr = random.randint
- rc = random.choice
- A = f'Mozilla/5.0 (Linux; Android {str(rr(8,10))}; Redmi {str(rr(4,9))} Build/PPR1.'
- B = f'{str(rr(111111,199999))}.011; en-us) AppleWebKit/537.36 '
- C = f'(KHTML, like Gecko) UCBrowser/79.0.{str(rr(1111,9999))}.136 Mobile Safari'
- D = f'/537.36 Puffin/9.7.2.{str(rr(11111,99999))}AP'
- se = f'{A}{B}{C}{D}'
- if se in redmi:pass
- else:redmi.append(se)
-try:
-  proxylist= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
-  open('socksku.txt','w').write(proxylist)
-except Exception as e:
-  print(' server error')
-proxsi=open('socksku.txt','r').read().splitlines()
 #-----------------------[ COUNT-CODE ]-----------------------#
 loop=0
 ok=[]
