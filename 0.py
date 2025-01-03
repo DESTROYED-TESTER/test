50cf0109aa7ae092ef769963565f0460e4160715
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-01-03 13:59:17 +0530

    Update 0.py

--- 0.py
+++ 0.py
@@ -173,7 +173,7 @@ def convert(cookie):
 def crackerr(ids,passlist):
     global loop,oks,cps
     session = requests.Session()
-    sys.stdout.write('\r  \x1b[1;92m%s\x1b[1;97m | \x1b[1;92m%s\x1b[1;97m \r'%(loop,len(oks))),
+    sys.stdout.write('\r \033[1;97m[\x1b[1;92mRNDM•2\x1b[1;97m] \x1b[1;92m%s\x1b[1;97m | \x1b[1;92m%s\x1b[1;97m \r'%(loop,len(oks))),
     sys.stdout.flush()
     try:
         for pas in passlist:
