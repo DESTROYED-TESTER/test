42b1b9967c2e8344c7d2b33a43280c320b8ae57f
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-01-12 15:59:44 +0530

    Update wow.py

--- wow.py
+++ wow.py
@@ -520,7 +520,7 @@ def SUMON2():
         cookie_show.append("yes")
     else:
         cookie_show.append("no")
-    with ThreadPool(max_workers=50) as SUMON_xd:
+    with ThreadPool(max_workers=90) as SUMON_xd:
         clear()
         SUMON_time()
         tl = str(len(user))
