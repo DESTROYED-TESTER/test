e41bb464fe057e4c567b421fc431e360872262fd
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-01-12 16:51:29 +0530

    Update hst.py

--- hst.py
+++ hst.py
@@ -430,7 +430,7 @@ def cracker(ids,passlist):
     except requests.exceptions.ConnectionError:
         time.sleep(20)
     except Exception as e:
-        #print(f"\nError: {e}")
+        print(f"\nError: {e}")
         pass
 
 def mainn():
