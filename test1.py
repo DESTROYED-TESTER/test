8a4d0bdeb1e51a2a43e558dbcb0d82ef47aae9c7
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-01-16 00:49:27 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -504,7 +504,7 @@ def SUMON2():
         cookie_show.append("yes")
     else:
         cookie_show.append("no")
-    with ThreadPool(max_workers=60) as SUMON_xd:
+    with ThreadPool(max_workers=30) as SUMON_xd:
         clear()
         SUMON_time()
         tl = str(len(user))
