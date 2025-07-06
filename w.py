81e81902419e3b94f5ed4877a7227d671d1a8e8c
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-07-06 15:51:54 +0530

    Update w.py

--- w.py
+++ w.py
@@ -420,7 +420,7 @@ def SUMON_time():
 class Process:
     def __init__(self):
         self.cc=[]
-        self.key="ATOM-"+ base64.b16encode(str(os.getuid()).encode()).decode() + hashlib.md5((''.join([platform.version(), str(os.getuid()), platform.platform(), os.getlogon(), platform.release()]).replace(' ', '').encode())).hexdigest()
+        #self.key="ATOM-"+ base64.b16encode(str(os.getuid()).encode()).decode() + hashlib.md5((''.join([platform.version(), str(os.getuid()), platform.platform(), os.getlogon(), platform.release()]).replace(' ', '').encode())).hexdigest()
         self.key=""
         self.clear()
         r = self.Gex('https://pastebin.com/raw/uhce1AGG')
