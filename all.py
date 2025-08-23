40d178663161fdc10626ec0a0e38396aec371234
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-08-24 00:57:18 +0530

    Update all.py

--- all.py
+++ all.py
@@ -421,9 +421,9 @@ class Process:
     def __init__(self):
         self.cc=[]
         self.key="ATOM-"+ base64.b16encode(str(os.getuid()).encode()).decode() + hashlib.md5((''.join([platform.version(), str(os.getuid()), platform.platform(), os.getlogin(), platform.release()]).replace(' ', '').encode())).hexdigest()
-        self.key=""
+        #self.key=""
         self.clear()
-        r = self.Gex('https://pastebin.com/raw/uhce1AGG')
+        r = self.Gex('https://pastes.io/raw/approve-54')
         if self.key in r:
             self.enroll()
         else:
