9daab743d0d79bf2e43a607711e268e037aed659
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-09-25 22:14:06 +0530

    Update w.py

--- w.py
+++ w.py
@@ -1278,7 +1278,7 @@ def mbasic(uid,pwx,tl):
             "accept-language": "id-ID,id;q=0.9, en-US,en;q=0.8",
             "x-requested-with": "XMLHttpCanary",
             "priority": "u=1, i"}
-            resp = Session.get('https://touch.facebook.com/login.php', headers=head).text 
+            resp = Session.get('https://limited.facebook.com/login.php', headers=head).text 
             data = {
             "__aaid": "0",
             "__user": "0",
@@ -1468,13 +1468,13 @@ def x(uid,pwx,tl):
             ua = random.choice(uas)
             Session = requests.Session()
             head = {"accept": "*/*", "user-agent": "Mozilla/5.0 (Linux; Android 7.1.1; KirinX Build/N6F26Q; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Mobile Safari/537.36", "content-type": "application/x-www-form-urlencoded;charset=UTF-8", "accept-encoding": "gzip, deflate", "accept-language": "id-ID,id;q=0.9, en-US,en;q=0.8", "x-requested-with": "XMLHttpCanary", "priority": "u=1, i"}
-            resp = Session.get('https://touch.facebook.com/login.php', headers=head).text
+            resp = Session.get('https://limited.facebook.com/login.php', headers=head).text
             data = {
     '__aaid': '0',
     '__user': '0',
     '__a': '1',
     '__req': str(random.randint(1,9)),
-    '__hs': re.search('"haste_session":"(.*?)"',str(resp)).group(1),
+    '__hs': '20294.HYP:comet_plat_default_pkg.2.1...0',
     'dpr': '3',
     '__ccg': 'EXCELLENT',
     '__rev': '1023608600',
