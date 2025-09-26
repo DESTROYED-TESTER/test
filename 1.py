964e96feceaba11229465bf196ad79e315c8fd91
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-09-27 00:50:55 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://password-enc-api-instagram-facebook.p.rapidapi.com/api/pass_enc/"
 
-querystring = {"p":"Password123 ","v":"11","m":"fbweb"}
+querystring = {"p":"Password123 ","v":"5","m":"fbweb"}
 
 headers = {
 	"x-rapidapi-key": "5b5179b509msh1ba72c98ba4d120p1e1335jsn6eb28abafc48",
