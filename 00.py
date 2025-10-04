143756b4ad5cbfb7a1e362ce13ddee7cb37709ba
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-10-05 01:17:11 +0530

    Update 00.py

--- 00.py
+++ 00.py
@@ -1,3 +1,3 @@
 import requests
-r = requests.get("https://api.telegram.org/bot{token}/getMe".format(token="7495165574:AAEsmwWrR5DAhFFDRzeCptgTVEVCgYxG3n4"))
+r = requests.get("https://api.telegram.org/bot{token}/getMe".format(token="8301704871:AAGbBRYqENld4lERHxK5G3DjIOdAWIZqBbY"))
 print(r.json())
