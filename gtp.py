cd525398a4d3f0e76f087e5d4a046e016b824535
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-10-14 22:33:24 +0530

    Update gtp.py

--- gtp.py
+++ gtp.py
@@ -41,16 +41,16 @@ headers = {
     # 'cookie': 'session-id=132-5312887-9428840; ad-oo=0; ci=eyJpc0dkcHIiOmZhbHNlfQ; ubid-main=132-2202699-7637707; at-main=Atza|IwEBIIhZ4ns4Huk1ftaJIcd2DVhJOTSlrpxLoE70QHiyetFPr7_FHsOEP_Z07q2z4drp2jhIRNUYftOMfevscGMt1g6KBvOaSgPf-VSalsZ2lAGidJ1S5DrH5ap6Rzx1GlKRQ5p6IpezphfbkTKUPOB0Fe28lAgs5BwckldNHfKWwj3AS8pc3MCKqWU6-gHiFHywOIekC5tx1571GExzpwYZVx1Cmug_ds7trK_Kdxh-awfWbA; sess-at-main=dyh7lGTSX//CX/vXoOk3ObsKQKznw4LG+lXInveSimY=; session-id-time=2082787201l; uu=eyJpZCI6InV1ZjRkM2Y0M2UxZTU5NDMwN2EyZjEiLCJwcmVmZXJlbmNlcyI6eyJmaW5kX2luY2x1ZGVfYWR1bHQiOmZhbHNlfX0=; signup-offer-territory=IN; x-main=6kWkQsiWmVaQ6tOVl9v2eIJ3vwe9PT9sTzEC0uLpEPzmKondTasnFd5WarFJ96mF; session-token=q2tWvYOEBpsWvtJebVgvfGwRzYHGnXKEgVL9DntSwMz3f4WDUu5myP2hlnkJvbg/VbHmXN7Yt5hduaR5pftFUFYyNYDlLFxX1tOACSK1xdnOVzoi0OZVjMEyiktbhYT6y0tH1uOM4sxRc+eRSWPBMkjPwDUPaDbRi75Np+HUC592VlUIw1t2sxAxVFdHJCJ/X7bZHd3PS1cIACyoAybB+3dpVorukTLZ6wwqY04zWHFvQalwE3rACoDYxcNw5FlTRVkXe6ITm4+bvLT2ejhq+SAWbWUV72mcBKSKMrnF22l9wOHt+YBwykzUCHEtcedCjxBZQkR6V9SojF3yQVzPTZPrPXI6V5OPgOKmS0+/adQhxH0XKG1Elr4KiFwcGbEP; international-seo=es',
 }
 
-bio_text = "https://www.imdb.com/user/ur205123734/"
+bio_text = "[url=https://click.hdfree.site/Adult/]🌐 𝖢𝖫𝖨𝖢𝖪 𝖧𝖤𝖱𝖤 🌐==►►[/url]"
 
 json_data = {
     'query': '''
-        mutation UpdateUserBio($bioText: String!, $originalTitleText: Boolean!) {
-          updateUserProfileBio(input: {bio: $bioText}) {
+        mutation UpdateUserBio($biourl: String!, $originalTitleurl: Boolean!) {
+          updateUserProfileBio(input: {bio: $biourl}) {
             status {
               updateStatus
               modifiedItem {
-                plaidHtml(showLineBreak: true, showOriginalTitleText: $originalTitleText)
+                plaidHtml(showLineBreak: true, showOriginalTitleurl: $originalTitleurl)
                 markdown
               }
               updateFeedback {
@@ -74,7 +74,7 @@ json_data = {
     },
 }
 
-response = requests.post('https://b-graph.imdb.com/', cookies=cookies, headers=headers, json=json_data)#https://api.graphql.imdb.com/
+response = requests.post('https://api.graphql.imdb.com/', cookies=cookies, headers=headers, json=json_data)#https://api.graphql.imdb.com/
 
 # Check response
 try:
