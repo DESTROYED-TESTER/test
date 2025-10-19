bb994258d7ae23e14366d3d3fe5f2007dc9d244c
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-10-19 14:05:44 +0530

    Update 0.py

--- 0.py
+++ 0.py
@@ -3,15 +3,29 @@ import requests
 url = "https://www.facebook.com/login/device-based/regular/login/?login_attempt=1"
 
 headers = {
-    "authority": "www.facebook.com",
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "accept-language": "en-US,en;q=0.9",
-    "cache-control": "max-age=0",
-    "content-type": "application/x-www-form-urlencoded",
-    "cookie": "fr=0yNyOdBia7SurAz5e..Bo9J-7..AAA.0.0.Bo9J-7.AWcpcPtzmuPsw5P6FNpwX8Zx-Tw; sb=u5_0aFF51EbifVBDoEnsi9Ut; datr=u5_0aCroimhveeODthgePFtZ; dpr=2.4749999046325684; wd=980x1146",
-    "origin": "https://www.facebook.com",
-    "referer": "https://www.facebook.com/Facebookids",
-    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+    'authority': 'www.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'content-type': 'application/x-www-form-urlencoded',
+    # 'cookie': 'fr=0yNyOdBia7SurAz5e..Bo9J-7..AAA.0.0.Bo9J-7.AWcpcPtzmuPsw5P6FNpwX8Zx-Tw; sb=u5_0aFF51EbifVBDoEnsi9Ut; datr=u5_0aCroimhveeODthgePFtZ; dpr=2.4749999046325684; wd=980x1146',
+    'dpr': '2.4749999046325684',
+    'origin': 'https://www.facebook.com',
+    'referer': 'https://www.facebook.com/Facebookids',
+    'sec-ch-prefers-color-scheme': 'light',
+    'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',
+    'sec-ch-ua-full-version-list': '"Chromium";v="107.0.5304.74", "Not=A?Brand";v="24.0.0.0"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-model': '""',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-ch-ua-platform-version': '""',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+    'viewport-width': '980',
 }
 
 data = {
