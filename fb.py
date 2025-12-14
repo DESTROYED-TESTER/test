ffdf37a3b20954ceba2956343cbac723a3e6a6ae
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-12-14 12:25:37 +0530

    Update fb.py

--- fb.py
+++ fb.py
@@ -245,7 +245,7 @@ def crack(uid, password_list, total_count):
                 cps.append(uid+"|"+pw)
                 continue
             else:
-                print(f"\r\033[1;91m [ERROR] - Status code {response.status_code}")
+                print(f"\r\033[1;91m [ERROR] - Status code {respon.status_code}")
                 continue
                 
     except requests.exceptions.Timeout:
