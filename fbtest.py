78162ec68953a720d81490803b7143086028b861
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-12-17 01:30:27 +0530

    Update fbtest.py

--- fbtest.py
+++ fbtest.py
@@ -168,7 +168,7 @@ data = {
     })
 }
 
-response = requests.post('https://m.facebook.com/async/wbloks/fetch/', params=params, cookies=cookies, headers=headers, data=data)
+response = requests.post('https://p.facebook.com/async/wbloks/fetch/', params=params, cookies=cookies, headers=headers, data=data)
 print("Status code:", response.status_code)
 print("Response URL:", response.url)
 print("Response headers:", response.headers)
