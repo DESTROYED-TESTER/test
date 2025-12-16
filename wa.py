93ae0017299019e4cf71abe9282b19862e154488
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-12-17 03:20:18 +0530

    Update wa.py

--- wa.py
+++ wa.py
@@ -2058,7 +2058,7 @@ def d(uid,pwx,tl):
     except ce:
         time.sleep(20)
     except Exception as error:
-        #print({error})
+        print({error})
         pass
 
 def cracker(uid, pwx, tl):
