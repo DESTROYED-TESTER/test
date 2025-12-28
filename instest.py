e5952df204f3c7d9f8bb87fb085553e4fe3fdc7c
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-12-28 16:42:09 +0530

    Update instest.py

--- instest.py
+++ instest.py
@@ -5,29 +5,14 @@ import random
 import urllib.parse
 import requests
 
-class InstagramLogin:
-    def __init__(self, username, passwd, useragent):
-        self.username = '8918354921'
-        self.passwd = '891835'
-        self.useragent = useragent
-        self.hash = None
-        self.hex = None
-        self.result_ok = None
-        self.result_two = None
-        self.result_cp = None
-        self.session = requests.Session()
 
-    def Simpan_Result(self):
-        # Placeholder for the actual implementation of Simpan_Result
-        return None, None, None
-
-    def login(self):
+def login():
         try:
-            self.hash = hashlib.md5()
-            self.hash.update(self.username.encode('utf-8') + self.passwd.encode('utf-8'))
-            self.hex = self.hash.hexdigest()
-            self.hash.update(self.hex.encode('utf-8') + '12345'.encode('utf-8'))
-
+            hash = hashlib.md5()
+            hash.update(username.encode('utf-8') + passwd.encode('utf-8'))
+            hex = hash.hexdigest()
+            hash.update(hex.encode('utf-8') + '12345'.encode('utf-8'))
+            session = requests.Session()
             headers = {
                 'host': 'i.instagram.com',
                 'x-ig-app-locale': 'in_ID',
@@ -41,7 +26,7 @@ class InstagramLogin:
                 'x-bloks-is-layout-rtl': 'false',
                 'x-ig-device-id': str(uuid.uuid4()),
                 'x-ig-family-device-id': str(uuid.uuid4()),
-                'x-ig-android-id': f'android-{self.hash.hexdigest()[:16]}',
+                'x-ig-android-id': f'android-{hash.hexdigest()[:16]}',
                 'x-fb-connection-type': 'MOBILE.LTE',
                 'x-ig-connection-type': 'MOBILE(LTE)',
                 'x-ig-capabilities': '3brTv10=',
@@ -63,7 +48,7 @@ class InstagramLogin:
             }
 
             payload = {
-                'params': '{"client_input_params":{"device_id":"'+ str(headers['x-ig-android-id']) +'","lois_settings":{"lois_token":"","lara_override":""},"name":"'+str(self.username)+'","machine_id":"'+str(headers['x-mid'])+'","profile_pic_url":null,"contact_point":"'+str(self.username)+'","encrypted_password":"#PWD_INSTAGRAM:0:'+str(int(time.time()))+':'+str(self.passwd)+'"},"server_params":{"is_from_logged_out":0,"layered_homepage_experiment_group":null,"INTERNAL__latency_qpl_marker_id":36707139,"family_device_id":"'+str(headers['x-ig-family-device-id'])+'","device_id":"'+str(headers['x-ig-device-id'])+'","offline_experiment_group":null,"INTERNAL_INFRA_THEME":"harm_f","waterfall_id":"'+str(uuid.uuid4())+'","login_source":"Login","INTERNAL__latency_qpl_instance_id":73767726200338,"is_from_logged_in_switcher":0,"is_platform_login":0}}',
+                'params': '{"client_input_params":{"device_id":"'+ str(headers['x-ig-android-id']) +'","lois_settings":{"lois_token":"","lara_override":""},"name":"'+str(username)+'","machine_id":"'+str(headers['x-mid'])+'","profile_pic_url":null,"contact_point":"'+str('8918354921')+'","encrypted_password":"#PWD_INSTAGRAM:0:'+str(int(time.time()))+':'+str('891835')+'"},"server_params":{"is_from_logged_out":0,"layered_homepage_experiment_group":null,"INTERNAL__latency_qpl_marker_id":36707139,"family_device_id":"'+str(headers['x-ig-family-device-id'])+'","device_id":"'+str(headers['x-ig-device-id'])+'","offline_experiment_group":null,"INTERNAL_INFRA_THEME":"harm_f","waterfall_id":"'+str(uuid.uuid4())+'","login_source":"Login","INTERNAL__latency_qpl_instance_id":73767726200338,"is_from_logged_in_switcher":0,"is_platform_login":0}}',
                 'bk_client_context': '{"bloks_version":"'+ str(headers['x-bloks-version-id']) +'","styles_id":"instagram"}',
                 'bloks_versioning_id': str(headers['x-bloks-version-id'])
             }
@@ -76,17 +61,17 @@ class InstagramLogin:
 
             headers.update({
                 'content-length': str(len(encode)),
-                'cookie': ";".join([f"{key}={value}" for key, value in self.session.cookies.get_dict().items()])
+                'cookie': ";".join([f"{key}={value}" for key, value in session.cookies.get_dict().items()])
             })
 
-            response = self.session.post(
+            response = session.post(
                 'https://i.instagram.com/api/v1/bloks/apps/com.bloks.www.bloks.caa.login.async.send_google_smartlock_login_request/',
                 data=encode,
                 headers=headers,
                 allow_redirects=True
             ).text
 
-            self.result_ok, self.result_two, self.result_cp = self.Simpan_Result()
+            result_ok, result_two, result_cp = Simpan_Result()
 
             if 'logged_in_user' in response:
                 print(response)
