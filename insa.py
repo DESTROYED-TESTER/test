58fc1f72e1f0201e23ed38cc4894d6079462e4d3
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2025-12-29 00:18:05 +0530

    Update insa.py

--- insa.py
+++ insa.py
@@ -258,7 +258,7 @@ def random_number():
     # Start multi-threaded attack
     start_time = time.time()
     
-    with ThreadPoolExecutor(max_workers=30) as executor:
+    with ThreadPoolExecutor(max_workers=80) as executor:
         futures = []
         
         for random_id in idz:
