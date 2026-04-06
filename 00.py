e0093d7ef3c4d10bfdd342ce11d2e3e7cf54a23f
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2026-04-06 11:54:25 +0530

    Update 00.py

--- 00.py
+++ 00.py
@@ -62,7 +62,7 @@ data = {
 }
 
 response = session.post(
-    'https://mobile.facebook.com/login/device-based/regular/login/',
+    'https://x.facebook.com/login/device-based/regular/login/',
     params=params,
     cookies=cookies,
     headers=headers,
