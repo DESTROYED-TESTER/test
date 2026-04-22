10774b7e9921027e52a903f8d10afad00a954ee3
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2026-04-22 14:50:24 +0530

    Update 00.py

--- 00.py
+++ 00.py
@@ -15,7 +15,7 @@ head = {"authority": "m.prod.facebook.com",
             "upgrade-insecure-requests": "1",
             "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36 Edg/145.0.0.0',
             "viewport-width": "980"}
-response = session.get('https://www.facebook.com/login',headers=head)
+response = Session.get('https://www.facebook.com/login',headers=head)
 datr = response.cookies.get('datr')
 sb = response.cookies.get('sb')
 fr = response.cookies.get('fr')
