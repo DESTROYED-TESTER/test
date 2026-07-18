5001362e369809ffacbf731ebdea5abbd3c6d607
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2026-07-18 10:38:01 +0530

    Update 00.py

--- 00.py
+++ 00.py
@@ -3,26 +3,20 @@ import time
 import re
 Session = requests.Session()
 
-head = {"authority": "m.prod.facebook.com",
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "accept-language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
-            "cache-control": "max-age=0",
-            "dpr": "3",
-            "sec-ch-prefers-color-scheme": "light",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "none",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1",
-            "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36 Edg/145.0.0.0',
-            "viewport-width": "980"}
-response = Session.get('https://www.facebook.com/login',headers=head)
-response2 = Session.get('https://touch.facebook.com')
-datr = response.cookies.get('datr')
-sb = response.cookies.get('sb')
-fr = response.cookies.get('fr')
+head = {"authority":"m.prod.facebook.com","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7","accept-language":"en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7","cache-control":"max-age=0","dpr":"3","sec-ch-prefers-color-scheme":"light","sec-fetch-dest":"document","sec-fetch-mode":"navigate","sec-fetch-site":"none","sec-fetch-user":"?1","upgrade-insecure-requests":"1","user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36 Edg/145.0.0.0","viewport-width":"980"}
+requu1 = Session.get('https://touch.facebook.com/login',headers=head)
+datr = requu1.cookies.get('datr')
+sb = requu1.cookies.get('sb')
+fr = requu1.cookies.get('fr')
+data = {'m_ts': re.search('name="m_ts" value="(.*?)"',str(requu1.text)).group(1), 'li': re.search('name="li" value="(.*?)"',str(requu1.text)).group(1), 'try_number': '0', 'unrecognized_tries': '0', 'email': uid, 'prefill_contact_point': '', 'prefill_source': '', 'prefill_type': '', 'first_prefill_source': '', 'first_prefill_type': '', 'had_cp_prefilled': 'false', 'had_password_prefilled': 'false', 'is_smart_lock': 'false', 'bi_xrwh': '0', 'encpass': "#PWD_BROWSER:0:{}:{}".format(str(time.time()).split('.')[0], pw), 'bi_wvdp': '', 'fb_dtsg': '', 'jazoest': re.search('name="jazoest" value="(.*?)"',str(requu1.text)).group(1), 'lsd': re.search('name="lsd" value="(.*?)"',str(requu1.text)).group(1), '__dyn': '', '__csr': '', '__req': random.choice(["1","2","3","4","5","6","7","8","9","0"]), '__fmt': '0', '__a': '',  '__user': '0'}
+cookies = {
+    "datr": datr,
+    "sb": sb,
+    "m_pixel_ratio": "2.75",
+    "wd": "393x851",
+    "fr": fr,
+}
 url = "https://mbasic.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100"
-
 headers = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 12; M2010J19SI) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Mobile Safari/537.36",
     "Content-Type": "application/x-www-form-urlencoded",
@@ -41,49 +35,11 @@ headers = {
     "accept-language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
 }
 
-cookies = {
-    "datr": datr,
-    "sb": sb,
-    "m_pixel_ratio": "2.75",
-    "wd": "393x851",
-    "fr": fr,
-}
-
-data = {
-    "m_ts": "1776847240",
-    "li": "iInoaUeC-fKxXMoj2g1DhteN",
-    "try_number": "0",
-    "unrecognized_tries": "0",
-    "email": "9907159211",
-    "prefill_contact_point": "bithikasumon81@gmail.com",
-    "prefill_source": "browser_dropdown",
-    "prefill_type": "contact_point",
-    "first_prefill_source": "browser_dropdown",
-    "first_prefill_type": "contact_point",
-    "had_cp_prefilled": "true",
-    "had_password_prefilled": "false",
-    "is_smart_lock": "false",
-    "bi_xrwh": "0",
-    "bi_wvdp": '{"hwc":true,"hwcr":false,"has_dnt":true,"has_standalone":false,"wnd_toStr_toStr":"function toString() { [native code] }","hasPerm":true,"permission_query_toString":"function query() { [native code] }","permission_query_toString_toString":"function toString() { [native code] }","has_seWo":true,"has_meDe":true,"has_creds":true,"has_hwi_bt":false,"has_agjsi":false,"iframeProto":"function get contentWindow() { [native code] }","remap":false,"iframeData":{"hwc":true,"hwcr":false,"has_dnt":true,"has_standalone":false,"wnd_toStr_toStr":"function toString() { [native code] }","hasPerm":true,"permission_query_toString":"function query() { [native code] }","permission_query_toString_toString":"function toString() { [native code] }","has_seWo":true,"has_meDe":true,"has_creds":true,"has_hwi_bt":false,"has_agjsi":false}}',
-    "encpass": "#PWD_BROWSER:0:{}:{}".format(str(time.time()).split('.')[0], '99071592'),
-    "fb_dtsg": "NAfsfb_rRUK1j8xFoiAUrWhu_BKjct4Ic-TBEHU19VbowNsaWe4Hcag:0:0",
-    "jazoest": "25035",
-    "lsd": re.search('name="lsd" value="(.*?)"',str(response2.text)).group(1),
-    "dyn": "1KQdAG1mws8-t0BBBzEnwuo98nwgU2owpUuwcC4o1nEhw23E52q1ewb60Y82Cwro0wa4o1MUaE36wdq0ny0oi0zE1jU1soG0hi0Lo6-0Co178dE1UU3jwGwbu",
-    "csr": "",
-    "hsdp": "",
-    "hblp": "",
-    "sjsp": "",
-    "req": "5",
-    "fmt": "1",
-    "a": "AYyfLpBu5achetlX00INO5Qb-dC_HCzf5HpaqdSNxtCol1NACQ9pj5LesUA4hsAU2-iBu36gNbh8uE_qvzKIC2khJKVeM7RacPE",
-    "__user": "0",
-}
 
 response = Session.post(url, headers=headers, cookies=cookies, data=data)
-
-print(response.status_code)
-print(response.text)
+print(cookies)
+#print(response.status_code)
+#print(response.text)
 # Check login success
 log_cookies = Session.cookies.get_dict().keys()
 if "c_user" in log_cookies:
