951deef2c4e2fcfd367584e44a9d5f722ea374bc
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2026-07-18 10:40:40 +0530

    Update 00.py

--- 00.py
+++ 00.py
@@ -4,7 +4,7 @@ import re
 Session = requests.Session()
 
 head = {"authority":"m.prod.facebook.com","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7","accept-language":"en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7","cache-control":"max-age=0","dpr":"3","sec-ch-prefers-color-scheme":"light","sec-fetch-dest":"document","sec-fetch-mode":"navigate","sec-fetch-site":"none","sec-fetch-user":"?1","upgrade-insecure-requests":"1","user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36 Edg/145.0.0.0","viewport-width":"980"}
-requu1 = Session.get('https://m.prod.facebook.com/',headers=head)
+requu1 = Session.get('https://touch.facebook.com/',headers=head)
 datr = requu1.cookies.get('datr')
 sb = requu1.cookies.get('sb')
 fr = requu1.cookies.get('fr')
